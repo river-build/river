3a0c63dd2a41a7cc66d393171aab5e3108b94605
Serge Khorun <104387024+sergekh2@users.noreply.github.com>
2024-05-20 14:38:24 -0700

    Switch solidity license from Apache to MIT (#6)

--- contracts/src/tokens/river/mainnet/claimer/IAuthorizedClaimers.sol
+++ contracts/src/tokens/river/mainnet/claimer/IAuthorizedClaimers.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
 // This contract is used to authorize claimers to claim rewards on the authorizer's behalf
