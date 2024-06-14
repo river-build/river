8a0f273883785548e5f0a1bead84427f401f5309
texuf <texuf.eth@gmail.com>
2024-06-14 16:02:23 -0700

    Stress test updates (#175)
    refactors for ergonomics and usablility
    updates for test output
    add ci test that runs node js code

--- packages/stress/src/utils/connection.ts
+++ packages/stress/src/utils/connection.ts
@@ -1,3 +1,4 @@
+import { check } from '@river-build/dlog'
 import { LocalhostWeb3Provider } from '@river-build/web3'
 import { RiverConfig, makeRiverRpcClient, makeSignerContext, userIdFromAddress } from '@river/sdk'
 import { ethers } from 'ethers'
@@ -7,19 +8,16 @@ export async function makeConnection(config: RiverConfig, wallet?: ethers.Wallet
     const delegateWallet = ethers.Wallet.createRandom()
     const signerContext = await makeSignerContext(wallet, delegateWallet)
     const userId = userIdFromAddress(signerContext.creatorAddress)
+    check(userId === wallet.address, `userId !== wallet.address ${userId} !== ${wallet.address}`)
     const riverProvider = new LocalhostWeb3Provider(config.river.rpcUrl, wallet)
     const baseProvider = new LocalhostWeb3Provider(config.base.rpcUrl, wallet)
     const rpcClient = await makeRiverRpcClient(riverProvider, config.river.chainConfig)
     return {
         userId,
-        wallet,
         delegateWallet,
         signerContext,
-        config,
         baseProvider,
         riverProvider,
         rpcClient,
     }
 }
-
-export type Connection = Awaited<ReturnType<typeof makeConnection>>
