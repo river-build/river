6b9abe3a7f19f82f4bfb20c622fc43bf0bd1c4eb
Kerem Kazan <kerem.kazan@gmail.com>
2024-06-18 13:23:14 -0400

    streamRegistryTest

--- contracts/test/river/registry/stream/StreamRegistry.t.sol
+++ contracts/test/river/registry/stream/StreamRegistry.t.sol
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+// interfaces
+import {IOwnableBase} from "contracts/src/diamond/facets/ownable/IERC173.sol";
+// structs
+// libraries
+// contracts
+// deployments
+import {RiverRegistryBaseSetup} from "contracts/test/river/registry/RiverRegistryBaseSetup.t.sol";
+
 contract StreamRegistryTest is RiverRegistryBaseSetup, IOwnableBase {
   string url = "https://node.com";
 
