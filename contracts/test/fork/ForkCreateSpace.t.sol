8310676aeaed15db68f72c7ef9502ef471799f6b
g <5714678+giuseppecrj@users.noreply.github.com>
2024-06-18 13:26:07 -0400

    airdrop nfts

--- contracts/test/fork/ForkCreateSpace.t.sol
+++ contracts/test/fork/ForkCreateSpace.t.sol
@@ -14,16 +14,10 @@ import {IMembership} from "contracts/src/spaces/facets/membership/IMembership.so
 import {SpaceHelper} from "contracts/test/spaces/SpaceHelper.sol";
 import {Architect} from "contracts/src/factory/facets/architect/Architect.sol";
 
-import {Migration_2024_05_01} from "contracts/scripts/interactions/Migration_2024_05_01.s.sol";
-
 contract ForkCreateSpace is IArchitectBase, TestUtils, SpaceHelper {
-  Migration_2024_05_01 internal migration = new Migration_2024_05_01();
-
   address spaceFactory = 0x968696BC59431Ef085441641f550C8e2Eaca8BEd;
 
-  function setUp() public onlyForked {
-    migration.run();
-  }
+  function setUp() public onlyForked {}
 
   function test_createForkSpace() external onlyForked {
     address founder = _randomAddress();
