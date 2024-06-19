e26b61df56ab35fbcf7bd2179b452007076e20f4
Crystal Lemire <crystal@hntlabs.com>
2024-06-19 10:16:50 -0700

    Revert config changes.

--- packages/sdk/jest.config.ts
+++ packages/sdk/jest.config.ts
@@ -41,7 +41,6 @@ const config: JestConfigWithTsJest = {
     },
     runner: 'groups',
     verbose: true,
-    maxWorkers: 1,
     testTimeout: 120000,
     modulePathIgnorePatterns: ['/dist/'],
     testPathIgnorePatterns: ['/dist/', '/node_modules/', 'util.test.ts', 'setupUrl.test.ts'],
