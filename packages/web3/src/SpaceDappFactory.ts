62fd8e52ebfd600b7f81b9dd32df9787c3c88c7b
Crystal Lemire <crystal@hntlabs.com>
2024-06-20 15:42:41 -0700

    Update log line removal.

--- packages/web3/src/SpaceDappFactory.ts
+++ packages/web3/src/SpaceDappFactory.ts
@@ -10,11 +10,5 @@ export function createSpaceDapp(
     if (provider === undefined) {
         throw new Error('createSpaceDapp() Provider is undefined')
     }
-    // For RPC providers that pool for events, we need to set the polling interval to a lower value
-    // so that we don't miss events that may be emitted in between polling intervals. The Ethers
-    // default is 4000ms, which is based on the assumption of 12s mainnet blocktimes.
-    if ('pollingInterval' in provider && typeof provider.pollingInterval === 'number') {
-        provider.pollingInterval = 250
-    }
     return new SpaceDapp(config, provider)
 }
