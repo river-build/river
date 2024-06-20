c2f0f504ea45b28ec41263ab7f018a69e83bf9d3
texuf <texuf.eth@gmail.com>
2024-06-20 00:07:18 -0700

    Move the polling adjustment in the space dapp to the constructor

--- packages/web3/src/v3/SpaceDapp.ts
+++ packages/web3/src/v3/SpaceDapp.ts
@@ -59,6 +59,15 @@ export class SpaceDapp implements ISpaceDapp {
             config.contractVersion,
             provider,
         )
+
+        // For RPC providers that pool for events, we need to set the polling interval to a lower value
+        // so that we don't miss events that may be emitted in between polling intervals. The Ethers
+        // default is 4000ms, which is based on the assumption of 12s mainnet blocktimes.
+        if ('pollingInterval' in provider && typeof provider.pollingInterval === 'number') {
+            const oldValue = provider.pollingInterval
+            provider.pollingInterval = 250
+            logger.info('pollingInterval was: ', oldValue, 'now: ', provider.pollingInterval)
+        }
     }
 
     public async addRoleToChannel(
