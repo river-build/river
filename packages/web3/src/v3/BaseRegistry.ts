5542dfd4c56c637aca843791c784bcc8bc301996
Kerem Kazan <kerem.kazan@gmail.com>
2024-06-24 15:19:26 -0400

    added base registry methods

--- packages/web3/src/v3/BaseRegistry.ts
+++ packages/web3/src/v3/BaseRegistry.ts
@@ -1,10 +1,14 @@
 import { ethers } from 'ethers'
-
 import { BaseChainConfig } from '../IStaticContractsInfo'
 import { INodeOperatorShim } from './INodeOperatorShim'
 import { IEntitlementCheckerShim } from './IEntitlementCheckerShim'
 import { ISpaceDelegationShim } from './ISpaceDelegationShim'
 import { IERC721AShim } from './IERC721AShim'
+
+export type BaseOperator = {
+    operatorAddress: string
+    status: number
+}
 
 export class BaseRegistry {
     public readonly config: BaseChainConfig
@@ -37,5 +41,49 @@ export class BaseRegistry {
             config.contractVersion,
             provider,
         )
+    }
+
+    private async ownerOf(tokenId: number) {
+        return this.erc721A.read.ownerOf(tokenId)
+    }
+
+    private async getOperatorStatus(operator: string) {
+        return this.nodeOperator.read.getOperatorStatus(operator)
+    }
+
+    async getOperators(): Promise<BaseOperator[]> {
+        const totalSupplyBigInt = await this.erc721A.read.totalSupply()
+        const totalSupply = Number(totalSupplyBigInt)
+        const zeroToTotalSupply = Array.from(Array(totalSupply).keys())
+        const operatorsPromises = zeroToTotalSupply.map((tokenId) => this.ownerOf(tokenId))
+        const operatorAddresses = await Promise.all(operatorsPromises)
+        const operatorStatusPromises = operatorAddresses.map((operatorAddress) =>
+            this.getOperatorStatus(operatorAddress),
+        )
+        const operatorsStatus = await Promise.all(operatorStatusPromises)
+        const operators = operatorAddresses.map((operatorAddress, index) => ({
+            operatorAddress,
+            status: operatorsStatus[index],
+        }))
+
+        return operators
+    }
+
+    private async getNodeCount() {
+        return this.entitlementChecker.read.getNodeCount()
+    }
+
+    private async getNodeAtIndex(index: number) {
+        return this.entitlementChecker.read.getNodeAtIndex(index)
+    }
+
+    public async getNodes() {
+        const nodeCountBigInt = await this.getNodeCount()
+        const nodeCount = Number(nodeCountBigInt)
+        const zeroToNodeCount = Array.from(Array(nodeCount).keys())
+        const nodeAtIndexPromises = zeroToNodeCount.map((index) => this.getNodeAtIndex(index))
+        const nodes = await Promise.all(nodeAtIndexPromises)
+
+        return nodes
     }
 }
