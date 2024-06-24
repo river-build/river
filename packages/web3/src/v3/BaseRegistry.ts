3325d40ce7bae63d9ff414f710f84f67e2db0e1c
Kerem Kazan <kerem.kazan@gmail.com>
2024-06-24 15:07:14 -0400

    created BaseRegistry

---
+++ packages/web3/src/v3/BaseRegistry.ts
@@ -0,0 +1,26 @@
+import { ethers } from 'ethers'
+import { BaseChainConfig } from '../IStaticContractsInfo'
+import { INodeOperatorShim } from './INodeOperatorShim'
+import { IEntitlementCheckerShim } from './IEntitlementCheckerShim'
+
+export class BaseRegistry {
+    public readonly config: BaseChainConfig
+    public readonly provider: ethers.providers.Provider
+    public readonly nodeOperator: INodeOperatorShim
+    public readonly entitlementChecker: IEntitlementCheckerShim
+
+    constructor(config: BaseChainConfig, provider: ethers.providers.Provider) {
+        this.config = config
+        this.provider = provider
+        this.nodeOperator = new INodeOperatorShim(
+            config.addresses.baseRegistry,
+            config.contractVersion,
+            provider,
+        )
+        this.entitlementChecker = new IEntitlementCheckerShim(
+            config.addresses.baseRegistry,
+            config.contractVersion,
+            provider,
+        )
+    }
+}
