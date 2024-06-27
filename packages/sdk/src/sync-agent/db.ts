b966179ddcde045a30259e144b25cf5bc00db96c
texuf <texuf.eth@gmail.com>
2024-06-26 22:09:59 -0700

    SyncAgent - better tests, better db initialization
    cleaning up the chaos before it spreads

--- packages/sdk/src/sync-agent/db.ts
+++ packages/sdk/src/sync-agent/db.ts
@@ -1,4 +1,3 @@
-import { Store } from '../store/store'
 import { StreamNodeUrls } from './river-connection/models/streamNodeUrls'
 import { UserDeviceKeys } from './user/models/userDeviceKeys'
 import { UserInbox } from './user/models/userInbox'
@@ -6,12 +5,12 @@ import { UserMemberships } from './user/models/userMemberships'
 import { UserSettings } from './user/models/userSettings'
 import { User } from './user/user'
 
-const VERSION = 1
-const DB_NAME = (userId: string) => `syncAgent-${userId}`
-const MODELS = [StreamNodeUrls, User, UserDeviceKeys, UserInbox, UserMemberships, UserSettings]
-
-export class SyncAgentStore extends Store {
-    constructor(userId: string) {
-        super(DB_NAME(userId), VERSION, MODELS)
-    }
-}
+export const DB_VERSION = 1
+export const DB_MODELS = [
+    StreamNodeUrls,
+    User,
+    UserDeviceKeys,
+    UserInbox,
+    UserMemberships,
+    UserSettings,
+]
--- packages/sdk/src/sync-agent/syncAgent.test.ts
+++ packages/sdk/src/sync-agent/syncAgent.test.ts
@@ -1,28 +1,19 @@
 /**
  * @group with-entitilements
  */
-import { Wallet } from 'ethers'
-import { makeSignerContext } from '../signerContext'
-import { makeRiverConfig } from '../riverConfig'
-import { SyncAgent } from './syncAgent'
 import { AuthStatus } from './user/user'
 import { dlogger } from '@river-build/dlog'
 import { waitFor } from '../util.test'
-import { LocalhostWeb3Provider } from '@river-build/web3'
 import { MembershipOp } from '@river-build/proto'
+import { TestUser } from './utils/testUser.test'
 
 const logger = dlogger('csb:test:syncAgent')
 
 describe('syncAgent.test.ts', () => {
-    const riverConfig = makeRiverConfig()
-    const rootWallet = Wallet.createRandom()
-    const delegateWallet = Wallet.createRandom()
-    const web3Provider = new LocalhostWeb3Provider(riverConfig.base.rpcUrl, rootWallet)
+    const testUser = new TestUser()
 
     test('syncAgent', async () => {
-        await web3Provider.fundWallet()
-        const signerContext = await makeSignerContext(rootWallet, delegateWallet, { days: 1 })
-        const syncAgent = new SyncAgent({ context: signerContext, riverConfig })
+        const syncAgent = await testUser.makeSyncAgent()
         expect(syncAgent.user.value.status).toBe('loading')
         await syncAgent.start()
         expect(syncAgent.user.value.status).toBe('loaded')
@@ -32,7 +23,7 @@ describe('syncAgent.test.ts', () => {
         syncAgent.store.newTransactionGroup('createSpace')
         const { spaceId, defaultChannelId } = await syncAgent.user.createSpace(
             { spaceName: 'BlastOff' },
-            web3Provider.signer,
+            testUser.signer,
         )
         logger.log('spaceId', spaceId)
         expect(Object.keys(syncAgent.user.streams.memberships.data.memberships).length).toBe(2)
@@ -50,8 +41,7 @@ describe('syncAgent.test.ts', () => {
         await syncAgent.stop()
     })
     test('syncAgent loads again', async () => {
-        const signerContext = await makeSignerContext(rootWallet, delegateWallet, { days: 1 })
-        const syncAgent = new SyncAgent({ context: signerContext, riverConfig })
+        const syncAgent = await testUser.makeSyncAgent()
         expect(syncAgent.user.value.status).toBe('loading')
         await syncAgent.start()
         expect(syncAgent.user.value.status).toBe('loaded')
