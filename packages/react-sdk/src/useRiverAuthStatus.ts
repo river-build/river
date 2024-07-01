211512f1d4eb27e8f156a8e4f95d9ea6177924d8
Miguel Nascimento <miguelgomes13@live.com>
2024-07-01 12:16:41 -0300

    update auth status

--- packages/react-sdk/src/useRiverAuthStatus.ts
+++ packages/react-sdk/src/useRiverAuthStatus.ts
@@ -4,13 +4,15 @@ import { AuthStatus } from '@river-build/sdk'
 import { useRiver } from './useRiver'
 
 export const useRiverAuthStatus = () => {
-    const { data: status } = useRiver((s) => s.userAuthStatus)
+    const { data: status } = useRiver((s) => s.riverAuthStatus)
     return {
         status,
-        isNone: status === AuthStatus.None,
+        isInitializing: status === AuthStatus.Initializing,
         isEvaluatingCredentials: status === AuthStatus.EvaluatingCredentials,
         isCredentialed: status === AuthStatus.Credentialed,
+        isConnectingToRiver: status === AuthStatus.ConnectingToRiver,
         isConnectedToRiver: status === AuthStatus.ConnectedToRiver,
         isDisconnected: status === AuthStatus.Disconnected,
+        isError: status === AuthStatus.Error,
     }
 }
