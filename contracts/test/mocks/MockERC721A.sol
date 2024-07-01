1eb4d4f695a3cf2c98eb6cb0f010a0157b7410f9
Giuseppe Rodriguez <5714678+giuseppecrj@users.noreply.github.com>
2024-07-01 13:53:15 -0400

    Deploy Test NFTS on multiple chains (#299)

--- contracts/test/mocks/MockERC721A.sol
+++ contracts/test/mocks/MockERC721A.sol
@@ -9,6 +9,10 @@ pragma solidity ^0.8.23;
 import {ERC721A} from "contracts/src/diamond/facets/token/ERC721A/ERC721A.sol";
 
 contract MockERC721A is ERC721A {
+  constructor() {
+    __ERC721A_init_unchained("TownsTest", "TNFT");
+  }
+
   function mintTo(address to) external returns (uint256 tokenId) {
     tokenId = _nextTokenId();
     _mint(to, 1);
