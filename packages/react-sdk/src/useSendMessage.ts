61fb684b2d8820ddd28df52fc1113ca7720bd83c
Miguel Nascimento <miguelgomes13@live.com>
2024-07-04 17:54:06 -0300

    action without useAction for now

--- packages/react-sdk/src/useSendMessage.ts
+++ packages/react-sdk/src/useSendMessage.ts
@@ -1,17 +1,54 @@
 'use client'
 
+import type { Channel } from '@river-build/sdk'
+import { useCallback, useState } from 'react'
 import { useCurrentChannelId } from './useChannel'
 import { useCurrentSpaceId } from './useSpace'
-import { type ActionConfig, useAction } from './internals/useAction'
+import { type ActionConfig } from './internals/useAction'
+import { useSyncAgent } from './useSyncAgent'
 
+// TODO: boilerplate that should be reduced with useAction internal hook.
+/// see: https://github.com/river-build/river/pull/326#discussion_r1663381191
 export const useSendMessage = (config: ActionConfig = {}) => {
     const spaceId = useCurrentSpaceId()
     const channelId = useCurrentChannelId()
 
-    const { action, ...rest } = useAction(
-        (sync) => sync.spaces.getSpace(spaceId).getChannel(channelId).sendMessage,
-        config,
-    )
+    const sync = useSyncAgent()
+    const [status, setStatus] = useState<'loading' | 'error' | 'success' | 'idle'>('idle')
+    const [data, setData] = useState<Awaited<ReturnType<Channel['sendMessage']>>>()
+    const [error, setError] = useState<Error | undefined>()
 
-    return { sendMessage: action, ...rest }
+    const action: Channel['sendMessage'] = useCallback(
+        async (...args) => {
+            setStatus('loading')
+            try {
+                const data = await sync.spaces
+                    .getSpace(spaceId)
+                    .getChannel(channelId)
+                    .sendMessage(...args)
+                setData(data)
+                setStatus('success')
+                return data
+            } catch (error: unknown) {
+                setStatus('error')
+                if (error instanceof Error) {
+                    setError(error)
+                    config.onError?.(error)
+                }
+                throw error
+            } finally {
+                setStatus('idle')
+            }
+        },
+        [channelId, config, spaceId, sync.spaces],
+    )
+    return {
+        createSpace: action,
+        status,
+        isPending: status === 'loading',
+        isError: status === 'error',
+        isLoaded: status === 'success',
+        error,
+        data,
+    }
 }
