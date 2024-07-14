6acda7fc9d5e7800f2293203a2e17e4d1d1187ab
Kerem Kazan <kerem.kazan@gmail.com>
2024-07-14 11:03:41 -0400

    added internal health check to the metrics discovery service

--- packages/metrics-discovery/src/index.ts
+++ packages/metrics-discovery/src/index.ts
@@ -3,9 +3,13 @@ import { MetricsDiscovery } from './metrics-discovery'
 import { envVarsSchema } from './env-vars'
 import { sleep } from './utils'
 import { createPrometheusConfig } from './create-prometheus-config'
+import http from 'node:http';
 
 const PROMETHEUS_TARGETS_FILE = './prometheus/etc/targets.json'
 const SLEEP_DURATION_MS = 1000 * 60 * 5 // 5 minutes
+const PORT = 8080
+
+let numWrites = 0
 
 const run = async () => {
     console.info('Creating prometheus config...')
@@ -18,6 +22,21 @@ const run = async () => {
         env: envVars.ENV,
     })
 
+    const server = http.createServer((req, res) => {
+        if (numWrites === 0) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('No prometheus targets written yet\n');
+            return
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Healthy\n');
+        }
+    });
+    
+    server.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}/`);
+    });
+
     for (;;) {
         console.info('Getting prometheus targets...')
         const targets = await metricsDiscovery.getPrometheusTargets()
@@ -25,6 +44,7 @@ const run = async () => {
         await fs.promises.writeFile(PROMETHEUS_TARGETS_FILE, targets, {
             encoding: 'utf8',
         })
+        numWrites++
         console.info(`Prometheus targets written to: ${PROMETHEUS_TARGETS_FILE}`)
         console.info(`Sleeping for ${SLEEP_DURATION_MS} ms...`)
         await sleep(SLEEP_DURATION_MS)
