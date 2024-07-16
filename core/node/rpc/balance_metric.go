793f8b3755dafa3e72e33f7efc82413033c2feb9
Serge Khorun <serge@hntlabs.com>
2024-07-15 23:06:49 -0700

    Add base eth balances to metrics

---
+++ core/node/rpc/balance_metric.go
@@ -0,0 +1,56 @@
+package rpc
+
+import (
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/river-build/river/core/node/crypto"
+)
+
+func (s *Service) initEthBalanceMetrics() {
+	if s.baseChain == nil {
+		return
+	}
+
+	go s.reportBaseEthMetric("node_base_eth", s.wallet.Address)
+
+	nodeRecord, err := s.nodeRegistry.GetNode(s.wallet.Address)
+	if err != nil {
+		s.defaultLogger.Error("Failed to find own node record", "err", err)
+		return
+	}
+
+	go s.reportBaseEthMetric("operator_base_eth", nodeRecord.Operator())
+}
+
+func (s *Service) reportBaseEthMetric(name string, address common.Address) {
+	metric := s.metrics.NewGaugeVecEx(name, "Eth balance of the account on base chain", "address").
+		WithLabelValues(address.Hex())
+
+	// Report once on start
+	balance, err := s.baseChain.Client.BalanceAt(s.serverCtx, address, nil)
+	if err != nil {
+		s.defaultLogger.Error("Unable to retrieve wallet balance from base", "err", err)
+	} else {
+		metric.Set(crypto.WeiToEth(balance))
+	}
+
+	ticker := time.NewTicker(time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-s.serverCtx.Done():
+			return
+		case <-ticker.C:
+			balance, err = s.baseChain.Client.BalanceAt(s.serverCtx, address, nil)
+			if err != nil {
+				s.defaultLogger.Error("Unable to retrieve wallet balance from base", "err", err)
+				continue
+			}
+
+			metric.Set(crypto.WeiToEth(balance))
+		}
+	}
+}
