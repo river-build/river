a558e8fab3f4e2b39f6b21ff569881de883293c6
Crystal Lemire <crystal@hntlabs.com>
2024-07-18 14:36:30 -0700

    lint fix - var rename and clean up error msg.

--- packages/sdk/src/client.ts
+++ packages/sdk/src/client.ts
@@ -1210,12 +1210,12 @@ export class Client
             // is a reply, a reaction, a self-redaction or an edit, it may have Write or ReactReply
             // permissions - any change to an existing message authored by the user is implicitly
             // permitted.
-            const expectedPermission: Permission[] =
+            const expectedPermissions: Permission[] =
                 payload.payload.case === 'reaction'
                     ? [Permission.React, Permission.Write]
                     : [Permission.Write]
             let isEntitled = false
-            for (const permission of expectedPermission) {
+            for (const permission of expectedPermissions) {
                 isEntitled = await this.entitlementsDelegate.isEntitled(
                     stream.view.channelContent.spaceId,
                     streamId,
@@ -1228,7 +1228,9 @@ export class Client
             }
             if (!isEntitled) {
                 throw new Error(
-                    `user is not entitled to add message to channel (requires '${expectedPermission}' permission)`,
+                    `user is not entitled to add message to channel (requires [${expectedPermissions.join(
+                        ',',
+                    )}] permission)`,
                 )
             }
         }
