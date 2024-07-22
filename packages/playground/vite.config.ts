78bf70985dade6865eb8a05d7859069c704a5765
texuf <texuf.eth@gmail.com>
2024-07-22 11:52:05 -0700

    add node polyfils

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 import { default as checker } from 'vite-plugin-checker'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
 
 // https://vitejs.dev/config/
@@ -10,6 +11,7 @@ export default ({ mode }: { mode: string }) => {
     return defineConfig({
         define: {
             'process.env': env,
+            'process.browser': true,
         },
         plugins: [
             checker({
@@ -18,6 +20,7 @@ export default ({ mode }: { mode: string }) => {
                     lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
                 },
             }),
+            nodePolyfills(),
             react(),
         ],
         resolve: {
