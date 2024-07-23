1c14a803413a4cedeaf7c6b344e8474bdd2117c9
Serge Khorun <serge@hntlabs.com>
2024-07-22 21:30:17 -0700

    Unify run_single.sh script to be the same as run_multi.sh

--- core/node/run_single.sh
+++ core/node/run_single.sh
@@ -2,38 +2,20 @@
 set -euo pipefail
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
-export DB_PORT=5433
-export LOG_LEVEL=info
-export LOG_NOCOLOR=false
-export METRICS_ENABLED=true
-export NUM_INSTANCES=1
-export REPL_FACTOR=1
-export RPC_PORT=5157
-export RUN_ENV=single
-export DISABLE_BASE_CHAIN=${DISABLE_BASE_CHAIN:-false}
+export DISABLE_BASE_CHAIN="${DISABLE_BASE_CHAIN:-false}"
+export METRICS_ENABLED="${METRICS_ENABLED:-true}"
+export RUN_ENV="${RUN_ENV:-single}"
+export NUM_INSTANCES="${NUM_INSTANCES:-1}"
+export RPC_PORT="${RPC_PORT:-5157}"
 
 # Parse command-line options
-RUN_OPT="-c -r"
 args=() # Collect arguments to pass to the last command
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
-        -c|--config-only)
-            RUN_OPT="-c"
-            shift
-            ;;
-        -sc|--skip-config)
-            RUN_OPT="-r"
-            shift
-            ;;
-        -r|--run-only) # same as -sc, but everything else uses -r
-            RUN_OPT="-r"
-            shift
-            ;;
         --disable_entitlements|--de)
-            RUN_ENV=single_ne
-            METRICS_ENABLED=false
-            RPC_PORT=5158
             DISABLE_BASE_CHAIN=true
+            RPC_PORT=5158
+            RUN_ENV="single_ne"
             shift
             ;;
         *)
@@ -43,4 +25,4 @@ while [[ "$#" -gt 0 ]]; do
     esac
 done
 
-./run_impl.sh $RUN_OPT "${args[@]:-}"
+./run_impl.sh "${args[@]:-}"
