bde94b9655d60d18e6c603f62a8b739c99c1116c
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-07-25 21:24:08 -0700

    add deriveKeyAndIV test

---
+++ packages/sdk/src/crypto_utils.test.ts
@@ -0,0 +1,23 @@
+import crypto from 'crypto'
+import { deriveKeyAndIV } from './crypto_utils'
+
+describe('crypto_utils', () => {
+    test('derivedKeyAndIV', () => {
+        const spaceId = generateRandomSpaceId()
+
+        const { key: key1, iv: iv1 } = deriveKeyAndIV(spaceId)
+        const { key: key2, iv: iv2 } = deriveKeyAndIV(spaceId)
+
+        // expect the same key and iv to be derived from the same spaceId
+        expect(key1.toString()).toEqual(key2.toString())
+        expect(iv1.toString()).toEqual(iv2.toString())
+    })
+})
+
+function generateRandomSpaceId(): string {
+    // Generate a random 32-byte buffer
+    const buffer = crypto.randomBytes(40)
+
+    // Convert the buffer to a hexadecimal string and prefix with '0x'
+    return '0x' + buffer.toString('hex')
+}
