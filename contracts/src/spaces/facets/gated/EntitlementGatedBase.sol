c8d2ad55bc584211d0ea6ac81a9fa1d927923c2c
Crystal Lemire <crystal@hntlabs.com>
2024-07-26 13:29:19 -0700

    Revert non-logic change.

--- contracts/src/spaces/facets/gated/EntitlementGatedBase.sol
+++ contracts/src/spaces/facets/gated/EntitlementGatedBase.sol
@@ -174,7 +174,6 @@ abstract contract EntitlementGatedBase is IEntitlementGatedBase {
     }
 
     IRuleEntitlement re = IRuleEntitlement(address(transaction.entitlement));
-
     IRuleEntitlement.RuleData memory ruleData = re.getRuleData(roleId);
 
     return ruleData;
