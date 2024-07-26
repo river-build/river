348ddb128883b9e20b8ea031d46d724f3b17465a
texuf <texuf.eth@gmail.com>
2024-07-26 12:12:44 -0700

    use space updates

--- packages/react-sdk/src/useSpace.ts
+++ packages/react-sdk/src/useSpace.ts
@@ -1,24 +1,17 @@
 'use client'
 
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState, useSyncExternalStore } from 'react'
 import { Observable, PersistedModel, SpaceModel } from '@river-build/sdk'
 import { useSyncAgent } from './useSyncAgent'
 
 export const useSpace = (spaceId: string): PersistedModel<SpaceModel> => {
     const sync = useSyncAgent()
-    console.log('useSpace', spaceId)
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_value, setValue] = useState<PersistedModel<SpaceModel>>(
-        sync.spaces.getSpace(spaceId).value,
+    const space = sync.spaces.getSpace(spaceId)
+    const f = useSyncExternalStore(
+        (subscriber: () => void) => {
+            return space.subscribe(subscriber)
+        },
+        () => space.value,
     )
-
-    useEffect(() => {
-        const subFn = (space: PersistedModel<SpaceModel>) => {
-            setValue(space)
-        }
-        const sub = sync.spaces.getSpace(spaceId).subscribe(subFn)
-        return () => sub.unsubscribe(subFn)
-    }, [spaceId, sync.spaces])
-
-    return sync.spaces.getSpace(spaceId).value
+    return f
 }
