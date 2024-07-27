554acecf91e5c25020b515c61926ce27390a9bec
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-07-27 01:19:35 -0700

    perf(sdk): Use `MulticallProvider` for RPC batching
    Integrated `@0xsequence/multicall` to enhance the RPC batching process. This change optimizes RPC calls by leveraging `MulticallProvider` and `StaticJsonRpcProvider`. Updated necessary dependencies and included deployment scripts for `SequenceUtils` contract.

--- packages/sdk/src/sync-agent/utils/providers.ts
+++ packages/sdk/src/sync-agent/utils/providers.ts
@@ -1,18 +1,23 @@
-import { providers } from 'ethers'
+import { providers } from '@0xsequence/multicall'
+import { providers as ethersProviders } from 'ethers'
 import { RiverConfig } from '../../riverConfig'
 
 export function makeRiverProvider(config: RiverConfig) {
     const river = config.river
-    return new providers.StaticJsonRpcProvider(river.rpcUrl, {
-        chainId: river.chainConfig.chainId,
-        name: `river-${river.chainConfig.chainId}`,
-    })
+    return new providers.MulticallProvider(
+        new ethersProviders.StaticJsonRpcProvider(river.rpcUrl, {
+            chainId: river.chainConfig.chainId,
+            name: `river-${river.chainConfig.chainId}`,
+        }),
+    )
 }
 
 export function makeBaseProvider(config: RiverConfig) {
     const base = config.base
-    return new providers.StaticJsonRpcProvider(base.rpcUrl, {
-        chainId: base.chainConfig.chainId,
-        name: `base-${base.chainConfig.chainId}`,
-    })
+    return new providers.MulticallProvider(
+        new ethersProviders.StaticJsonRpcProvider(base.rpcUrl, {
+            chainId: base.chainConfig.chainId,
+            name: `base-${base.chainConfig.chainId}`,
+        }),
+    )
 }
