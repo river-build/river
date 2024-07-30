3aa6e4f93e4162770ed5604ef80903b0b40ec156
g <5714678+giuseppecrj@users.noreply.github.com>
2024-07-30 17:14:52 -0400

    add more revert tests

--- contracts/test/spaces/roles/setChannelPermissionOverrides.t.sol
+++ contracts/test/spaces/roles/setChannelPermissionOverrides.t.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.23;
 // libraries
 
 import {Permissions} from "contracts/src/spaces/facets/Permissions.sol";
+import {ChannelService__ChannelDoesNotExist} from "contracts/src/spaces/facets/channels/ChannelService.sol";
 
 // contracts
 import {RolesBaseSetup} from "contracts/test/spaces/roles/RolesBaseSetup.sol";
@@ -35,5 +36,44 @@ contract RolesTest_SetChannelPermissionsOverrides is RolesBaseSetup {
 
     assertEq(channelPermissions.length, 1);
     assertEq(channelPermissions[0], permissions[0]);
+  }
+
+  function test_revertWhen_setChannelPermissionOverrideInvalidPermission()
+    external
+  {
+    string[] memory permissions = new string[](1);
+    permissions[0] = Permissions.Read;
+
+    vm.prank(_randomAddress());
+    vm.expectRevert(Entitlement__NotAllowed.selector);
+    roles.setChannelPermissionOverrides(ROLE_ID, CHANNEL_ID, permissions);
+  }
+
+  function test_revertWhen_setChannelPermissionOverrideChannelDoesNotExist()
+    external
+  {
+    string[] memory permissions = new string[](1);
+    permissions[0] = Permissions.Read;
+
+    vm.prank(founder);
+    vm.expectRevert(ChannelService__ChannelDoesNotExist.selector);
+    roles.setChannelPermissionOverrides(ROLE_ID, CHANNEL_ID, permissions);
+  }
+
+  function test_revertWhen_setChannelPermissionOverrideRoleDoesNotExist()
+    external
+    givenRoleExists
+    givenRoleIsInChannel
+  {
+    string[] memory permissions = new string[](1);
+    permissions[0] = Permissions.Read;
+
+    vm.prank(founder);
+    vm.expectRevert(Roles__RoleDoesNotExist.selector);
+    roles.setChannelPermissionOverrides(
+      _randomUint256(),
+      CHANNEL_ID,
+      permissions
+    );
   }
 }
