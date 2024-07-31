630751b5c7d30b8624800a383a3682a19356555b
Miguel Nascimento <miguelgomes13@live.com>
2024-07-31 18:22:14 -0300

    Revert "stress: catch and log some errors"
    This reverts commit ba1a8d3e609e6689b30d412793a3da29031ba95a.

--- packages/stress/src/mode/chat/joinChat.ts
+++ packages/stress/src/mode/chat/joinChat.ts
@@ -27,18 +27,9 @@ export async function joinChat(client: StressClient, cfg: ChatConfig) {
 
     const announceChannelId = cfg.announceChannelId
     // start up the client
-    await startFollowerClient(client, cfg.spaceId, announceChannelId).catch((e) =>
-        logger.error('startFollowerClient', e),
-    )
+    await startFollowerClient(client, cfg.spaceId, announceChannelId)
 
-    const announceChannel = await client.streamsClient
-        .waitForStream(announceChannelId)
-        .catch((e) => logger.error('waitForStream announceChannel', e))
-
-    if (!announceChannel) {
-        throw new Error('announceChannel not found')
-    }
-
+    const announceChannel = await client.streamsClient.waitForStream(announceChannelId)
     let count = 0
     const message = await client.waitFor(
         () => {
