b1deec5001ee8a126630fbb708f54ffda6ffb57c
Serge Khorun <104387024+sergekh2@users.noreply.github.com>
2024-08-07 17:04:12 -0700

    Log sync start/exit, add syncId to callstacks (#673)
    While service doesn't lock up anymore (#640), notification service still
    fails to receive all expected data.

--- core/node/rpc/service_sync_streams.go
+++ core/node/rpc/service_sync_streams.go
@@ -2,6 +2,9 @@ package rpc
 
 import (
 	"context"
+	"encoding/binary"
+	"encoding/hex"
+	"time"
 
 	"connectrpc.com/connect"
 
@@ -10,15 +13,37 @@ import (
 	"github.com/river-build/river/core/node/utils"
 )
 
+func magicFromString(s string) (uint64, string) {
+	bb := []byte(s)
+	if len(bb) < 8 {
+		padded := make([]byte, 8)
+		copy(padded, bb)
+		bb = padded
+	}
+	return binary.BigEndian.Uint64(bb), hex.EncodeToString(bb)
+}
+
 func (s *Service) SyncStreams(
 	ctx context.Context,
 	req *connect.Request[SyncStreamsRequest],
 	res *connect.ServerStream[SyncStreamsResponse],
 ) error {
 	ctx, log := utils.CtxAndLogForRequest(ctx, req)
-	syncId, err := s.syncHandler.SyncStreams(ctx, req, res)
+	startTime := time.Now()
+	syncId := GenNanoid()
+	magic, hex := magicFromString(syncId)
+	log.Info("SyncStreams START", "syncId", syncId, "magic", hex)
+
+	err := s.syncHandler.SyncStreams(magic, ctx, syncId, req, res)
 	if err != nil {
-		err = AsRiverError(err).Func("SyncStreams").Tag("syncId", syncId).LogWarn(log).AsConnectError()
+		err = AsRiverError(
+			err,
+		).Func("SyncStreams").
+			Tags("syncId", syncId, "duration", time.Since(startTime), "magic", hex).
+			LogWarn(log).
+			AsConnectError()
+	} else {
+		log.Info("SyncStreams DONE", "syncId", syncId, "duration", time.Since(startTime), "magic", hex)
 	}
 	return err
 }
@@ -28,12 +53,13 @@ func (s *Service) AddStreamToSync(
 	req *connect.Request[AddStreamToSyncRequest],
 ) (*connect.Response[AddStreamToSyncResponse], error) {
 	ctx, log := utils.CtxAndLogForRequest(ctx, req)
-	res, err := s.syncHandler.AddStreamToSync(ctx, req)
+	magic, hex := magicFromString(req.Msg.GetSyncId())
+	res, err := s.syncHandler.AddStreamToSync(magic, ctx, req)
 	if err != nil {
 		err = AsRiverError(
 			err,
 		).Func("AddStreamToSync").
-			Tags("syncId", req.Msg.GetSyncId(), "streamId", req.Msg.GetSyncPos().GetStreamId()).
+			Tags("syncId", req.Msg.GetSyncId(), "streamId", req.Msg.GetSyncPos().GetStreamId(), "magic", hex).
 			LogWarn(log).
 			AsConnectError()
 	}
@@ -45,12 +71,13 @@ func (s *Service) RemoveStreamFromSync(
 	req *connect.Request[RemoveStreamFromSyncRequest],
 ) (*connect.Response[RemoveStreamFromSyncResponse], error) {
 	ctx, log := utils.CtxAndLogForRequest(ctx, req)
-	res, err := s.syncHandler.RemoveStreamFromSync(ctx, req)
+	magic, hex := magicFromString(req.Msg.GetSyncId())
+	res, err := s.syncHandler.RemoveStreamFromSync(magic, ctx, req)
 	if err != nil {
 		err = AsRiverError(
 			err,
 		).Func("RemoveStreamFromSync").
-			Tags("syncId", req.Msg.GetSyncId(), "streamId", req.Msg.GetStreamId()).
+			Tags("syncId", req.Msg.GetSyncId(), "streamId", req.Msg.GetStreamId(), "magic", hex).
 			LogWarn(log).
 			AsConnectError()
 	}
@@ -62,12 +89,13 @@ func (s *Service) CancelSync(
 	req *connect.Request[CancelSyncRequest],
 ) (*connect.Response[CancelSyncResponse], error) {
 	ctx, log := utils.CtxAndLogForRequest(ctx, req)
-	res, err := s.syncHandler.CancelSync(ctx, req)
+	magic, hex := magicFromString(req.Msg.GetSyncId())
+	res, err := s.syncHandler.CancelSync(magic, ctx, req)
 	if err != nil {
 		err = AsRiverError(
 			err,
 		).Func("CancelSync").
-			Tag("syncId", req.Msg.GetSyncId()).
+			Tags("syncId", req.Msg.GetSyncId(), "magic", hex).
 			LogWarn(log).
 			AsConnectError()
 	}
@@ -79,12 +107,13 @@ func (s *Service) PingSync(
 	req *connect.Request[PingSyncRequest],
 ) (*connect.Response[PingSyncResponse], error) {
 	ctx, log := utils.CtxAndLogForRequest(ctx, req)
-	res, err := s.syncHandler.PingSync(ctx, req)
+	magic, hex := magicFromString(req.Msg.GetSyncId())
+	res, err := s.syncHandler.PingSync(magic, ctx, req)
 	if err != nil {
 		err = AsRiverError(
 			err,
 		).Func("PingSync").
-			Tag("syncId", req.Msg.GetSyncId()).
+			Tags("syncId", req.Msg.GetSyncId(), "magic", hex).
 			LogWarn(log).
 			AsConnectError()
 	}
