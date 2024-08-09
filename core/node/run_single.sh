c7a17001fec973de8a1a08d107849d2953672783
Serge Khorun <serge@hntlabs.com>
2024-08-09 15:27:36 -0700

    Move local env vars to explicit separate files

--- core/node/run_single.sh
+++ core/node/run_single.sh
@@ -2,19 +2,13 @@
 set -euo pipefail
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
-export DISABLE_BASE_CHAIN="${DISABLE_BASE_CHAIN:-false}"
-export RUN_ENV="${RUN_ENV:-single}"
-export NUM_INSTANCES="${NUM_INSTANCES:-1}"
-export RPC_PORT="${RPC_PORT:-5157}"
-export RIVER_BLOCK_TIME="${RIVER_BLOCK_TIME:-1}"
+export RUN_ENV="single"
 
 # Parse command-line options
 args=() # Collect arguments to pass to the last command
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
         --disable_entitlements|--de)
-            DISABLE_BASE_CHAIN=true
-            RPC_PORT=5158
             RUN_ENV="single_ne"
             shift
             ;;
