8111838c4db96c6a5b435668fc6f80e7d24cfb17
g <5714678+giuseppecrj@users.noreply.github.com>
2024-08-13 11:20:50 -0400

    initial script for alpha space deploy

---
+++ contracts/scripts/interactions/AlphaSpaceDeploy.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+// interfaces
+
+// libraries
+
+// contracts
+import {Interaction} from "contracts/scripts/common/Interaction.s.sol";
+
+contract AlphaSpaceDeploy is Interaction {
+  function __interact(address deployer) public override {}
+}
