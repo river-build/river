7211f12e3923f686d905d8875926aaf3af33e235
Brian Meek <brian@hntlabs.com>
2024-08-14 14:34:08 -0700

    Remove vacuum steps as they need to be run outside of the migration
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- core/node/storage/migrations/000006_vacuum.up.sql
+++
@@ -1,6 +0,0 @@
--- This must be done in a seperate file to avoid transactions
--- Apply changes to existing data by performing VACUUM FULL
-VACUUM FULL miniblock_candidates;
-VACUUM FULL es;
-VACUUM FULL miniblocks;
-VACUUM FULL minipools;
