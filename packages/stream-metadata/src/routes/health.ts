c70c9fc1d6deeff5371740a33d9d0c5736e1634d
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-08-15 08:17:05 -0700

    [stream-metadata service] re-org folder structure, code clean up and setup server function (#776)
    Re-organizing folders, cleaning up code and
    function names + arguments so that tests can pass in test args.
    
    ---------
    
    Co-authored-by: Kerem Kazan <kerem.kazan@gmail.com>

--- packages/stream-metadata/src/routes/health.ts
+++ packages/stream-metadata/src/routes/health.ts
@@ -1,20 +1,14 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
-import { Config } from './environment'
-import { getLogger } from './logger'
-import { getRiverRegistry } from './evmRpcClient'
+import { getLogger } from '../logger'
+import { getRiverRegistry } from '../evmRpcClient'
 
 const logger = getLogger('handleHealthCheckRequest')
 
-export async function handleHealthCheckRequest(
-	config: Config,
-	request: FastifyRequest,
-	reply: FastifyReply,
-) {
+export async function checkHealth(request: FastifyRequest, reply: FastifyReply) {
 	// Do a health check on the river registry
 	try {
-		const riverRegistry = getRiverRegistry(config)
-		await riverRegistry.getAllNodes()
+		await getRiverRegistry().getAllNodes()
 		// healthy
 		return reply.code(200).send({ status: 'ok' })
 	} catch (e) {
