82121e96b9e21d09c125e725247abc11696fd3eb
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-08-16 10:33:14 -0700

    RIVER_ROOT

--- scripts/start-stream-metadata-service.sh
+++ scripts/start-stream-metadata-service.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-PROJECT_ROOT=$(basename $(git rev-parse --show-toplevel))
-
-if [ "$PROJECT_ROOT" == "river" ]; then
-    RUN_DIR=$(git rev-parse --show-toplevel)/packages/stream-metadata
+# Set RIVER_ROOT based on whether an argument is provided
+if [ -n "$1" ]; then
+    RIVER_ROOT="$1"
 else
-    RUN_DIR=$(git rev-parse --show-toplevel)/river/packages/stream-metadata
+    # Determine the Git project root
+    RIVER_ROOT=$(git rev-parse --show-toplevel)
 fi
 
-echo "Starting stream-metadata service in $RUN_DIR"
+RUN_DIR="$RIVER_ROOT/packages/stream-metadata"
 
-pushd $RUN_DIR
+echo "Starting stream-metadata service in $RUN_DIR"
+pushd "$RUN_DIR"
+
 cp ./.env.local.sample .env.local
 yarn dev
