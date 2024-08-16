1d3293ef1a6dbbc8df5bee7a28087f1cef6b1167
texuf <texuf.eth@gmail.com>
2024-08-15 22:34:27 -0700

    chore(sdk) remove unused transport type on makeStreamRpcClient
    this was lingering from the very first implementation, it gets in the way when we want to override it with an http2 client in node.

--- packages/sdk/src/makeStreamRpcClient.ts
+++ packages/sdk/src/makeStreamRpcClient.ts
@@ -9,41 +9,35 @@ const logInfo = dlog('csb:rpc:info')
 let nextRpcClientNum = 0
 
 export type StreamRpcClient = PromiseClient<typeof StreamService> & { url?: string }
+export type MakeRpcClientType = typeof makeStreamRpcClient
 
 export function makeStreamRpcClient(
-    dest: Transport | string,
+    dest: string,
     retryParams: RetryParams = { maxAttempts: 3, initialRetryDelay: 2000, maxRetryDelay: 6000 },
     refreshNodeUrl?: () => Promise<string>,
 ): StreamRpcClient {
     const transportId = nextRpcClientNum++
     logInfo('makeStreamRpcClient, transportId =', transportId)
-    let transport: Transport
-    let url: string | undefined
-    if (typeof dest === 'string') {
-        url = randomUrlSelector(dest)
-        logInfo('makeStreamRpcClient: Connecting to url=', url, ' allUrls=', dest)
-        const options: ConnectTransportOptions = {
-            baseUrl: url,
-            interceptors: [
-                retryInterceptor({ ...retryParams, refreshNodeUrl }),
-                loggingInterceptor(transportId),
-            ],
-        }
-        if (getEnvVar('RIVER_DEBUG_TRANSPORT') !== 'true') {
-            options.useBinaryFormat = true
-        } else {
-            logInfo('makeStreamRpcClient: running in debug mode, using JSON format')
-            options.useBinaryFormat = false
-            options.jsonOptions = {
-                emitDefaultValues: true,
-                useProtoFieldName: true,
-            }
-        }
-        transport = createConnectTransport(options)
-    } else {
-        logInfo('makeStreamRpcClient: Connecting to provided transport')
-        transport = dest
+    const url = randomUrlSelector(dest)
+    logInfo('makeStreamRpcClient: Connecting to url=', url, ' allUrls=', dest)
+    const options: ConnectTransportOptions = {
+        baseUrl: url,
+        interceptors: [
+            retryInterceptor({ ...retryParams, refreshNodeUrl }),
+            loggingInterceptor(transportId),
+        ],
     }
+    if (getEnvVar('RIVER_DEBUG_TRANSPORT') !== 'true') {
+        options.useBinaryFormat = true
+    } else {
+        logInfo('makeStreamRpcClient: running in debug mode, using JSON format')
+        options.useBinaryFormat = false
+        options.jsonOptions = {
+            emitDefaultValues: true,
+            useProtoFieldName: true,
+        }
+    }
+    const transport = createConnectTransport(options)
 
     const client: StreamRpcClient = createPromiseClient(StreamService, transport) as StreamRpcClient
     client.url = url
