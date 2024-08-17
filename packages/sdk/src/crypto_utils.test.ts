097c07f96b886b0a37e88d395f1b4f852af344ec
Erik Olsson <valross2@gmail.com>
2024-08-17 23:46:43 +0200

    cleanup

--- packages/sdk/src/crypto_utils.test.ts
+++ packages/sdk/src/crypto_utils.test.ts
@@ -19,13 +19,13 @@ describe('crypto_utils', () => {
 
     test('aesGcmEncryption', async () => {
         const keyPhrase = '0xaabbccddeeff00112233445566778899'
-        const { key: key1, iv: iv1 } = await deriveKeyAndIV(keyPhrase)
+        const { key, iv } = await deriveKeyAndIV(keyPhrase)
 
-        const data = new Uint8Array([
+        const plaintext = new Uint8Array([
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
         ])
 
-        const { ciphertext } = await encryptAESGCM(data, key1, iv1)
+        const { ciphertext } = await encryptAESGCM(plaintext, key, iv)
         expect(ciphertext).toEqual(
             new Uint8Array([
                 241, 174, 242, 10, 73, 18, 35, 179, 216, 45, 231, 145, 130, 224, 207, 196, 199, 179,
