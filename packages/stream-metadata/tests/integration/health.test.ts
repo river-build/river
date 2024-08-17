f1f9b65e389749f25118e2344fed7331a70fe642
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-08-16 18:35:44 -0700

    /health 200 passing

--- packages/stream-metadata/tests/integration/health.test.ts
+++ packages/stream-metadata/tests/integration/health.test.ts
@@ -1,8 +1,9 @@
 import Fastify from 'fastify'
 
+import { config } from '../../src/environment'
 import { setupRoutes, Server } from '../../src/node'
 import { getLogger } from '../../src/logger'
-import pino from 'pino'
+import * as healthModule from '../../src/routes/health'
 
 const logger = getLogger('stream-metadata:tests:integration:health')
 
@@ -14,7 +15,7 @@ describe('GET /health Integration Test', () => {
 			logger,
 		})
 		setupRoutes(server)
-		await server.listen({ port: 0 }) // Listen on a random available port
+		await server.listen({ port: config.port }) // Listen on a random available port
 	})
 
 	afterAll(async () => {
@@ -29,19 +30,5 @@ describe('GET /health Integration Test', () => {
 
 		expect(response.statusCode).toBe(200)
 		expect(response.json()).toEqual({ status: 'ok' })
-	})
-
-	it('should return status 500 if there is an issue with the server', async () => {
-		// Simulate a server issue by altering the server's internal state
-		// For example, you could stop a required service or database connection if needed
-
-		const response = await server.inject({
-			method: 'GET',
-			url: '/health',
-		})
-
-		// Adjust this to the actual behavior when the server is unhealthy
-		expect(response.statusCode).toBe(500)
-		expect(response.json()).toEqual({ status: 'error' })
 	})
 })
