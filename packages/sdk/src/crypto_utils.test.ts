02ef11376ffaee1c191da93e9c0765df37d8cae2
Erik Olsson <valross2@gmail.com>
2024-08-17 23:43:31 +0200

    add non-random aes-gcm encryption test

--- packages/sdk/src/crypto_utils.test.ts
+++ packages/sdk/src/crypto_utils.test.ts
@@ -16,6 +16,24 @@ describe('crypto_utils', () => {
         expect(key1.toString()).toEqual(key2.toString())
         expect(iv1.toString()).toEqual(iv2.toString())
     })
+
+    test('aesGcmEncryption', async () => {
+        const keyPhrase = '0xaabbccddeeff00112233445566778899'
+        const { key: key1, iv: iv1 } = await deriveKeyAndIV(keyPhrase)
+
+        const data = new Uint8Array([
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ])
+
+        const { ciphertext } = await encryptAESGCM(data, key1, iv1)
+        expect(ciphertext).toEqual(
+            new Uint8Array([
+                241, 174, 242, 10, 73, 18, 35, 179, 216, 45, 231, 145, 130, 224, 207, 196, 199, 179,
+                167, 153, 119, 212, 159, 228, 232, 30, 108, 251, 1, 165, 140, 231, 68, 57, 22, 5,
+                121,
+            ]),
+        )
+    })
 })
 
 function generateRandomSpaceId(): string {
