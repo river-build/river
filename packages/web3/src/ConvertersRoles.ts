2b5c5d8e349ebbb0941021e687e427b641fb5e29
Crystal Lemire <crystal@hntlabs.com>
2024-08-19 16:41:37 -0700

    sp.

--- packages/web3/src/ConvertersRoles.ts
+++ packages/web3/src/ConvertersRoles.ts
@@ -29,10 +29,10 @@ export async function createLegacyEntitlementStruct<Space extends SpaceV3>(
         }
     }
     if (!userEntitlementAddress) {
-        throw new Error('User entitlement moodule address not found.')
+        throw new Error('User entitlement module address not found.')
     }
     if (!ruleEntitlementAddress) {
-        throw new Error('Rule entitlement moodule address not found.')
+        throw new Error('Rule entitlement module address not found.')
     }
 
     // create the entitlements array
