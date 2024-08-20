798ded29933d18b02bfdc099779401977533f948
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-08-20 10:39:24 -0700

    fix getTestServer to use riverEnv to determine the server's protocol

--- packages/stream-metadata/tests/testUtils.ts
+++ packages/stream-metadata/tests/testUtils.ts
@@ -10,9 +10,8 @@ export function isTest(): boolean {
 
 export function getTestServerInfo() {
 	// use the .env.test config to derive the baseURL of the server under test
-	const { host, port } = config
-	const protocol =
-		host === 'localhost' || host === '127.0.0.1' || host === '0.0.0.0' ? 'http' : 'https'
+	const { host, port, riverEnv } = config
+	const protocol = riverEnv.startsWith('localhost') ? 'http' : 'https'
 	const baseURL = `${protocol}://${host}:${port}`
 	return baseURL
 }
