c75cadc8e5f4dae02813d660a9efaca8ad09fbf0
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-08-19 17:48:31 -0700

    yarn lint passes

--- packages/stream-metadata/jest.config.ts
+++ packages/stream-metadata/jest.config.ts
@@ -1,26 +1,24 @@
-import { resolve } from 'path';
-import type { Config } from '@jest/types';
+import { resolve } from 'path'
+import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts'],
+	preset: 'ts-jest/presets/default-esm',
+	extensionsToTreatAsEsm: ['.ts'],
 	modulePathIgnorePatterns: ['/dist/'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-    '@matrix-org/olm/olm.wasm': require.resolve('./tests/__mocks__/mock-wasm-file.js'),
-  },
+	moduleNameMapper: {
+		'^(\\.{1,2}/.*)\\.js$': '$1',
+		'@matrix-org/olm/olm.wasm': require.resolve('./tests/__mocks__/mock-wasm-file.js'),
+	},
 	setupFilesAfterEnv: [resolve('./jest.setup.ts')],
-  testEnvironment: 'node',
+	testEnvironment: 'node',
 	testPathIgnorePatterns: ['/dist/', '/node_modules/'],
-	testTimeout: 10000, // Set global timeout of 10 seconds for all tests
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true }],
-    '^.+\\.wasm$': 'jest-transform-stub',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!@river-build)',
-  ],
+	testTimeout: 10000,
+	transform: {
+		'^.+\\.ts$': ['ts-jest', { useESM: true }],
+		'^.+\\.wasm$': 'jest-transform-stub',
+	},
+	transformIgnorePatterns: ['/node_modules/(?!@river-build)'],
 	verbose: true,
-};
+}
 
 export default config
