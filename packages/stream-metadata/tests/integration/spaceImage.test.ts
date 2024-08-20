df478bedf2336de5d589a5be99756bd7ee5c00a2
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-08-20 10:01:35 -0700

    expected failure http status

--- packages/stream-metadata/tests/integration/spaceImage.test.ts
+++ packages/stream-metadata/tests/integration/spaceImage.test.ts
@@ -1,19 +1,51 @@
 import axios from 'axios'
-import { makeTestClient } from '@river-build/sdk'
 
 import { getTestServerInfo } from '../testUtils'
 
 describe('GET /space/:spaceAddress/image', () => {
 	const baseURL = getTestServerInfo()
 
-	it('should return status 404 without spaceAddress', async () => {
+	it('should return 404 /space/0x0000000000000000000000000000000000000000/image', async () => {
+		const expectedStatus = 404
 		try {
 			await axios.get(`${baseURL}/space`)
-			throw new Error('Expected request to fail with status 404')
+			throw new Error(`Expected request to fail with status ${expectedStatus})`)
 		} catch (error) {
 			if (axios.isAxiosError(error)) {
 				expect(error.response).toBeDefined()
-				expect(error.response?.status).toBe(404)
+				expect(error.response?.status).toBe(expectedStatus)
+			} else {
+				// If the error is not an Axios error, rethrow it
+				throw error
+			}
+		}
+	})
+
+	it('should return 400 /space', async () => {
+		const expectedStatus = 400
+		try {
+			await axios.get(`${baseURL}/space`)
+			throw new Error(`Expected request to fail with status ${expectedStatus})`)
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				expect(error.response).toBeDefined()
+				expect(error.response?.status).toBe(expectedStatus)
+			} else {
+				// If the error is not an Axios error, rethrow it
+				throw error
+			}
+		}
+	})
+
+	it('should return 400 /space/0x0/image', async () => {
+		const expectedStatus = 400
+		try {
+			await axios.get(`${baseURL}/space`)
+			throw new Error(`Expected request to fail with status ${expectedStatus})`)
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				expect(error.response).toBeDefined()
+				expect(error.response?.status).toBe(expectedStatus)
 			} else {
 				// If the error is not an Axios error, rethrow it
 				throw error
