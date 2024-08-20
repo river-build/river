e926b2ac5d6f3dae3d08a01656add8907e28d7f1
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2024-08-19 18:32:25 -0700

    /space without arguments should throw error

---
+++ packages/stream-metadata/tests/integration/spaceImage.test.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosError } from 'axios'
+
+import { getTestServerInfo } from '../testUtils'
+
+describe('GET /spaceImage', () => {
+	const baseURL = getTestServerInfo()
+
+	it('should return status 404 without arguments', async () => {
+		try {
+			await axios.get(`${baseURL}/space`)
+			throw new Error('Expected request to fail with status 404')
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				expect(error.response).toBeDefined()
+				expect(error.response?.status).toBe(404)
+			} else {
+				// If the error is not an Axios error, rethrow it
+				throw error
+			}
+		}
+	})
+})
