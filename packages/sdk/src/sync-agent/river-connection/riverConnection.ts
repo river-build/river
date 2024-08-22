bae70584dcf4a911e3a46bc061fa7ca2d5a4a87d
texuf <texuf.eth@gmail.com>
2024-08-22 16:56:56 -0700

    actually 5k

--- packages/sdk/src/sync-agent/river-connection/riverConnection.ts
+++ packages/sdk/src/sync-agent/river-connection/riverConnection.ts
@@ -161,7 +161,7 @@ export class RiverConnection extends PersistedObservable<RiverConnectionModel> {
             this.clientParams.logNamespaceFilter,
             this.clientParams.highPriorityStreamIds,
         )
-        client.setMaxListeners(500)
+        client.setMaxListeners(5000)
         this.client = client
         // initialize views
         this.store.withTransaction('RiverConnection::onNewClient', () => {
