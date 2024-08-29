5ca872621c81c7936ab94ad5431cfb16d37ab2a2
John Terzis <john@hntlabs.com>
2024-08-29 15:04:37 -0700

    add positional arg support to cobra run for pairwise envrionmnet diff

--- scripts/bytecode-diff/main.go
+++ scripts/bytecode-diff/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"bytecode-diff/utils"
+	u "bytecode-diff/utils"
 
 	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
@@ -16,17 +16,81 @@ func main() {
 		log.Println("No .env file found")
 	}
 
+	supportedEnvironments := []string{"alpha", "gamma", "omega"}
 	var rpcURL string
 	var facetSourcePath string
 	var compiledFacetsPath string
 	var sourceDiffDir string
 	var sourceDiff bool
 	var reportOutDir string
+	var originEnvironment, targetEnvironment string
 
 	rootCmd := &cobra.Command{
-		Use:   "bytecode-diff",
+		Use:   "bytecode-diff [origin_environment] [target_environment]",
 		Short: "A tool to retrieve and display contract bytecode diff for Base",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if sourceDiff {
+				if len(args) != 0 {
+					return fmt.Errorf("no positional arguments expected when --source-diff-only is set")
+				}
+			} else {
+				if len(args) != 2 {
+					return fmt.Errorf("exactly two arguments required when --source-diff-only is not set, [origin_environment], [target_environment]")
+				}
+			}
+			return nil
+		},
+		PreRun: func(cmd *cobra.Command, args []string) {
+			if sourceDiff {
+				envSourceDiffDir := os.Getenv("SOURCE_DIFF_DIR")
+				if envSourceDiffDir != "" {
+					sourceDiffDir = envSourceDiffDir
+				}
+
+				if sourceDiffDir == "" {
+					sourceDiffDir = cmd.Flag("source-diff-log").Value.String()
+				}
+
+				facetSourcePath = os.Getenv("FACET_SOURCE_PATH")
+				if facetSourcePath == "" {
+					facetSourcePath = cmd.Flag("facets").Value.String()
+				}
+				if facetSourcePath == "" {
+					log.Fatal("Facet source path is missing. Set it using --facets flag or FACET_SOURCE_PATH environment variable")
+				}
+
+				compiledFacetsPath = os.Getenv("COMPILED_FACETS_PATH")
+				fmt.Println("Compiled facets path:", compiledFacetsPath)
+				if compiledFacetsPath == "" {
+					compiledFacetsPath = cmd.Flag("compiled-facets").Value.String()
+				}
+				if compiledFacetsPath == "" {
+					log.Fatal("Compiled facets path is missing. Set it using --compiled-facets flag or COMPILED_FACETS_PATH environment variable")
+				}
+
+				envReportOutDir := os.Getenv("REPORT_OUT_DIR")
+				if envReportOutDir != "" {
+					reportOutDir = envReportOutDir
+				}
+				if reportOutDir == "" {
+					reportOutDir = cmd.Flag("report-out-dir").Value.String()
+				}
+				if reportOutDir == "" {
+					log.Fatal("Report out directory is missing. Set it using --report-out-dir flag or REPORT_OUT_DIR environment variable")
+				}
+			}
+		},
 		Run: func(cmd *cobra.Command, args []string) {
+			if !sourceDiff {
+				originEnvironment, targetEnvironment = args[0], args[1]
+				for _, environment := range []string{originEnvironment, targetEnvironment} {
+					if !u.Contains(supportedEnvironments, environment) {
+						log.Fatalf("Environment %s not supported. Environment can be one of alpha, gamma, or omega.", environment)
+					}
+				}
+				fmt.Printf("Origin Environment: %s, Target Environment: %s\n", originEnvironment, targetEnvironment)
+			}
+
 			if rpcURL == "" {
 				rpcURL = os.Getenv("BASE_RPC_URL")
 				if rpcURL == "" {
@@ -50,49 +114,6 @@ func main() {
 	rootCmd.Flags().BoolP("verbose", "v", false, "Enable verbose output")
 	rootCmd.Flags().StringVar(&reportOutDir, "report-out-dir", "deployed-diffs", "Path to report output directory")
 
-	rootCmd.PreRun = func(cmd *cobra.Command, args []string) {
-		if sourceDiff {
-			envSourceDiffDir := os.Getenv("SOURCE_DIFF_DIR")
-			if envSourceDiffDir != "" {
-				sourceDiffDir = envSourceDiffDir
-			}
-
-			if sourceDiffDir == "" {
-				sourceDiffDir = cmd.Flag("source-diff-log").Value.String()
-			}
-
-			facetSourcePath = os.Getenv("FACET_SOURCE_PATH")
-			if facetSourcePath == "" {
-				facetSourcePath = cmd.Flag("facets").Value.String()
-			}
-			if facetSourcePath == "" {
-				log.Fatal("Facet source path is missing. Set it using --facets flag or FACET_SOURCE_PATH environment variable")
-			}
-
-			compiledFacetsPath = os.Getenv("COMPILED_FACETS_PATH")
-			fmt.Println("Compiled facets path:", compiledFacetsPath)
-			if compiledFacetsPath == "" {
-				compiledFacetsPath = cmd.Flag("compiled-facets").Value.String()
-			}
-			if compiledFacetsPath == "" {
-				log.Fatal("Compiled facets path is missing. Set it using --compiled-facets flag " +
-					"or COMPILED_FACETS_PATH environment variable")
-			}
-
-			envReportOutDir := os.Getenv("REPORT_OUT_DIR")
-			if envReportOutDir != "" {
-				reportOutDir = envReportOutDir
-			}
-			if reportOutDir == "" {
-				reportOutDir = cmd.Flag("report-out-dir").Value.String()
-			}
-			if reportOutDir == "" {
-				log.Fatal("Report out directory is missing. Set it using --report-out-dir flag " +
-					"or REPORT_OUT_DIR environment variable")
-			}
-		}
-	}
-
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -101,13 +122,15 @@ func main() {
 }
 
 func executeSourceDiff(cmd *cobra.Command, facetSourcePath, compiledFacetsPath string, reportOutDir string) error {
-	facetFiles, err := utils.GetFacetFiles(facetSourcePath)
+	facetFiles, err := u.GetFacetFiles(facetSourcePath)
 	if err != nil {
+		fmt.Println("facetSourcePath", facetSourcePath)
+		fmt.Println("compiledFacetsPath", compiledFacetsPath)
 		return fmt.Errorf("error getting facet files: %v", err)
 	}
 	fmt.Println("Facet files length:", len(facetFiles))
 
-	compiledHashes, err := utils.GetCompiledFacetHashes(compiledFacetsPath, facetFiles)
+	compiledHashes, err := u.GetCompiledFacetHashes(compiledFacetsPath, facetFiles)
 	if err != nil {
 		return fmt.Errorf("error getting compiled facet hashes for path %s: %v", compiledFacetsPath, err)
 	}
@@ -120,7 +143,7 @@ func executeSourceDiff(cmd *cobra.Command, facetSourcePath, compiledFacetsPath s
 		}
 	}
 
-	err = utils.CreateFacetHashesReport(compiledFacetsPath, compiledHashes, reportOutDir, verbose)
+	err = u.CreateFacetHashesReport(compiledFacetsPath, compiledHashes, reportOutDir, verbose)
 	if err != nil {
 		return fmt.Errorf("error creating facet hashes report: %v", err)
 	}
--- scripts/bytecode-diff/utils/utils.go
+++ scripts/bytecode-diff/utils/utils.go
@@ -267,3 +267,12 @@ func categorizeHashes(
 
 	return updatedHashes, existingHashes
 }
+
+func Contains(slice []string, item string) bool {
+	for _, s := range slice {
+		if s == item {
+			return true
+		}
+	}
+	return false
+}
