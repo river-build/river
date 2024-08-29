325ddaf4c93fee976db207851df0067333b825c6
texuf <texuf.eth@gmail.com>
2024-08-28 22:50:58 -0700

    Remove unused load testing docker container (#888)
    We do stress testing now

--- scripts/kill-all-local-dev.sh
+++ scripts/kill-all-local-dev.sh
@@ -98,17 +98,6 @@ do_killl anvil "$1"
 do_killl wrangler "$1"
 do_killl mitmweb "$1"
 
-# Specify the name or ID of the Docker container you want to stop
-container_name="bullmq-redis"
-
-# Check if the container is running
-if docker ps --filter "name=$container_name" --format '{{.ID}}' | grep -qE "^[0-9a-f]+$"; then
-  # The container is running, so stop it
-  docker stop "$container_name"
-  echo "Container $container_name stopped."
-else
-  echo "Container $container_name is not running."
-fi
 
 echo ""
 if prompt 'Remove Casbablanca Docker Containers?:y/n '
