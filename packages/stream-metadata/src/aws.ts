747b26f20f1bb75e94fe9f44abb1a5086267eb1b
Kerem Kazan <kerem.kazan@gmail.com>
2024-09-04 17:37:10 -0400

    preventing semi-valid aws config for stream metadata (#966)

--- packages/stream-metadata/src/aws.ts
+++ packages/stream-metadata/src/aws.ts
@@ -2,13 +2,12 @@ import { CloudFront } from '@aws-sdk/client-cloudfront'
 
 import { config } from './environment'
 
-export const cloudFront = new CloudFront({
-	region: config.aws.region,
-	credentials:
-		config.aws.accessKeyId && config.aws.secretAccessKey
-			? {
-					accessKeyId: config.aws.accessKeyId,
-					secretAccessKey: config.aws.secretAccessKey,
-			  }
-			: undefined,
-})
+export const cloudFront = config.aws
+	? new CloudFront({
+			region: config.aws.AWS_REGION,
+			credentials: {
+				accessKeyId: config.aws.AWS_ACCESS_KEY_ID,
+				secretAccessKey: config.aws.AWS_SECRET_ACCESS_KEY,
+			},
+	  })
+	: undefined
