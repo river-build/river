4be6a6fdd44cb630c0f1e12a0a466a6247100787
texuf <texuf.eth@gmail.com>
2024-09-04 22:51:00 -0700

    Add pin and unpin to the sync agent (#975)
    Update return values for pin and unpin to return event id

--- packages/sdk/src/sync-agent/syncAgents.test.ts
+++ packages/sdk/src/sync-agent/syncAgents.test.ts
@@ -75,4 +75,38 @@ describe('syncAgents.test.ts', () => {
             { timeoutMS: 10000 },
         )
     })
+
+    test('syncAgents pin a message', async () => {
+        await Promise.all([bob.start(), alice.start()])
+        await waitFor(() => bob.spaces.value.status === 'loaded')
+        expect(bob.spaces.data.spaceIds.length).toBeGreaterThan(0)
+        const spaceId = bob.spaces.data.spaceIds[0]
+        expect(alice.user.memberships.isJoined(spaceId)).toBe(true) // alice joined above
+        const space = bob.spaces.getSpace(spaceId)
+        const channel = space.getDefaultChannel()
+        const channelId = channel.data.id
+        const event = channel.timeline.events.value.find((e) => e.text === 'Hello, World!')
+        expect(event).toBeDefined()
+        // bob can pin
+        const result = await channel.pin(event!.eventId)
+        expect(result).toBeDefined()
+        expect(result.error).toBeUndefined()
+        await waitFor(() =>
+            expect(
+                bob.riverConnection.client?.streams.get(channelId)?.view.membershipContent.pins
+                    .length,
+            ).toBe(1),
+        )
+        // bob can unpin
+        const result2 = await channel.unpin(event!.eventId)
+        expect(result2).toBeDefined()
+        expect(result2.error).toBeUndefined()
+
+        // alice can pin too (for now)
+        const aliceChannel = alice.spaces.getSpace(spaceId).getChannel(channelId)
+        await waitFor(() => aliceChannel.value.status === 'loaded')
+        const aliceEvent = await aliceChannel.pin(event!.eventId)
+        expect(aliceEvent).toBeDefined()
+        expect(aliceEvent.error).toBeUndefined()
+    })
 })
