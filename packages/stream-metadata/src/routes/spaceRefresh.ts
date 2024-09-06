a76b1368ead3a5f2d7fe7a0c473845e694c965fc
Kerem Kazan <kerem.kazan@gmail.com>
2024-09-06 15:19:38 -0400

    moved opensea into promise.all with cloudfront

--- packages/stream-metadata/src/routes/spaceRefresh.ts
+++ packages/stream-metadata/src/routes/spaceRefresh.ts
@@ -1,12 +1,9 @@
-import { FastifyReply, FastifyRequest, type FastifyBaseLogger } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-import { BigNumber } from 'ethers'
-import { BASE_MAINNET, BASE_SEPOLIA } from '@river-build/web3'
 
 import { isValidEthereumAddress } from '../validators'
-import { config } from '../environment'
 import { createCloudfrontInvalidation } from '../aws'
-import { spaceDapp } from '../contract-utils'
+import { refreshOpenSea } from '../opensea'
 
 const paramsSchema = z.object({
 	spaceAddress: z
@@ -33,60 +30,21 @@ export async function spaceRefresh(request: FastifyRequest, reply: FastifyReply)
 
 	try {
 		const path = `/space/${spaceAddress}/image`
-		await createCloudfrontInvalidation({ path, logger })
 
-		const openseaStatus = await refreshOpenSea(logger, spaceAddress)
-		if (!openseaStatus) {
-			return reply.code(500).send({ error: 'Failed to refresh space' })
-		}
+		await Promise.all([
+			createCloudfrontInvalidation({ path, logger }),
+			refreshOpenSea({ spaceAddress, logger }),
+		])
 
-		return reply.code(openseaStatus.status).send({ ok: openseaStatus.ok })
+		return reply.code(200).send({ ok: true })
 	} catch (error) {
 		logger.error(
 			{
 				error,
+				spaceAddress,
 			},
 			'Failed to refresh space',
 		)
 		return reply.code(500).send({ error: 'Failed to refresh space' })
 	}
-}
-
-const refreshOpenSea = async (logger: FastifyBaseLogger, spaceAddress: string) => {
-	if (!config.openSeaApiKey) {
-		return
-	}
-
-	const space = await spaceDapp.getSpaceInfo(spaceAddress)
-	if (!space) {
-		throw new Error('Space not found')
-	}
-
-	const tokenId = BigNumber.from(space.tokenId).toString()
-	let chain
-	let url
-	if (space.networkId === String(BASE_MAINNET)) {
-		chain = 'base'
-		url = `https://api.opensea.io/api/v2/chain/${chain}/contract/${spaceAddress}/nfts/${tokenId}/refresh`
-	} else if (space.networkId === String(BASE_SEPOLIA)) {
-		chain = 'base_sepolia'
-		url = `https://testnets-api.opensea.io/api/v2/chain/${chain}/contract/${spaceAddress}/nfts/${tokenId}/refresh`
-	} else {
-		throw new Error('Unsupported network')
-	}
-
-	logger.info({ url }, 'refreshing openSea')
-	const status = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'x-api-key': config.openSeaApiKey,
-		},
-	}).then((response) => {
-		if (!response.ok) {
-			return { ok: false, status: response.status }
-		}
-		return { ok: true, status: response.status }
-	})
-
-	return status
 }
