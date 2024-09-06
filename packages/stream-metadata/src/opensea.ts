04af3f2d48e8f447ff94853c709f9099f160b45e
Kerem Kazan <kerem.kazan@gmail.com>
2024-09-06 15:19:52 -0400

    moved opensea into own module

---
+++ packages/stream-metadata/src/opensea.ts
@@ -0,0 +1,58 @@
+import { BASE_MAINNET, BASE_SEPOLIA, SpaceInfo } from '@river-build/web3'
+import { BigNumber } from 'ethers'
+import { FastifyBaseLogger } from 'fastify'
+
+import { config } from './environment'
+import { spaceDapp } from './contract-utils'
+
+const getOpenSeaAPIUrl = (space: SpaceInfo) => {
+	const tokenId = BigNumber.from(space.tokenId).toString()
+	if (space.networkId === String(BASE_MAINNET)) {
+		return `https://api.opensea.io/api/v2/chain/base/contract/${space.address}/nfts/${tokenId}/refresh`
+	} else if (space.networkId === String(BASE_SEPOLIA)) {
+		return `https://testnets-api.opensea.io/api/v2/chain/base_sepolia/contract/${space.address}/nfts/${tokenId}/refresh`
+	} else {
+		throw new Error('Unsupported network')
+	}
+}
+
+export const refreshOpenSea = async ({
+	logger,
+	spaceAddress,
+}: {
+	logger: FastifyBaseLogger
+	spaceAddress: string
+}) => {
+	if (!config.openSeaApiKey) {
+		logger.warn(
+			{
+				spaceAddress,
+			},
+			'OpenSea API key not set, skipping OpenSea refresh',
+		)
+		return
+	}
+
+	const space = await spaceDapp.getSpaceInfo(spaceAddress)
+	if (!space) {
+		throw new Error('Space not found')
+	}
+
+	const url = getOpenSeaAPIUrl(space)
+	logger.info({ url, spaceAddress }, 'refreshing openSea')
+
+	const response = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'x-api-key': config.openSeaApiKey,
+		},
+	})
+
+	if (!response.ok) {
+		logger.error(
+			{ status: response.status, statusText: response.statusText, spaceAddress },
+			'Failed to refresh OpenSea',
+		)
+		throw new Error('Failed to refresh OpenSea')
+	}
+}
