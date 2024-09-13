cd90253ea6750f0388637e42be9d4b7801b84641
Kerem Kazan <kerem.kazan@gmail.com>
2024-09-13 12:21:08 -0400

    user image cache control headers done

--- packages/stream-metadata/src/routes/profileImage.ts
+++ packages/stream-metadata/src/routes/profileImage.ts
@@ -15,6 +15,13 @@ const paramsSchema = z.object({
 	}),
 })
 
+const CACHE_CONTROL = {
+	307: 'public, max-age=30, s-maxage=300',
+	400: 'public, max-age=30, s-maxage=300',
+	404: 'public, max-age=30, s-maxage=300',
+	422: 'public, max-age=30, s-maxage=300',
+}
+
 export async function fetchUserProfileImage(request: FastifyRequest, reply: FastifyReply) {
 	const logger = request.log.child({ name: fetchUserProfileImage.name })
 	const parseResult = paramsSchema.safeParse(request.params)
@@ -22,7 +29,10 @@ export async function fetchUserProfileImage(request: FastifyRequest, reply: Fast
 	if (!parseResult.success) {
 		const errorMessage = parseResult.error.errors[0]?.message || 'Invalid parameters'
 		logger.info(errorMessage)
-		return reply.code(400).send({ error: 'Bad Request', message: errorMessage })
+		return reply
+			.code(400)
+			.header('Cache-Control', CACHE_CONTROL[400])
+			.send({ error: 'Bad Request', message: errorMessage })
 	}
 
 	const { userId } = parseResult.data
@@ -40,13 +50,16 @@ export async function fetchUserProfileImage(request: FastifyRequest, reply: Fast
 			},
 			'Failed to get stream',
 		)
-		return reply.code(404).send('Stream not found')
+		return reply.code(404).header('Cache-Control', CACHE_CONTROL[404]).send('Stream not found')
 	}
 
 	// get the image metadata from the stream
 	const profileImage = await getUserProfileImage(stream)
 	if (!profileImage) {
-		return reply.code(404).send('profileImage not found')
+		return reply
+			.header('Cache-Control', CACHE_CONTROL[404])
+			.code(404)
+			.send('profileImage not found')
 	}
 
 	try {
@@ -61,7 +74,10 @@ export async function fetchUserProfileImage(request: FastifyRequest, reply: Fast
 				},
 				'Invalid key or iv',
 			)
-			return reply.code(422).send('Failed to get encryption key or iv')
+			return reply
+				.header('Cache-Control', CACHE_CONTROL[422])
+				.code(422)
+				.send('Failed to get encryption key or iv')
 		}
 		const redirectUrl = `${config.streamMetadataBaseUrl}/media/${
 			profileImage.streamId
@@ -71,7 +87,7 @@ export async function fetchUserProfileImage(request: FastifyRequest, reply: Fast
 			reply
 				// client should cache the image for 30 seconds, and the CDN for 5 minutes
 				// after 30 seconds, the client will check the CDN for a new image
-				.header('Cache-Control', 'public, max-age=30, s-maxage=300')
+				.header('Cache-Control', CACHE_CONTROL[307])
 				.redirect(redirectUrl, 307)
 		)
 	} catch (error) {
@@ -83,7 +99,10 @@ export async function fetchUserProfileImage(request: FastifyRequest, reply: Fast
 			},
 			'Failed to get encryption key or iv',
 		)
-		return reply.code(422).send('Failed to get encryption key or iv')
+		return reply
+			.code(422)
+			.header('Cache-Control', CACHE_CONTROL[422])
+			.send('Failed to get encryption key or iv')
 	}
 }
 
