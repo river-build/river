0cb56aa100038d888e475c184222e475ab6555fb
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-09-16 03:07:39 -0400

    Refactor staking logic for operator commission
    Updated `StakingRewards` to include commission logic for delegatees. Introduced `RewardsDistribution` contract to handle staking operations, simplifying reward distributions. Added `commissionRateByDelegatee` mapping to manage commission rates.

---
+++ contracts/src/base/registry/facets/distribution/v2/IRewardsDistribution.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// interfaces
+
+// libraries
+
+// contracts
+
+interface IRewardsDistributionBase {}
+
+interface IRewardsDistribution is IRewardsDistributionBase {}
