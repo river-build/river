ae2ee0217887120e09f7f31a1ccd7aba940975ed
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-09-21 03:58:49 -0400

    Add viewer functions and interface
    Implemented several view functions in `RewardsDistribution` to provide state queries. Updated `IRewardsDistribution` interface and `StakingRewards` struct definitions to support these changes.

--- contracts/src/base/registry/facets/distribution/v2/RewardsDistribution.sol
+++ contracts/src/base/registry/facets/distribution/v2/RewardsDistribution.sol
@@ -20,8 +20,9 @@ import {IRewardsDistribution} from "./IRewardsDistribution.sol";
 contract RewardsDistribution is IRewardsDistribution, Facet {
   using StakingRewards for StakingRewards.Layout;
 
-  error RewardsDistribution_NotDepositOwner();
-  error RewardsDistribution_NotRewardNotifier();
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                       STATE MUTATING                       */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
   function __RewardsDistribution_init() external onlyInitializing {
     _addInterface(type(IRewardsDistribution).interfaceId);
@@ -76,7 +77,7 @@ contract RewardsDistribution is IRewardsDistribution, Facet {
   function increaseStake(uint256 depositId, uint96 amount) external {
     RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
       .layout();
-    StakingRewards.Deposit storage deposit = ds.staking.deposits[depositId];
+    StakingRewards.Deposit storage deposit = ds.staking.depositById[depositId];
     _revertIfNotDepositOwner(deposit);
 
     StakingRewards.increaseStake(ds.staking, deposit, amount);
@@ -85,7 +86,7 @@ contract RewardsDistribution is IRewardsDistribution, Facet {
   function redelegate(uint256 depositId, address delegatee) external {
     RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
       .layout();
-    StakingRewards.Deposit storage deposit = ds.staking.deposits[depositId];
+    StakingRewards.Deposit storage deposit = ds.staking.depositById[depositId];
     _revertIfNotDepositOwner(deposit);
 
     StakingRewards.redelegate(ds.staking, deposit, delegatee);
@@ -97,7 +98,7 @@ contract RewardsDistribution is IRewardsDistribution, Facet {
   ) external {
     RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
       .layout();
-    StakingRewards.Deposit storage deposit = ds.staking.deposits[depositId];
+    StakingRewards.Deposit storage deposit = ds.staking.depositById[depositId];
     _revertIfNotDepositOwner(deposit);
 
     StakingRewards.changeBeneficiary(ds.staking, deposit, newBeneficiary);
@@ -106,7 +107,7 @@ contract RewardsDistribution is IRewardsDistribution, Facet {
   function withdraw(uint256 depositId, uint96 amount) external {
     RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
       .layout();
-    StakingRewards.Deposit storage deposit = ds.staking.deposits[depositId];
+    StakingRewards.Deposit storage deposit = ds.staking.depositById[depositId];
     _revertIfNotDepositOwner(deposit);
 
     StakingRewards.withdraw(ds.staking, deposit, amount);
@@ -127,6 +128,119 @@ contract RewardsDistribution is IRewardsDistribution, Facet {
 
     StakingRewards.notifyRewardAmount(ds.staking, reward);
   }
+
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                          VIEWERS                           */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+  /// @inheritdoc IRewardsDistribution
+  function stakingState()
+    external
+    view
+    returns (
+      address rewardToken,
+      address stakeToken,
+      uint256 totalStaked,
+      uint256 rewardDuration,
+      uint256 rewardEndTime,
+      uint256 lastUpdateTime,
+      uint256 rewardRate,
+      uint256 rewardPerTokenAccumulated,
+      uint256 nextDepositId
+    )
+  {
+    StakingRewards.Layout storage staking = RewardsDistributionStorage
+      .layout()
+      .staking;
+    return (
+      staking.rewardToken,
+      staking.stakeToken,
+      staking.totalStaked,
+      staking.rewardDuration,
+      staking.rewardEndTime,
+      staking.lastUpdateTime,
+      staking.rewardRate,
+      staking.rewardPerTokenAccumulated,
+      staking.nextDepositId
+    );
+  }
+
+  /// @inheritdoc IRewardsDistribution
+  function stakedByDepositor(
+    address depositor
+  ) external view returns (uint256 amount) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return ds.staking.stakedByDepositor[depositor];
+  }
+
+  /// @inheritdoc IRewardsDistribution
+  function treasureByBeneficiary(
+    address beneficiary
+  ) external view returns (StakingRewards.Treasure memory) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return ds.staking.treasureByBeneficiary[beneficiary];
+  }
+
+  /// @inheritdoc IRewardsDistribution
+  function depositById(
+    uint256 depositId
+  ) external view returns (StakingRewards.Deposit memory) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return ds.staking.depositById[depositId];
+  }
+
+  function delegationProxies(
+    address delegatee
+  ) external view returns (address proxy) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return ds.staking.delegationProxies[delegatee];
+  }
+
+  function commissionRateByDelegatee(
+    address delegatee
+  ) external view returns (uint256) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return ds.staking.commissionRateByDelegatee[delegatee];
+  }
+
+  function isRewardNotifier(address notifier) external view returns (bool) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return ds.isRewardNotifier[notifier];
+  }
+
+  function lastTimeRewardDistributed() external view returns (uint256) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return StakingRewards.lastTimeRewardDistributed(ds.staking);
+  }
+
+  function currentRewardPerTokenAccumulated() external view returns (uint256) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return StakingRewards.currentRewardPerTokenAccumulated(ds.staking);
+  }
+
+  function currentUnclaimedReward(
+    address beneficiary
+  ) external view returns (uint256) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    return
+      StakingRewards.currentUnclaimedReward(
+        ds.staking,
+        ds.staking.treasureByBeneficiary[beneficiary]
+      );
+  }
+
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                          INTERNAL                          */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
   function _revertIfNotDepositOwner(
     StakingRewards.Deposit storage deposit
