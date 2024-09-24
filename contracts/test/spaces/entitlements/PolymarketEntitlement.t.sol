30025d1c176f24dc7ae217a6b4bb83039096d8d4
Brian Meek <brian@hntlabs.com>
2024-09-24 10:13:47 -0700

    Code review feedback
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- contracts/test/spaces/entitlements/PolymarketEntitlement.t.sol
+++ contracts/test/spaces/entitlements/PolymarketEntitlement.t.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.23;
 
 import "forge-std/Test.sol";
-import {PolymarketEntitlement, IGnosisSafeProxyFactory} from "contracts/src/custom/PolymarketEntitlement.sol";
+import {PolymarketEntitlement, IGnosisSafeProxyFactory} from "contracts/src/spaces/entitlements/PolymarketEntitlement.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
-contract CrossChainEntitlementTest is Test {
+contract PolyMarketEntitlementTest is Test {
   PolymarketEntitlement entitlement;
   address user1;
   address user2;
