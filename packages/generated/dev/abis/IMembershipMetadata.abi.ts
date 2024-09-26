845a03a4c6acad4124740f698a4c5bbcb79754a2
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-09-26 15:48:45 -0400

    Add `IMembershipMetadata` interface and related typings
    Introduced the `IMembershipMetadata` interface along with ABI files, TypeScript typings, and factory files. Updated the build script and included necessary export statements for the new interface.

---
+++ packages/generated/dev/abis/IMembershipMetadata.abi.ts
@@ -0,0 +1,60 @@
+export default [
+  {
+    "type": "function",
+    "name": "refreshMetadata",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "tokenURI",
+    "inputs": [
+      {
+        "name": "tokenId",
+        "type": "uint256",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "",
+        "type": "string",
+        "internalType": "string"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "event",
+    "name": "BatchMetadataUpdate",
+    "inputs": [
+      {
+        "name": "_fromTokenId",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      },
+      {
+        "name": "_toTokenId",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "MetadataUpdate",
+    "inputs": [
+      {
+        "name": "_tokenId",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  }
+] as const
