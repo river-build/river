e7005e95fcbdda9ef46d639c874c95d3900c0dcc
Crystal Lemire <crystal@hntlabs.com>
2024-09-27 09:10:17 -0700

    Space, channel isEnabled errors propagate back to caller (#1167)
    In authorization checks:
    - update space and channel isEnabled check to propogated isEnabled,
    error state back to entitlement check
    - in entitlement check, only return a disallowed result if the space or
    channel is disabled. If there was an error in evaluating either state,
    return the error.
    
    These changes should prevent users from being booted from channels in
    the event of partial or total base availability outages.
    
    As part of this PR, I added some happy path e2e tests for disabled
    channels and spaces. This does not check the specific bug I was fixing
    but should help prevent regressions in general.

---
+++ packages/sdk/src/disableSpace.test.ts
@@ -0,0 +1,74 @@
+/**
+ * @group with-entitlements
+ */
+
+import { NoopRuleData } from '@river-build/web3'
+import {
+    expectUserCanJoin,
+    createTownWithRequirements,
+    createUserStreamAndSyncClient,
+    everyoneMembershipStruct,
+} from './util.test'
+
+describe('disableSpace', () => {
+    test('User cannot join a space after it is disabled', async () => {
+        const {
+            alice,
+            carol,
+            alicesWallet,
+            aliceProvider,
+            bobProvider,
+            carolProvider,
+            aliceSpaceDapp,
+            bobSpaceDapp,
+            carolSpaceDapp,
+            spaceId,
+            channelId,
+        } = await createTownWithRequirements({
+            everyone: true,
+            users: [],
+            ruleData: NoopRuleData,
+        })
+
+        await expectUserCanJoin(
+            spaceId,
+            channelId,
+            'alice',
+            alice,
+            aliceSpaceDapp,
+            alicesWallet.address,
+            aliceProvider.wallet,
+        )
+
+        // Disable the channel
+        const txn = await bobSpaceDapp.setSpaceAccess(spaceId, true, bobProvider.wallet)
+        await bobProvider.waitForTransaction(txn.hash)
+        const spaceInfo = await bobSpaceDapp.getSpaceInfo(spaceId)
+        expect(spaceInfo).toBeDefined()
+        expect(spaceInfo?.disabled).toBeTruthy()
+
+        // Wait 5 seconds for the positive space enabled cache entry to expire
+        await new Promise((f) => setTimeout(f, 5000))
+
+        // Have carol create a user stream attached to her own space.
+        // Then she will attempt to join the space from the client, which should fail.
+        await createUserStreamAndSyncClient(
+            carol,
+            carolSpaceDapp,
+            'carol',
+            await everyoneMembershipStruct(carolSpaceDapp, carol),
+            carolProvider.wallet,
+        )
+
+        // Expect user cannot join the space.
+        // TODO: the spaceDapp does not check for space enabled when evaluating if user
+        // is eligible to join the space. Fix and replace the below logic with
+        // await expectUserCannotJoinSpace(
+        //     spaceId,
+        //     carol,
+        //     carolSpaceDapp,
+        //     carolsWallet.address
+        // )
+        await expect(carol.joinStream(spaceId)).rejects.toThrow(/PERMISSION_DENIED/)
+    })
+})
