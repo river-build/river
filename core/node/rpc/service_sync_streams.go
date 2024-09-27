9bfc94c0f5caa5a7d02eae78b5f62184399ecff7
Crystal Lemire <crystal@hntlabs.com>
2024-09-27 09:41:36 -0700

    Add scrub scheduling to sync streams init and add stream to sync.

--- core/node/rpc/service_sync_streams.go
+++ core/node/rpc/service_sync_streams.go
@@ -9,6 +9,7 @@ import (
 
 	. "github.com/river-build/river/core/node/base"
 	. "github.com/river-build/river/core/node/protocol"
+	"github.com/river-build/river/core/node/shared"
 	"github.com/river-build/river/core/node/utils"
 )
 
@@ -46,6 +47,18 @@ func (s *Service) SyncStreams(
 			LogWarn(log).
 			AsConnectError()
 	} else {
+		for _, cookie := range req.Msg.SyncPos {
+			streamId, err := shared.StreamIdFromBytes(cookie.StreamId)
+			if err != nil {
+				log.Error(
+					"Unable to derive stream id from sync cookie for scrubbing",
+					"rawStreamId",
+					cookie.StreamId,
+				)
+				continue
+			}
+			_, _ = s.scrubTaskProcessor.TryScheduleScrub(ctx, streamId)
+		}
 		log.Info("SyncStreams DONE", "syncId", syncId, "duration", time.Since(startTime))
 	}
 	return err
@@ -68,6 +81,17 @@ func (s *Service) AddStreamToSync(
 			Tags("syncId", req.Msg.GetSyncId(), "streamId", req.Msg.GetSyncPos().GetStreamId()).
 			LogWarn(log).
 			AsConnectError()
+	} else {
+		streamId, err := shared.StreamIdFromBytes(req.Msg.SyncPos.StreamId)
+		if err != nil {
+			log.Error(
+				"Unable to derive stream id from sync cookie for scrubbing",
+				"rawStreamId",
+				req.Msg.SyncPos.StreamId,
+			)
+		} else {
+			_, _ = s.scrubTaskProcessor.TryScheduleScrub(ctx, streamId)
+		}
 	}
 	return res, err
 }
