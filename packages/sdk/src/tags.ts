b58de84ccab3f52c56d34498a1b4d67eb7855c74
texuf <texuf.eth@gmail.com>
2024-09-30 21:47:58 -0700

    Add tags message to StreamEvent, populate with channel message data

---
+++ packages/sdk/src/tags.ts
@@ -0,0 +1,100 @@
+import { PlainMessage } from '@bufbuild/protobuf'
+import { ChannelMessage, GroupMentionType, MessageInteractionType, Tags } from '@river-build/proto'
+import { IStreamStateView } from './streamStateView'
+import { addressFromUserId } from './id'
+
+export function makeTags(
+    message: PlainMessage<ChannelMessage>,
+    streamView: IStreamStateView,
+): PlainMessage<Tags> {
+    return {
+        messageInteractionType: getMessageInteractionType(message),
+        groupMentionType: getGroupMentionType(message),
+        mentionedUserAddresses: getMentionedUserIds(message),
+        participatingUserIds: getParticipatingUserIds(message, streamView),
+    } satisfies PlainMessage<Tags>
+}
+
+function getMessageInteractionType(message: PlainMessage<ChannelMessage>): MessageInteractionType {
+    switch (message.payload.case) {
+        case 'reaction':
+            return MessageInteractionType.REACTION
+        case 'post':
+            if (message.payload.value.threadId) {
+                return MessageInteractionType.REPLY
+            } else if (message.payload.value.replyId) {
+                return MessageInteractionType.REPLY
+            } else {
+                return MessageInteractionType.UNSPECIFIED
+            }
+        default:
+            return MessageInteractionType.UNSPECIFIED
+    }
+}
+
+function getGroupMentionType(message: PlainMessage<ChannelMessage>): GroupMentionType {
+    if (
+        message.payload.case === 'post' &&
+        message.payload.value.content.case === 'text' &&
+        message.payload.value.content.value.mentions.find(
+            (m) => m.mentionBehavior.case === 'atChannel',
+        )
+    ) {
+        return GroupMentionType.AT_CHANNEL
+    }
+    return GroupMentionType.UNSPECIFIED
+}
+
+function getMentionedUserIds(message: PlainMessage<ChannelMessage>): Uint8Array[] {
+    if (message.payload.case === 'post' && message.payload.value.content.case === 'text') {
+        return message.payload.value.content.value.mentions
+            .filter((m) => m.mentionBehavior.case === undefined && m.userId.length > 0)
+            .map((m) => addressFromUserId(m.userId))
+    }
+    return []
+}
+
+function getParticipatingUserIds(
+    message: PlainMessage<ChannelMessage>,
+    streamView: IStreamStateView,
+): Uint8Array[] {
+    switch (message.payload.case) {
+        case 'reaction': {
+            const event = streamView.events.get(message.payload.value.refEventId)
+            if (event && event.remoteEvent?.event.creatorAddress) {
+                return [event.remoteEvent.event.creatorAddress]
+            }
+            return []
+        }
+        case 'post': {
+            const participating = new Set<Uint8Array>()
+            const parentId = message.payload.value.threadId || message.payload.value.replyId
+            if (parentId) {
+                const parentEvent = streamView.events.get(parentId)
+                if (parentEvent && parentEvent.remoteEvent?.event.creatorAddress) {
+                    participating.add(parentEvent.remoteEvent.event.creatorAddress)
+                }
+                streamView.timeline.forEach((event) => {
+                    if (
+                        event.decryptedContent?.kind === 'channelMessage' &&
+                        event.decryptedContent.content.payload.case === 'post' &&
+                        event.decryptedContent.content.payload.value.threadId === parentId &&
+                        event.remoteEvent?.event.creatorAddress
+                    ) {
+                        participating.add(event.remoteEvent.event.creatorAddress)
+                    } else if (
+                        event.decryptedContent?.kind === 'channelMessage' &&
+                        event.decryptedContent.content.payload.case === 'reaction' &&
+                        event.decryptedContent.content.payload.value.refEventId === parentId &&
+                        event.remoteEvent?.event.creatorAddress
+                    ) {
+                        participating.add(event.remoteEvent.event.creatorAddress)
+                    }
+                })
+            }
+            return Array.from(participating)
+        }
+        default:
+            return []
+    }
+}
