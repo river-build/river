e3cedc7789301f46094c6f70e8c50a529d7896ef
texuf <texuf.eth@gmail.com>
2024-10-09 16:05:46 +0200

    Reduce overhead of cache of spaces in SpaceRegistrar
    we don't really need to cache spaces, but they instantiate a lot of objects for the contracts and it's worth not wasting memory if we need to access the same space multiple times this code is also used on the server so we don't want to cache spaces for too long

--- packages/web3/src/v3/SpaceRegistrar.ts
+++ packages/web3/src/v3/SpaceRegistrar.ts
@@ -7,7 +7,7 @@ import { Space } from './Space'
 import { ethers } from 'ethers'
 
 interface SpaceMap {
-    [spaceId: string]: Space
+    [spaceId: string]: { space: Space | undefined; lastSeen: number }
 }
 
 /**
@@ -41,13 +41,30 @@ export class SpaceRegistrar {
     }
 
     public getSpace(spaceId: string): Space | undefined {
-        if (this.spaces[spaceId] === undefined) {
-            const spaceAddress = SpaceAddressFromSpaceId(spaceId)
-            if (!spaceAddress || spaceAddress === ethers.constants.AddressZero) {
-                return undefined // space is not found
+        // aellis 10/2024 we don't really need to cache spaces, but they instantiate a lot of objects
+        // for the contracts and it's worth not wasting memory if we need to access the same space multiple times
+        // this code is also used on the server so we don't want to cache spaces for too long
+        const space = this.spaces[spaceId]?.space || this.createSpace(spaceId)
+        this.pruneSpaces()
+        this.spaces[spaceId] = { lastSeen: new Date().getTime(), space }
+        return space
+    }
+
+    private pruneSpaces(): void {
+        // clear out spaces that haven't been seen in 5 seconds
+        const fiveSecondsAgo = new Date().getTime() - 1000 * 5
+        for (const spaceId in this.spaces) {
+            if (this.spaces[spaceId].lastSeen < fiveSecondsAgo) {
+                delete this.spaces[spaceId]
             }
-            this.spaces[spaceId] = new Space(spaceAddress, spaceId, this.config, this.provider)
         }
-        return this.spaces[spaceId]
+    }
+
+    private createSpace(spaceId: string): Space | undefined {
+        const spaceAddress = SpaceAddressFromSpaceId(spaceId)
+        if (!spaceAddress || spaceAddress === ethers.constants.AddressZero) {
+            return undefined // space is not found
+        }
+        return new Space(spaceAddress, spaceId, this.config, this.provider)
     }
 }
