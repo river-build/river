244e8f3292d379af8e5a6ec8780a660af7d17d49
Serge Khorun <104387024+sergekh2@users.noreply.github.com>
2024-10-12 14:05:56 -0700

    Extend duplicate event check to include previous miniblocks (#1244)

--- core/node/events/stream_view.go
+++ core/node/events/stream_view.go
@@ -209,6 +209,23 @@ func (r *streamViewImpl) copyAndAddEvent(event *ParsedEvent) (*streamViewImpl, e
 			Tags("event", event.ShortDebugStr(), "streamId", r.streamId)
 	}
 
+	for _, block := range r.blocks {
+		for _, e := range block.events {
+			if event.Hash == e.Hash {
+				return nil, RiverError(
+					Err_DUPLICATE_EVENT,
+					"streamViewImpl: previous block contains this event",
+					"blockNum",
+					block.Num,
+					"event",
+					event.ShortDebugStr(),
+					"streamId",
+					r.streamId,
+				).Func("copyAndAddEvent")
+			}
+		}
+	}
+
 	ret := &streamViewImpl{
 		streamId:      r.streamId,
 		blocks:        r.blocks,
@@ -259,7 +276,6 @@ func (r *streamViewImpl) makeMiniblockHeader(
 		)
 	}
 
-	log := dlog.FromCtx(ctx)
 	hashes := make([][]byte, 0, r.minipool.events.Len())
 	events := make([]*ParsedEvent, 0, r.minipool.events.Len())
 
@@ -295,7 +311,7 @@ func (r *streamViewImpl) makeMiniblockHeader(
 				offset := block.header().EventNumOffset
 				err := Update_Snapshot(snapshot, e, miniblockNum, offset+int64(j))
 				if err != nil {
-					log.Error("Failed to update snapshot",
+					dlog.FromCtx(ctx).Error("Failed to update snapshot",
 						"error", err,
 						"streamId", r.streamId,
 						"event", e.ShortDebugStr(),
@@ -303,11 +319,11 @@ func (r *streamViewImpl) makeMiniblockHeader(
 				}
 			}
 		}
-		// update with current events in minipool
+		// update with events from the new block
 		for i, e := range events {
 			err := Update_Snapshot(snapshot, e, nextMiniblockNum, eventNumOffset+int64(i))
 			if err != nil {
-				log.Error("Failed to update snapshot",
+				dlog.FromCtx(ctx).Error("Failed to update snapshot",
 					"error", err,
 					"streamId", r.streamId,
 					"event", e.ShortDebugStr(),
