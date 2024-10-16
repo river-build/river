591489b16b1cb1de2f86f935771d78bd02820f8d
Kerem Kazan <kerem.kazan@gmail.com>
2024-10-16 12:32:35 -0400

    custom logging logic for stream metadata service (#1268)
    fastify's default logging reports status codes of responses, without
    showing anything about the original request. this PR removes the default
    logger and replaces it with one that standardizes everything

--- packages/stream-metadata/src/node.ts
+++ packages/stream-metadata/src/node.ts
@@ -47,28 +47,41 @@ logger.info(
 /*
  * Server setup
  */
-export type Server = FastifyInstance<
-	HTTPServer | HTTPSServer,
-	IncomingMessage,
-	ServerResponse,
-	typeof logger
->
+export type Server = FastifyInstance<HTTPServer | HTTPSServer, IncomingMessage, ServerResponse>
 
 const server = Fastify({
-	logger,
+	logger: false,
 	genReqId: () => uuidv4(),
 })
 
 server.addHook('onRequest', (request, reply, done) => {
-	request.log = request.log.child({
-		request: {
+	request.log = logger.child({
+		req: {
 			id: request.id,
 			url: request.url,
 			query: request.query,
 			params: request.params,
 			routerPath: request.routerPath,
+			method: request.method,
 		},
 	})
+
+	request.log.info('incoming request')
+
+	done()
+})
+
+server.addHook('onResponse', (request, reply, done) => {
+	request.log.info(
+		{
+			res: {
+				statusCode: reply.statusCode,
+				elapsedTime: reply.elapsedTime,
+			},
+		},
+		'request completed',
+	)
+
 	done()
 })
 
