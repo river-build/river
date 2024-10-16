99e990f58bba6da241b0f3f20be052b3684e3805
Brian Meek <brian@hntlabs.com>
2024-10-15 17:13:19 -0700

    Prototype of checking signatures in a worker
    Signed-off-by: Brian Meek <brian@hntlabs.com>

---
+++ packages/stream-metadata/src/unpackStreamWorker.ts
@@ -0,0 +1,39 @@
+// unpackStreamWorker.js
+import { parentPort } from 'worker_threads'
+
+import { ParsedStreamResponse, unpackStream } from '@river-build/sdk'
+import { StreamAndCookie } from '@river-build/proto'
+
+export type WorkerMessage = {
+	stream: StreamAndCookie
+}
+
+export type WorkerResponse =
+	| {
+			unpackedResponse: ParsedStreamResponse
+	  }
+	| {
+			error: {
+				message: string
+				name?: string
+				stack?: string
+			}
+	  }
+
+if (parentPort) {
+	parentPort.on('message', async (message: { stream: StreamAndCookie }) => {
+		try {
+			const unpackedResponse = await unpackStream(message.stream)
+			parentPort?.postMessage({ unpackedResponse })
+		} catch (error: unknown) {
+			const errorResponse: WorkerResponse = {
+				error: {
+					message: error instanceof Error ? error.message : String(error),
+					name: error instanceof Error ? error.name : undefined,
+					stack: error instanceof Error ? error.stack : undefined,
+				},
+			}
+			parentPort?.postMessage(errorResponse)
+		}
+	})
+}
