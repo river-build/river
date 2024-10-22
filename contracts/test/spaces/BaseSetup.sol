8ec3b0f7c1443ab77d7790f34a4034adc2c5f80a
Giuseppe Rodriguez <5714678+giuseppecrj@users.noreply.github.com>
2024-10-23 00:25:47 +0200

    allow setting river token in dependency (#1331)

--- contracts/test/spaces/BaseSetup.sol
+++ contracts/test/spaces/BaseSetup.sol
@@ -109,7 +109,11 @@ contract BaseSetup is TestUtils, SpaceHelper {
       IEntryPoint(_randomAddress())
     );
 
+    // River Token
+    riverToken = deployRiverTokenBase.deploy(deployer);
+
     // Base Registry
+    deployBaseRegistry.setDependencies({riverToken_: riverToken});
     baseRegistry = deployBaseRegistry.deploy(deployer);
     entitlementChecker = IEntitlementChecker(baseRegistry);
     nodeOperator = INodeOperator(baseRegistry);
@@ -139,7 +143,7 @@ contract BaseSetup is TestUtils, SpaceHelper {
     eip712Facet = EIP712Facet(spaceFactory);
 
     // Base Registry Diamond
-    riverToken = deployRiverTokenBase.deploy(deployer);
+
     bridge = deployRiverTokenBase.bridgeBase();
 
     // POST DEPLOY
