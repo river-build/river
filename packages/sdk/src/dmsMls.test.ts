c9c009f970c326cd72819389c5862af4aeb477a7
Jakub Zalewski <jakub@hntlabs.com>
2024-10-23 15:55:50 +0200

    Add basic MLS sanity test

--- packages/sdk/src/dmsMls.test.ts
+++ packages/sdk/src/dmsMls.test.ts
@@ -33,6 +33,34 @@ describe('dmsTests', () => {
     const utf8Encoder = new TextEncoder()
     const utf8Decoder = new TextDecoder()
 
+    // Sanity check for MLS
+    test('jestCanLoadMlsLibrary', async () => {
+        const aliceMlsClient: MlsClient = await MlsClient.create('Alice')
+        const aliceMlsGroup = await aliceMlsClient.createGroup()
+
+        const bobMlsClient: MlsClient = await MlsClient.create('Bob')
+        const bobKeyPackage = await bobMlsClient.generateKeyPackageMessage()
+
+        const {
+            welcomeMessages: [welcome],
+        } = await aliceMlsGroup.addMember(bobKeyPackage)
+
+        const { group: bobMlsGroup } = await bobMlsClient.joinGroup(welcome)
+        await aliceMlsGroup.applyPendingCommit()
+
+        const message = await aliceMlsGroup.encryptApplicationMessage(
+            utf8Encoder.encode('Hello Bob!'),
+        )
+
+        const received = await bobMlsGroup.processIncomingMessage(message)
+        const applicationMessage = received.asApplicationMessage()!
+
+        expect(applicationMessage).toBeDefined()
+
+        expect(utf8Decoder.decode(applicationMessage.data())).toBe('Hello Bob!')
+    })
+
+    // NOTE: MLS Encryption is done out-of band
     test('clientsCanSendMlsMessages', async () => {
         const bobsClient = await makeInitAndStartClient()
         const alicesClient = await makeInitAndStartClient()
