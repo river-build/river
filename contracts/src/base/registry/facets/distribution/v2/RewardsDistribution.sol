f15f3faeb77e552fdfd7fd11436aa52400569bbe
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-10-26 13:16:00 -0400

    Refactor `RewardsDistribution` contract logic into base contract
    Extracted common staking functionality and sanity checks from `RewardsDistribution` into a new abstract `RewardsDistributionBase` contract. This change simplifies the `RewardsDistribution` contract and makes the code more modular and maintainable. It also removed redundant imports and streamlined function calls.

--- contracts/src/base/registry/facets/distribution/v2/RewardsDistribution.sol
+++ contracts/src/base/registry/facets/distribution/v2/RewardsDistribution.sol
@@ -6,14 +6,9 @@ import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC2
 import {IRewardsDistribution} from "./IRewardsDistribution.sol";
 
 // libraries
-import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import {LibClone} from "solady/utils/LibClone.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
-import {SignatureCheckerLib} from "solady/utils/SignatureCheckerLib.sol";
 import {UpgradeableBeacon} from "solady/utils/UpgradeableBeacon.sol";
 import {CustomRevert} from "contracts/src/utils/libraries/CustomRevert.sol";
-import {NodeOperatorStorage} from "contracts/src/base/registry/facets/operator/NodeOperatorStorage.sol";
-import {SpaceDelegationStorage} from "contracts/src/base/registry/facets/delegation/SpaceDelegationStorage.sol";
 import {StakingRewards} from "./StakingRewards.sol";
 import {RewardsDistributionStorage} from "./RewardsDistributionStorage.sol";
 
@@ -23,15 +18,16 @@ import {OwnableBase} from "contracts/src/diamond/facets/ownable/OwnableBase.sol"
 import {Nonces} from "contracts/src/diamond/utils/Nonces.sol";
 import {EIP712Base} from "contracts/src/diamond/utils/cryptography/signature/EIP712Base.sol";
 import {DelegationProxy} from "./DelegationProxy.sol";
+import {RewardsDistributionBase} from "./RewardsDistributionBase.sol";
 
 contract RewardsDistribution is
   IRewardsDistribution,
+  RewardsDistributionBase,
   OwnableBase,
   EIP712Base,
   Nonces,
   Facet
 {
-  using EnumerableSet for EnumerableSet.AddressSet;
   using SafeTransferLib for address;
   using StakingRewards for StakingRewards.Layout;
 
@@ -93,28 +89,12 @@ contract RewardsDistribution is
   /*                       STATE MUTATING                       */
   /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-  modifier onlyOperatorOrSpace(address delegatee) {
-    _onlyOperatorOrSpace(delegatee);
-    _;
-  }
-
   function stake(
     uint96 amount,
     address delegatee,
     address beneficiary
-  ) external onlyOperatorOrSpace(delegatee) returns (uint256 depositId) {
-    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
-      .layout();
-    depositId = ds.staking.stake(
-      msg.sender,
-      amount,
-      delegatee,
-      beneficiary,
-      _getCommissionRate(delegatee)
-    );
-
-    address proxy = _deployDelegationProxy(depositId, delegatee);
-    ds.staking.stakeToken.safeTransferFrom(msg.sender, proxy, amount);
+  ) external returns (uint256 depositId) {
+    depositId = _stake(amount, delegatee, beneficiary, msg.sender);
   }
 
   function permitAndStake(
@@ -125,11 +105,10 @@ contract RewardsDistribution is
     uint8 v,
     bytes32 r,
     bytes32 s
-  ) external onlyOperatorOrSpace(delegatee) returns (uint256 depositId) {
-    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
-      .layout();
+  ) external returns (uint256 depositId) {
+    address stakeToken = RewardsDistributionStorage.layout().staking.stakeToken;
     try
-      IERC20Permit(ds.staking.stakeToken).permit(
+      IERC20Permit(stakeToken).permit(
         msg.sender,
         address(this),
         amount,
@@ -139,16 +118,8 @@ contract RewardsDistribution is
         s
       )
     {} catch {}
-    depositId = ds.staking.stake(
-      msg.sender,
-      amount,
-      delegatee,
-      beneficiary,
-      _getCommissionRate(delegatee)
-    );
 
-    address proxy = _deployDelegationProxy(depositId, delegatee);
-    ds.staking.stakeToken.safeTransferFrom(msg.sender, proxy, amount);
+    depositId = _stake(amount, delegatee, beneficiary, msg.sender);
   }
 
   function stakeOnBehalf(
@@ -158,8 +129,9 @@ contract RewardsDistribution is
     address owner,
     uint256 deadline,
     bytes calldata signature
-  ) external onlyOperatorOrSpace(delegatee) returns (uint256 depositId) {
+  ) external returns (uint256 depositId) {
     _revertIfPastDeadline(deadline);
+
     bytes32 structHash = keccak256(
       abi.encode(
         STAKE_TYPEHASH,
@@ -176,18 +148,8 @@ contract RewardsDistribution is
       _hashTypedDataV4(structHash),
       signature
     );
-    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
-      .layout();
-    depositId = ds.staking.stake(
-      owner,
-      amount,
-      delegatee,
-      beneficiary,
-      _getCommissionRate(delegatee)
-    );
 
-    address proxy = _deployDelegationProxy(depositId, delegatee);
-    ds.staking.stakeToken.safeTransferFrom(msg.sender, proxy, amount);
+    depositId = _stake(amount, delegatee, beneficiary, owner);
   }
 
   function increaseStake(uint256 depositId, uint96 amount) external {
@@ -206,14 +168,12 @@ contract RewardsDistribution is
     ds.staking.stakeToken.safeTransferFrom(msg.sender, proxy, amount);
   }
 
-  function redelegate(
-    uint256 depositId,
-    address delegatee
-  ) external onlyOperatorOrSpace(delegatee) {
+  function redelegate(uint256 depositId, address delegatee) external {
     RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
       .layout();
     StakingRewards.Deposit storage deposit = ds.staking.depositById[depositId];
     _revertIfNotDepositOwner(deposit.owner);
+    _revertIfNotOperatorOrSpace(delegatee);
 
     ds.staking.redelegate(deposit, delegatee, _getCommissionRate(delegatee));
 
@@ -269,14 +229,12 @@ contract RewardsDistribution is
       .layout();
     // If the beneficiary is a space, only the operator can claim the reward
     if (_isSpace(beneficiary)) {
-      SpaceDelegationStorage.Layout storage sd = SpaceDelegationStorage
-        .layout();
-      address operator = sd.operatorBySpace[beneficiary];
-      _checkClaimer(operator);
+      address operator = _getOperatorBySpace(beneficiary);
+      _revertIfNotClaimer(operator);
     }
     // If the beneficiary is an operator, only the claimer can claim the reward
     else if (_isOperator(beneficiary)) {
-      _checkClaimer(beneficiary);
+      _revertIfNotClaimer(beneficiary);
     }
     // If the beneficiary is not an operator or space, only the beneficiary can claim the reward
     else if (msg.sender != beneficiary) {
@@ -397,95 +355,5 @@ contract RewardsDistribution is
   /// @inheritdoc IRewardsDistribution
   function beacon() external view returns (address) {
     return RewardsDistributionStorage.layout().beacon;
-  }
-
-  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-  /*                          INTERNAL                          */
-  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-
-  /// @dev Checks if the caller is the claimer of the operator
-  function _checkClaimer(address operator) internal view {
-    NodeOperatorStorage.Layout storage nos = NodeOperatorStorage.layout();
-    address claimer = nos.claimerByOperator[operator];
-    if (msg.sender != claimer) {
-      CustomRevert.revertWith(RewardsDistribution__NotClaimer.selector);
-    }
-  }
-
-  /// @dev Returns the commission rate of the operator or space
-  function _getCommissionRate(
-    address delegatee
-  ) internal view returns (uint256) {
-    // If the delegatee is a space, get the operator
-    if (_isSpace(delegatee)) {
-      SpaceDelegationStorage.Layout storage sd = SpaceDelegationStorage
-        .layout();
-      delegatee = sd.operatorBySpace[delegatee];
-    }
-    NodeOperatorStorage.Layout storage nos = NodeOperatorStorage.layout();
-    return nos.commissionByOperator[delegatee];
-  }
-
-  /// @dev Checks if the delegatee is an operator
-  function _isOperator(address delegatee) internal view returns (bool) {
-    NodeOperatorStorage.Layout storage nos = NodeOperatorStorage.layout();
-    return nos.operators.contains(delegatee);
-  }
-
-  /// @dev Checks if the delegatee is a space
-  function _isSpace(address delegatee) internal view returns (bool) {
-    SpaceDelegationStorage.Layout storage sd = SpaceDelegationStorage.layout();
-    return sd.operatorBySpace[delegatee] != address(0);
-  }
-
-  /// @dev Reverts if the delegatee is not an operator or space
-  function _onlyOperatorOrSpace(address delegatee) internal view {
-    if (!(_isOperator(delegatee) || _isSpace(delegatee))) {
-      CustomRevert.revertWith(RewardsDistribution__NotOperatorOrSpace.selector);
-    }
-  }
-
-  /// @dev Reverts if the caller is not the owner of the deposit
-  function _revertIfNotDepositOwner(address owner) internal view {
-    if (msg.sender != owner) {
-      CustomRevert.revertWith(RewardsDistribution__NotDepositOwner.selector);
-    }
-  }
-
-  function _revertIfPastDeadline(uint256 deadline) internal view {
-    if (block.timestamp > deadline) {
-      CustomRevert.revertWith(RewardsDistribution__ExpiredDeadline.selector);
-    }
-  }
-
-  function _revertIfSignatureIsNotValidNow(
-    address signer,
-    bytes32 hash,
-    bytes calldata signature
-  ) internal view {
-    bool _isValid = SignatureCheckerLib.isValidSignatureNowCalldata(
-      signer,
-      hash,
-      signature
-    );
-    if (!_isValid) {
-      CustomRevert.revertWith(RewardsDistribution__InvalidSignature.selector);
-    }
-  }
-
-  function _deployDelegationProxy(
-    uint256 depositId,
-    address delegatee
-  ) internal returns (address proxy) {
-    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
-      .layout();
-    proxy = LibClone.deployDeterministicERC1967BeaconProxy(
-      ds.beacon,
-      bytes32(depositId)
-    );
-    ds.proxyById[depositId] = proxy;
-    DelegationProxy(proxy).initialize(ds.staking.stakeToken, delegatee);
-
-    emit DelegationProxyDeployed(depositId, delegatee, proxy);
   }
 }
---
+++ contracts/src/base/registry/facets/distribution/v2/RewardsDistributionBase.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+// interfaces
+import {IRewardsDistributionBase} from "./IRewardsDistribution.sol";
+
+// libraries
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {LibClone} from "solady/utils/LibClone.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {SignatureCheckerLib} from "solady/utils/SignatureCheckerLib.sol";
+import {CustomRevert} from "contracts/src/utils/libraries/CustomRevert.sol";
+import {NodeOperatorStorage} from "contracts/src/base/registry/facets/operator/NodeOperatorStorage.sol";
+import {SpaceDelegationStorage} from "contracts/src/base/registry/facets/delegation/SpaceDelegationStorage.sol";
+import {StakingRewards} from "./StakingRewards.sol";
+import {RewardsDistributionStorage} from "./RewardsDistributionStorage.sol";
+
+// contracts
+import {DelegationProxy} from "./DelegationProxy.sol";
+
+abstract contract RewardsDistributionBase is IRewardsDistributionBase {
+  using EnumerableSet for EnumerableSet.AddressSet;
+  using SafeTransferLib for address;
+  using StakingRewards for StakingRewards.Layout;
+
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                           STAKING                          */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+  function _stake(
+    uint96 amount,
+    address delegatee,
+    address beneficiary,
+    address owner
+  ) internal returns (uint256 depositId) {
+    _revertIfNotOperatorOrSpace(delegatee);
+
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    depositId = ds.staking.stake(
+      owner,
+      amount,
+      delegatee,
+      beneficiary,
+      _getCommissionRate(delegatee)
+    );
+
+    address proxy = _deployDelegationProxy(depositId, delegatee);
+    ds.staking.stakeToken.safeTransferFrom(msg.sender, proxy, amount);
+  }
+
+  function _deployDelegationProxy(
+    uint256 depositId,
+    address delegatee
+  ) internal returns (address proxy) {
+    RewardsDistributionStorage.Layout storage ds = RewardsDistributionStorage
+      .layout();
+    proxy = LibClone.deployDeterministicERC1967BeaconProxy(
+      ds.beacon,
+      bytes32(depositId)
+    );
+    ds.proxyById[depositId] = proxy;
+    DelegationProxy(proxy).initialize(ds.staking.stakeToken, delegatee);
+
+    emit DelegationProxyDeployed(depositId, delegatee, proxy);
+  }
+
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                          OPERATOR                          */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+  /// @dev Returns the commission rate of the operator or space
+  function _getCommissionRate(
+    address delegatee
+  ) internal view returns (uint256) {
+    // If the delegatee is a space, get the operator
+    if (_isSpace(delegatee)) {
+      SpaceDelegationStorage.Layout storage sd = SpaceDelegationStorage
+        .layout();
+      delegatee = sd.operatorBySpace[delegatee];
+    }
+    NodeOperatorStorage.Layout storage nos = NodeOperatorStorage.layout();
+    return nos.commissionByOperator[delegatee];
+  }
+
+  /// @dev Checks if the delegatee is an operator
+  function _isOperator(address delegatee) internal view returns (bool) {
+    NodeOperatorStorage.Layout storage nos = NodeOperatorStorage.layout();
+    // TODO: check operator status
+    return nos.operators.contains(delegatee);
+  }
+
+  /// @dev Checks if the delegatee is a space
+  function _isSpace(address delegatee) internal view returns (bool) {
+    SpaceDelegationStorage.Layout storage sd = SpaceDelegationStorage.layout();
+    return sd.operatorBySpace[delegatee] != address(0);
+  }
+
+  /// @dev Returns the operator of the space if it exists
+  function _getOperatorBySpace(
+    address space
+  ) internal view returns (address operator) {
+    SpaceDelegationStorage.Layout storage sd = SpaceDelegationStorage.layout();
+    operator = sd.operatorBySpace[space];
+    if (!_isOperator(operator)) {
+      CustomRevert.revertWith(RewardsDistribution__NotOperatorOrSpace.selector);
+    }
+  }
+
+  /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+  /*                        SANITY CHECKS                       */
+  /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+  /// @dev Reverts if the delegatee is not an operator or space
+  function _revertIfNotOperatorOrSpace(address delegatee) internal view {
+    if (!(_isOperator(delegatee) || _isSpace(delegatee))) {
+      CustomRevert.revertWith(RewardsDistribution__NotOperatorOrSpace.selector);
+    }
+  }
+
+  /// @dev Reverts if the caller is not the owner of the deposit
+  function _revertIfNotDepositOwner(address owner) internal view {
+    if (msg.sender != owner) {
+      CustomRevert.revertWith(RewardsDistribution__NotDepositOwner.selector);
+    }
+  }
+
+  /// @dev Checks if the caller is the claimer of the operator
+  function _revertIfNotClaimer(address operator) internal view {
+    NodeOperatorStorage.Layout storage nos = NodeOperatorStorage.layout();
+    address claimer = nos.claimerByOperator[operator];
+    if (msg.sender != claimer) {
+      CustomRevert.revertWith(RewardsDistribution__NotClaimer.selector);
+    }
+  }
+
+  function _revertIfPastDeadline(uint256 deadline) internal view {
+    if (block.timestamp > deadline) {
+      CustomRevert.revertWith(RewardsDistribution__ExpiredDeadline.selector);
+    }
+  }
+
+  function _revertIfSignatureIsNotValidNow(
+    address signer,
+    bytes32 hash,
+    bytes calldata signature
+  ) internal view {
+    bool _isValid = SignatureCheckerLib.isValidSignatureNowCalldata(
+      signer,
+      hash,
+      signature
+    );
+    if (!_isValid) {
+      CustomRevert.revertWith(RewardsDistribution__InvalidSignature.selector);
+    }
+  }
+}
