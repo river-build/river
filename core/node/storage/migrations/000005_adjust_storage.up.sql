af855651e26e7397b8f6c40f99f6d3e54ae4efda
Serge Khorun <serge@hntlabs.com>
2024-10-26 16:48:46 -0700

    Refactor database schema and migration scripts for storage optimization and trigger enhancements.

--- core/node/storage/migrations/000005_adjust_storage.up.sql
+++ core/node/storage/migrations/000005_adjust_storage.up.sql
@@ -1,16 +1,50 @@
--- Alter CHAR(64) columns to be INLINE and UNCOMPRESSED
-ALTER TABLE miniblock_candidates ALTER COLUMN stream_id SET STORAGE PLAIN;
-ALTER TABLE miniblock_candidates ALTER COLUMN block_hash SET STORAGE PLAIN;
+CREATE TABLE IF NOT EXISTS singlenodekey (
+  uuid VARCHAR NOT NULL,
+  storage_connection_time TIMESTAMP NOT NULL,
+  info VARCHAR NOT NULL
+);
 
-ALTER TABLE es ALTER COLUMN stream_id SET STORAGE PLAIN;
+CREATE OR REPLACE FUNCTION notify_on_upsert()
+    RETURNS TRIGGER
+AS
+$$
+    BEGIN
+    PERFORM pg_notify(TG_TABLE_NAME, TG_TABLE_SCHEMA);
+    RETURN NEW;
+    END;
+$$ LANGUAGE PLPGSQL;
 
-ALTER TABLE miniblocks ALTER COLUMN stream_id SET STORAGE PLAIN;
+DROP TRIGGER IF EXISTS notify_on_singlenodekey_upserts on singlenodekey;
+CREATE TRIGGER notify_on_singlenodekey_upserts
+    AFTER INSERT OR UPDATE
+    ON singlenodekey
+    FOR EACH ROW
+    EXECUTE PROCEDURE notify_on_upsert();
 
-ALTER TABLE minipools ALTER COLUMN stream_id SET STORAGE PLAIN;
+CREATE TABLE IF NOT EXISTS es (
+  stream_id CHAR(64) PRIMARY KEY STORAGE PLAIN,
+  latest_snapshot_miniblock BIGINT NOT NULL);
 
--- Alter BYTEA columns to be UNCOMPRESSED
-ALTER TABLE miniblock_candidates ALTER COLUMN blockdata SET STORAGE EXTERNAL;
+CREATE TABLE IF NOT EXISTS miniblocks (
+  stream_id CHAR(64) NOT NULL STORAGE PLAIN,
+  seq_num BIGINT NOT NULL,
+  blockdata BYTEA NOT NULL STORAGE EXTERNAL,
+  PRIMARY KEY (stream_id, seq_num)
+  ) PARTITION BY LIST (stream_id);
 
-ALTER TABLE miniblocks ALTER COLUMN blockdata SET STORAGE EXTERNAL;
+CREATE TABLE IF NOT EXISTS minipools (
+  stream_id CHAR(64) NOT NULL STORAGE PLAIN,
+  generation BIGINT NOT NULL ,
+  slot_num BIGINT NOT NULL ,
+  envelope BYTEA STORAGE EXTERNAL,
+  PRIMARY KEY (stream_id, generation, slot_num)
+  ) PARTITION BY LIST (stream_id);
 
-ALTER TABLE minipools ALTER COLUMN envelope SET STORAGE EXTERNAL;
+
+CREATE TABLE IF NOT EXISTS miniblock_candidates (
+  stream_id CHAR(64) NOT NULL STORAGE PLAIN,
+  seq_num BIGINT NOT NULL,
+  block_hash CHAR(64) NOT NULL STORAGE PLAIN,
+  blockdata BYTEA NOT NULL STORAGE EXTERNAL,
+  PRIMARY KEY (stream_id, seq_num, block_hash)
+) PARTITION BY LIST (stream_id);
