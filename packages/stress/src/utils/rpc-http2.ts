664acbd00ec2c466a08e63c621032fab80015a19
texuf <texuf.eth@gmail.com>
2024-10-30 11:26:34 -0700

    GetStream improvements
    1. fix up logs so they are easier to differentiate
    2. Throw the error when a stream fails to fetch from the rpc client. Otherwise we are left in a very bad state where we returned a stream that is not registered in any way. This will cause serious issues (like trying to wait for an event that never comes)
    3. Retry deadline_exceeded errors
    4. use the rpc default timeout for the wait for timeouts
    
    seeing tons of these in the stress tests now that we have timeouts on gamma
    ```
    _ConnectError: [deadline_exceeded] the operation timed out
        at Timeout.listener (/monorepo/node_modules/@connectrpc/connect/dist/esm/protocol/signals.js:62:26)
        at listOnTimeout (node:internal/timers:581:17)
        at process.processTimers (node:internal/timers:519:7) {
      rawMessage: 'the operation timed out',
      code: 4,
      metadata: Headers {},
      details: [],
      cause: undefined
    }
    ```

--- packages/stress/src/utils/rpc-http2.ts
+++ packages/stress/src/utils/rpc-http2.ts
@@ -5,10 +5,14 @@ import {
     loggingInterceptor,
     randomUrlSelector,
     retryInterceptor,
+    StreamRpcClientOptions,
     type RetryParams,
 } from '@river-build/sdk'
 
-export type StreamRpcClient = PromiseClient<typeof StreamService> & { url?: string }
+export type StreamHttp2RpcClient = PromiseClient<typeof StreamService> & {
+    url: string
+    opts: StreamRpcClientOptions
+}
 
 let nextRpcClientNum = 0
 
@@ -16,7 +20,7 @@ export function makeHttp2StreamRpcClient(
     urls: string,
     retryParams: RetryParams = { maxAttempts: 3, initialRetryDelay: 2000, maxRetryDelay: 6000 },
     refreshNodeUrl?: () => Promise<string>,
-): StreamRpcClient {
+): StreamHttp2RpcClient {
     const transportId = nextRpcClientNum++
     const url = randomUrlSelector(urls)
     const options: ConnectTransportOptions = {
@@ -26,7 +30,7 @@ export function makeHttp2StreamRpcClient(
             loggingInterceptor(transportId),
             retryInterceptor({ ...retryParams, refreshNodeUrl }),
         ],
-        defaultTimeoutMs: 30000,
+        defaultTimeoutMs: 20000,
     }
     if (!process.env.RIVER_DEBUG_TRANSPORT) {
         options.useBinaryFormat = true
@@ -39,7 +43,11 @@ export function makeHttp2StreamRpcClient(
     }
     const transport = createConnectTransport(options)
 
-    const client: StreamRpcClient = createPromiseClient(StreamService, transport) as StreamRpcClient
+    const client: StreamHttp2RpcClient = createPromiseClient(
+        StreamService,
+        transport,
+    ) as StreamHttp2RpcClient
     client.url = url
+    client.opts = { retryParams, defaultTimeoutMs: options.defaultTimeoutMs }
     return client
 }
