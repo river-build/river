2bd34e8d3a68d01f23775849467f64f2da43cdfe
Miguel Nascimento <miguelgomes13@live.com>
2024-11-04 19:24:48 -0300

    generated

--- packages/generated/tsup.config.ts
+++ packages/generated/tsup.config.ts
@@ -3,9 +3,20 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: [
     "./dev/**/*.ts",
-    "./deployments/**/*.json"
+    "./dev/**/*.json",
+    './deployments/**/*.ts',
+    './deployments/**/*.json',
+    './config/**/*.ts',
+    './config/**/*.json',
   ],
-  outDir: "./dist",
-  format: ["esm"],
+  outDir: './dist',
+  format: ['esm'],
   dts: true,
-});
+  esbuildOptions(options) {
+      options.loader = {
+          ...options.loader,
+          '.json': 'copy', // Copy JSON files to the dist folder
+      }
+  },
+})
+
