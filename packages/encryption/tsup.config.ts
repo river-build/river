d528d06f6745867cadab099d6d8bcd66c63e8ac1
Miguel Nascimento <miguelgomes13@live.com>
2024-11-06 18:19:03 -0300

    build & export in cjs too

--- packages/encryption/tsup.config.ts
+++ packages/encryption/tsup.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ['esm'],
+    format: ['esm', 'cjs'],
     dts: true,
     clean: true,
     sourcemap: true,
+    outExtension: ({ format }) => ({
+        js: format === 'esm' ? '.js' : '.cjs',
+    }),
 })
--- packages/web3/tsup.config.ts
+++ packages/web3/tsup.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['./src/index.ts', './src/v3/index.ts'],
-    format: ['esm'],
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
     dts: true,
     clean: true,
     sourcemap: true,
+    outExtension: ({ format }) => ({
+        js: format === 'esm' ? '.js' : '.cjs',
+    }),
 })
