8879b7d65d9b415e536587d6662b824429e5ad6c
Miguel Nascimento <miguelgomes13@live.com>
2024-11-06 20:08:57 -0300

    update vitest & set workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,14 +1,6 @@
 import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
-    {
-        test: {
-            name: '@river-build/react-sdk',
-            environment: 'node',
-            include: ['./packages/react-sdk/src/**/*.test.ts'],
-            testTimeout: 10_000,
-            // Later, we can use the `setupFiles` option to run a setup file before each test.
-            // setupFiles: ['./packages/react-sdk/test/setup.ts'],
-        },
-    },
+    // matches every package that has a vitest.config.ts file
+    'packages/*/vitest.config.ts',
 ])
