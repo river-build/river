f3b3fe9ec3f8bbfc5937c011aea1ef2bcf9276b1
Evan Salzbrenner <15717901+salzbrenner@users.noreply.github.com>
2024-11-06 12:02:06 -0700

    SpaceDapp: WalletLink.unlinkCaller

--- packages/sdk/src/util.test.ts
+++ packages/sdk/src/util.test.ts
@@ -704,6 +704,28 @@ export function twoNftRuleData(
     return treeToRuleData(root)
 }
 
+export async function unlinkCaller(
+    rootSpaceDapp: ISpaceDapp,
+    rootWallet: ethers.Wallet,
+    caller: ethers.Wallet,
+) {
+    const walletLink = rootSpaceDapp.getWalletLink()
+    let txn: ContractTransaction | undefined
+    try {
+        txn = await walletLink.removeCallerLink(caller)
+    } catch (err: any) {
+        const parsedError = walletLink.parseError(err)
+        log('linkWallets error', parsedError)
+    }
+
+    expect(txn).toBeDefined()
+    const receipt = await txn?.wait()
+    expect(receipt!.status).toEqual(1)
+
+    const linkedWallets = await walletLink.getLinkedWallets(rootWallet.address)
+    expect(linkedWallets).not.toContain(caller.address)
+}
+
 export async function unlinkWallet(
     rootSpaceDapp: ISpaceDapp,
     rootWallet: ethers.Wallet,
