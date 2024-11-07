6ef96561e9eb379f0bb13aea0bf3c54aef625fb5
Miguel Nascimento <miguelgomes13@live.com>
2024-11-07 16:24:29 -0300

    prettier

--- packages/proto/scripts/watch_proto_files.cjs
+++ packages/proto/scripts/watch_proto_files.cjs
@@ -1,40 +1,38 @@
-const fs = require("fs");
-const { exec } = require("child_process");
-const debounce = require("lodash.debounce");
+const fs = require('fs')
+const { exec } = require('child_process')
+const debounce = require('lodash.debounce')
 
-const currentDirectory = process.cwd();
-const protocolDirectory = process.cwd() + "/../../protocol";
-const buildCommand = "yarn build";
+const currentDirectory = process.cwd()
+const protocolDirectory = process.cwd() + '/../../protocol'
+const buildCommand = 'yarn build'
 
 const handleFileChange = debounce((eventType, filename) => {
-  console.log(`Detected ${eventType} in ${filename}, running build command...`);
-  exec(buildCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-    } else if (stderr) {
-      console.error(`Stderr: ${stderr}`);
-    } else if (stdout) {
-      console.log(`Stdout: ${stdout}`);
-    }
-    console.log("Done.");
-  });
-}, 1000);
+    console.log(`Detected ${eventType} in ${filename}, running build command...`)
+    exec(buildCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`)
+        } else if (stderr) {
+            console.error(`Stderr: ${stderr}`)
+        } else if (stdout) {
+            console.log(`Stdout: ${stdout}`)
+        }
+        console.log('Done.')
+    })
+}, 1000)
 
 const watcher = fs.watch(currentDirectory, (eventType, filename) => {
-  if (filename.endsWith(".proto")) {
-    handleFileChange(eventType, filename);
-  }
-});
+    if (filename.endsWith('.proto')) {
+        handleFileChange(eventType, filename)
+    }
+})
 
 const watcher2 = fs.watch(protocolDirectory, (eventType, filename) => {
-  if (filename.endsWith(".proto")) {
-    handleFileChange(eventType, filename);
-  }
-});
+    if (filename.endsWith('.proto')) {
+        handleFileChange(eventType, filename)
+    }
+})
 
-console.log(
-  `Watching ${currentDirectory} and ${protocolDirectory} for changes...`,
-);
+console.log(`Watching ${currentDirectory} and ${protocolDirectory} for changes...`)
 
 // To close the watcher when you're done
 // watcher.close();
--- packages/proto/scripts/watch_proto_files_go.cjs
+++ packages/proto/scripts/watch_proto_files_go.cjs
@@ -1,47 +1,47 @@
-const fs = require("fs");
-const { exec } = require("child_process");
-const debounce = require("lodash.debounce");
+const fs = require('fs')
+const { exec } = require('child_process')
+const debounce = require('lodash.debounce')
 
-const currentDirectory = process.cwd();
-const toolsDirectory = process.cwd() + "/../../core/node/protocol_extensions";
-const protocolDirectory = process.cwd() + "/../../protocol";
-const buildCommand = "cd ../../core/node && go generate -v -x protocol/gen.go";
+const currentDirectory = process.cwd()
+const toolsDirectory = process.cwd() + '/../../core/node/protocol_extensions'
+const protocolDirectory = process.cwd() + '/../../protocol'
+const buildCommand = 'cd ../../core/node && go generate -v -x protocol/gen.go'
 
 const handleFileChange = debounce((eventType, filename) => {
-  console.log(`Detected ${eventType} in ${filename}, running build command...`);
-  exec(buildCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-    } else if (stderr) {
-      console.error(`stderr:\n${stderr}\nstdout:\n${stdout}`);
-    } else if (stdout) {
-      console.log(`Stdout: ${stdout}`);
-    }
-    console.log("Done.");
-  });
-}, 1000);
+    console.log(`Detected ${eventType} in ${filename}, running build command...`)
+    exec(buildCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`)
+        } else if (stderr) {
+            console.error(`stderr:\n${stderr}\nstdout:\n${stdout}`)
+        } else if (stdout) {
+            console.log(`Stdout: ${stdout}`)
+        }
+        console.log('Done.')
+    })
+}, 1000)
 
 const watcher = fs.watch(currentDirectory, (eventType, filename) => {
-  if (filename.endsWith(".proto")) {
-    handleFileChange(eventType, filename);
-  }
-});
+    if (filename.endsWith('.proto')) {
+        handleFileChange(eventType, filename)
+    }
+})
 
 const watcher2 = fs.watch(toolsDirectory, (eventType, filename) => {
-  if (filename.endsWith(".go")) {
-    handleFileChange(eventType, filename);
-  }
-});
+    if (filename.endsWith('.go')) {
+        handleFileChange(eventType, filename)
+    }
+})
 
 const watcher3 = fs.watch(protocolDirectory, (eventType, filename) => {
-  if (filename.endsWith(".proto")) {
-    handleFileChange(eventType, filename);
-  }
-});
+    if (filename.endsWith('.proto')) {
+        handleFileChange(eventType, filename)
+    }
+})
 
 console.log(
-  `Watching ${currentDirectory} && ${toolsDirectory} && ${protocolDirectory} for changes...`,
-);
+    `Watching ${currentDirectory} && ${toolsDirectory} && ${protocolDirectory} for changes...`,
+)
 
 // To close the watcher when you're done
 // watcher.close();
