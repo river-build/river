a836201ede7455c714cd9de2bf10403930c30cb4
Miguel Nascimento <miguelgomes13@live.com>
2024-11-07 01:27:44 -0300

    run ci multi ne using vitest

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -5,15 +5,17 @@ export default defineWorkspace([
     'packages/*/vitest.config.ts',
     // TODO: group these tests
     {
-        extends: './packages/sdk/vitest.config.ts',
+        extends: './vitest.config.ts',
         test: {
             name: 'sdk-ne',
             include: ['./packages/sdk/**/*.test.ts'],
             setupFiles: ['./packages/sdk/vitest.setup.ts'],
+            testTimeout: 120_000,
+            hookTimeout: 120_000,
         },
     },
     {
-        extends: './packages/sdk/vitest.config.ts',
+        extends: './vitest.config.ts',
         test: {
             name: 'sdk-ent',
             include: [
@@ -21,14 +23,18 @@ export default defineWorkspace([
                 './packages/sdk/**/*.test.entitlements-v2.ts',
             ],
             setupFiles: ['./packages/sdk/vitest.setup.ts'],
+            testTimeout: 120_000,
+            hookTimeout: 120_000,
         },
     },
     {
-        extends: './packages/sdk/vitest.config.ts',
+        extends: './vitest.config.ts',
         test: {
             name: 'sdk-ent-legacy',
             include: ['./packages/sdk/**/*.test.entitlements.ts'],
             setupFiles: ['./packages/sdk/vitest.setup.ts'],
+            testTimeout: 120_000,
+            hookTimeout: 120_000,
         },
     },
 ])
