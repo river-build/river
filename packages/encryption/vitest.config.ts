2fe9afb26b712fd5872f78dd5a3858ffa8fcd282
Miguel Nascimento <miguelgomes13@live.com>
2024-11-06 21:11:18 -0300

    encryption

---
+++ packages/encryption/vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineProject } from 'vitest/config'
+
+export default defineProject({
+    test: {
+        environment: 'node',
+        globals: true,
+        setupFiles: ['./vitest.setup.ts'],
+    },
+})
