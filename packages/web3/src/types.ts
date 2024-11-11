4b45e47dd9ae2176951e9334e743c9749e13424a
Miguel Nascimento <miguelgomes13@live.com>
2024-11-11 18:31:24 -0300

    docs: space info

--- packages/web3/src/types.ts
+++ packages/web3/src/types.ts
@@ -1,12 +1,28 @@
 export interface SpaceInfo {
+    /** The on-chain address of the space. */
     address: string
-    createdAt: string // is BigNumber
+    /**
+     * The timestamp of when the space was created.
+     * It is a {@link https://docs.ethers.org/v5/api/utils/bignumber/ ethers.BigNumber} serialized as a `string`.
+     */
+    createdAt: string
+    /** The River `spaceId` of the space. */
     networkId: string
+    /** The name of the space. */
     name: string
+    /** The on-chain address of the space owner. */
     owner: string
+    /** Whether the space is disabled. */
     disabled: boolean
+    /** A short description of the space. */
     shortDescription: string
+    /** The long description of the space. */
     longDescription: string
-    tokenId: string // is BigNumber
+    /**
+     * The on-chain token id of the space. All spaces are indexed by their token id in the SpaceOwner collection.
+     * It is a {@link https://docs.ethers.org/v5/api/utils/bignumber/ ethers.BigNumber} serialized as a `string`.
+     */
+    tokenId: string
+    /** The URI of the space. */
     uri: string
 }
