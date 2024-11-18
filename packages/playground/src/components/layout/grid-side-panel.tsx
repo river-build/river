ede141bf87b7dbde0ab30052e2034479d2c76de1
Miguel Nascimento <miguelgomes13@live.com>
2024-11-18 16:54:29 -0300

    playground: support system theme if dark mode

--- packages/playground/src/components/layout/grid-side-panel.tsx
+++ packages/playground/src/components/layout/grid-side-panel.tsx
@@ -6,11 +6,11 @@ export const GridSidePanel = ({
     side?: React.ReactNode
 }) => {
     return (
-        <main className="grid flex-1 grid-cols-[1fr_3fr] bg-zinc-50">
-            <aside className="flex max-h-[calc(100dvh-64px)] w-full flex-col gap-4 overflow-y-auto border-r border-zinc-200 bg-zinc-50 px-4 py-2">
+        <main className="grid flex-1 grid-cols-[1fr_3fr] bg-zinc-50 dark:bg-zinc-900">
+            <aside className="flex max-h-[calc(100dvh-64px)] w-full flex-col gap-4 overflow-y-auto border-r border-zinc-200 bg-zinc-50 px-4 py-2 dark:border-zinc-800 dark:bg-zinc-900">
                 {side}
             </aside>
-            <section className="flex max-h-[calc(100dvh-64px)] w-full flex-col gap-4 overflow-y-auto bg-zinc-50 px-4 py-2">
+            <section className="flex max-h-[calc(100dvh-64px)] w-full flex-col gap-4 overflow-y-auto bg-zinc-50 px-4 py-2 dark:bg-zinc-900">
                 {main}
             </section>
         </main>
