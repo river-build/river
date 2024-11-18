3aaf1e1e5fcdf3e78d655ab9cdc15cd124c051d6
g <5714678+giuseppecrj@users.noreply.github.com>
2024-11-18 12:37:05 -0500

    update tiered log pricing v3 for new create town flow

--- contracts/src/spaces/facets/membership/pricing/tiered/TieredLogPricingOracleV2.sol
+++ contracts/src/spaces/facets/membership/pricing/tiered/TieredLogPricingOracleV2.sol
@@ -107,9 +107,14 @@ contract TieredLogPricingOracleV2 is IMembershipPricing, IntrospectionFacet {
   }
 
   function _calculateStablePrice(
-    uint256,
+    uint256 freeAllocation,
     uint256 totalMinted
   ) internal pure returns (uint256) {
+    // Free allocation handling
+    if (freeAllocation > 0 && totalMinted < freeAllocation) {
+      return 0;
+    }
+
     // Define minted tiers
     uint256 tier1 = 100;
     uint256 tier2 = 1000;
@@ -120,20 +125,23 @@ contract TieredLogPricingOracleV2 is IMembershipPricing, IntrospectionFacet {
     uint256 basePriceTier2 = 1000; // $10.00
     uint256 basePriceTier3 = 10000; // $100.00
 
-    if (totalMinted <= tier1) {
-      // Logarithmic scaling for tier 1
-      uint256 logScale = _calculateLogScale(totalMinted == 0 ? 1 : totalMinted);
-      return logScale / 2 + basePriceTier1; // Dividing by 2 is an arbitrary scaling factor
-    } else if (totalMinted <= tier2) {
+    if (totalMinted > tier3) {
+      return basePriceTier3;
+    } else if (totalMinted > tier2) {
       // Logarithmic scaling for tier 2
       uint256 logScale = _calculateLogScale(totalMinted);
-      return logScale * 3 + basePriceTier1;
-    } else if (totalMinted <= tier3) {
-      // Logarithmic scaling for tier 3
-      uint256 logScale = _calculateLogScale(totalMinted);
       return logScale * 22 + basePriceTier2;
+    } else if (totalMinted > tier1) {
+      // Logarithmic scaling for tier 1
+      uint256 logScale = _calculateLogScale(totalMinted);
+      return logScale * 3 + basePriceTier1;
     } else {
-      return basePriceTier3;
+      // Below tier 1
+      if (freeAllocation > totalMinted) return 0;
+
+      // Logarithmic scaling for tier 0
+      uint256 logScale = _calculateLogScale(totalMinted == 0 ? 1 : totalMinted);
+      return logScale / 2 + basePriceTier1; // // Dividing by 2 is an arbitrary scaling factor
     }
   }
 
