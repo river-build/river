eed83f66d777a08fc2cfc830311bbfd5843f2c45
Miguel Gomes <miguelgomes13@live.com>
2024-11-20 12:30:15 -0300

    react-sdk: only one timeline hook (#1563)
    Cleaning the timeline hook using the same idea from #1386

--- packages/playground/src/routes/t/channel-timeline.tsx
+++ packages/playground/src/routes/t/channel-timeline.tsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom'
-import { useChannel, useChannelTimeline, useThreadMap } from '@river-build/react-sdk'
+import { useChannel, useThreadMap, useTimeline } from '@river-build/react-sdk'
 import { Timeline } from '@/components/blocks/timeline'
 import { ChannelProvider } from '@/hooks/current-channel'
 import { useCurrentSpaceId } from '@/hooks/current-space'
@@ -8,7 +8,7 @@ export const ChannelTimelineRoute = () => {
     const { channelId } = useParams<{ channelId: string }>()
     const spaceId = useCurrentSpaceId()
     const { data: channel } = useChannel(spaceId, channelId!)
-    const { data: events } = useChannelTimeline(spaceId, channelId!)
+    const { data: events } = useTimeline(channelId!)
     const { data: threadMap } = useThreadMap(spaceId, channelId!)
     return (
         <ChannelProvider channelId={channelId}>
