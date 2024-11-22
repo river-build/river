ceea1944eb5acdc1d7a7e84657d51b9479a41ae9
Jakub Zalewski <jakub@hntlabs.com>
2024-11-21 23:24:41 -0500

    Refactor EpochKeyStore to simplify key state management
    Removed redundant types and improved key state handling by consolidating multiple maps into a single epochKeySates map. This simplifies the code and improves readability, making key state retrieval and setting more efficient.

--- packages/sdk/src/mls/epochKeyStore.ts
+++ packages/sdk/src/mls/epochKeyStore.ts
@@ -1,8 +1,6 @@
 import {
     CipherSuite as MlsCipherSuite,
     HpkeCiphertext,
-    HpkePublicKey,
-    HpkeSecretKey,
     Secret as MlsSecret,
 } from '@river-build/mls-rs-wasm'
 import { MlsStore } from './mlsStore'
@@ -101,11 +99,6 @@ export class EpochKeyService {
     }
 }
 
-type HpkeCiphertextBytes = Uint8Array & { __brand: 'HpkeCiphertext' }
-type MlsSecretBytes = Uint8Array & { __brand: 'MlsSecret' }
-type HpkeSecretKeyBytes = Uint8Array & { __brand: 'HpkeSecretKey' }
-type HpkePublicKeyBytes = Uint8Array & { __brand: 'HpkePublicKey' }
-
 type EpochKeyId = string & { __brand: 'EpochKeyId' }
 
 function epochKeyId(streamId: string, epoch: bigint): EpochKeyId {
@@ -113,120 +106,27 @@ function epochKeyId(streamId: string, epoch: bigint): EpochKeyId {
 }
 
 export class EpochKeyStore {
-    private sealedEpochSecrets: Map<EpochKeyId, HpkeCiphertextBytes> = new Map()
-    private openEpochSecrets: Map<EpochKeyId, MlsSecretBytes> = new Map()
-    private secretKeys: Map<EpochKeyId, HpkeSecretKeyBytes> = new Map()
-    private publicKeys: Map<EpochKeyId, HpkePublicKeyBytes> = new Map()
-    private announceds: Map<EpochKeyId, boolean> = new Map()
+    private epochKeySates: Map<EpochKeyId, EpochKeyState> = new Map()
     log: DLogger
 
     constructor(log: DLogger) {
-        // this.log = log.extend(shortenHexString(streamId))
         this.log = log
-    }
-
-    private getEpochKeyState(epochId: EpochKeyId): EpochKeyState | undefined {
-        const derivedKeys = this.getDerivedKeys(epochId)
-        const openEpochSecret = this.getOpenEpochSecret(epochId)
-        const sealedEpochSecret = this.getSealedEpochSecret(epochId)
-        const announced = this.announceds.get(epochId) || false
-
-        if (derivedKeys) {
-            if (!openEpochSecret) {
-                throw new Error('Derived keys without open epoch secret')
-            }
-            return {
-                status: 'EPOCH_KEY_OPEN',
-                secretKey: derivedKeys.secretKey,
-                publicKey: derivedKeys.publicKey,
-                openEpochSecret,
-                sealedEpochSecret,
-                announced,
-            }
-        }
-        if (sealedEpochSecret) {
-            return {
-                status: 'EPOCH_KEY_SEALED',
-                sealedEpochSecret,
-                announced,
-            }
-        }
-        return undefined
     }
 
     public async getEpochKey(streamId: string, epoch: bigint): Promise<EpochKey | undefined> {
         const epochId: EpochKeyId = epochKeyId(streamId, epoch)
-        const state = this.getEpochKeyState(epochId)
+        const state = this.epochKeySates.get(epochId)
         if (!state) {
             return undefined
         }
         return new EpochKey(streamId, epoch, state)
     }
 
-    // TODO: Optimise this
     public async setEpochKeyState(epochKey: EpochKey): Promise<void> {
         const streamId = epochKey.streamId
         const epoch = epochKey.epoch
         const state = epochKey.state
         const epochId = epochKeyId(streamId, epoch)
-        switch (state.status) {
-            case 'EPOCH_KEY_SEALED':
-                this.addSealedEpochSecret(epochId, state.sealedEpochSecret)
-                this.announceds.set(epochId, state.announced)
-                break
-            case 'EPOCH_KEY_OPEN':
-                this.addOpenEpochSecret(epochId, state.openEpochSecret)
-                this.addDerivedKeys(epochId, state)
-                if (state.sealedEpochSecret) {
-                    this.addSealedEpochSecret(epochId, state.sealedEpochSecret)
-                }
-                this.announceds.set(epochId, state.announced)
-                break
-        }
-    }
-
-    private addSealedEpochSecret(epochId: EpochKeyId, sealedEpochSecret: HpkeCiphertext) {
-        const sealedEpochSecretBytes = sealedEpochSecret.toBytes() as HpkeCiphertextBytes
-        this.sealedEpochSecrets.set(epochId, sealedEpochSecretBytes)
-    }
-
-    private getSealedEpochSecret(epochId: EpochKeyId): HpkeCiphertext | undefined {
-        const sealedEpochSecretBytes = this.sealedEpochSecrets.get(epochId)
-        if (sealedEpochSecretBytes) {
-            return HpkeCiphertext.fromBytes(sealedEpochSecretBytes)
-        }
-        return undefined
-    }
-
-    private addOpenEpochSecret(epochId: EpochKeyId, openEpochSecret: MlsSecret) {
-        const openEpochSecretBytes = openEpochSecret.toBytes() as MlsSecretBytes
-        this.openEpochSecrets.set(epochId, openEpochSecretBytes)
-    }
-
-    private getOpenEpochSecret(epochId: EpochKeyId): MlsSecret | undefined {
-        const openEpochSecretBytes = this.openEpochSecrets.get(epochId)
-        if (openEpochSecretBytes) {
-            return MlsSecret.fromBytes(openEpochSecretBytes)
-        }
-        return undefined
-    }
-
-    private addDerivedKeys(epochId: EpochKeyId, keys: DerivedKeys) {
-        const secretKeyBytes = keys.secretKey.toBytes() as HpkeSecretKeyBytes
-        const publicKeyBytes = keys.publicKey.toBytes() as HpkePublicKeyBytes
-        this.secretKeys.set(epochId, secretKeyBytes)
-        this.publicKeys.set(epochId, publicKeyBytes)
-    }
-
-    private getDerivedKeys(epochId: EpochKeyId): DerivedKeys | undefined {
-        const secretKeyBytes = this.secretKeys.get(epochId)
-        const publicKeyBytes = this.publicKeys.get(epochId)
-        if (secretKeyBytes && publicKeyBytes) {
-            return {
-                secretKey: HpkeSecretKey.fromBytes(secretKeyBytes),
-                publicKey: HpkePublicKey.fromBytes(publicKeyBytes),
-            }
-        }
-        return undefined
+        this.epochKeySates.set(epochId, state)
     }
 }
