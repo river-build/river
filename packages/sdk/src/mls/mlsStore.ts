bcc8b1573f011d78125a0c0d84aa7ac30af65ab1
Jakub Zalewski <jakub@hntlabs.com>
2024-12-03 16:09:26 +0100

    Use new EpochKey API

--- packages/sdk/src/mls/mlsStore.ts
+++ packages/sdk/src/mls/mlsStore.ts
@@ -1,28 +1,27 @@
 import { bin_toString, DLogger } from '@river-build/dlog'
 import Dexie, { Table } from 'dexie'
+import { IEpochKeyStore } from './epochKeyStore'
+import { EpochKey, EpochKeyState } from './epochKey'
+import {
+    HpkeCiphertext,
+    HpkePublicKey,
+    HpkeSecretKey,
+    Secret as MlsSecret,
+} from '@river-build/mls-rs-wasm'
 
-interface Key {
+interface EpochKeyDTO {
     streamId: string
     epoch: string
-    bytes: Uint8Array
+    announced: boolean
+    status: string
+    openEpochSecret?: Uint8Array
+    sealedEpochSecret?: Uint8Array
+    secretKey?: Uint8Array
+    publicKey?: Uint8Array
 }
 
-interface Group {
-    streamId: string
-    bytes: Uint8Array
-}
-
-interface EpochSecret {
-    streamId: string
-    epoch: string
-    bytes: Uint8Array
-}
-
-export class MlsStore extends Dexie {
-    secretKeys!: Table<Key>
-    publicKeys!: Table<Key>
-    groups!: Table<Group>
-    epochSecrets!: Table<EpochSecret>
+export class MlsStore extends Dexie implements IEpochKeyStore {
+    epochKeys!: Table<EpochKeyDTO>
     log: DLogger
 
     constructor(deviceKey: Uint8Array, log: DLogger) {
@@ -32,50 +31,94 @@ export class MlsStore extends Dexie {
         this.log = log
 
         this.version(1).stores({
-            secretKeys: '[streamId+epoch]',
-            publicKeys: '[streamId+epoch]',
-            groups: 'streamId',
-            epochSecrets: '[streamId+epoch]',
+            epochKeys: '[streamId+epoch]',
         })
     }
 
-    async saveSecretKey(streamId: string, epoch: bigint, secretKey: Uint8Array): Promise<void> {
-        await this.secretKeys.put({ streamId, epoch: epoch.toString(), bytes: secretKey })
-    }
+    async getEpochKey(streamId: string, epoch: bigint): Promise<EpochKey | undefined> {
+        const epochKeyDTO = await this.epochKeys.get([streamId, epoch.toString()])
+        if (epochKeyDTO === undefined) {
+            return undefined
+        }
 
-    async getSecretKey(streamId: string, epoch: bigint): Promise<Uint8Array | undefined> {
-        // this could either throw or return undefined
-        const record = await this.secretKeys.get([streamId, epoch.toString()])
-        return record?.bytes
-    }
+        if (
+            epochKeyDTO.status === 'EPOCH_KEY_SEALED' &&
+            epochKeyDTO.sealedEpochSecret !== undefined
+        ) {
+            const sealedEpochSecret = HpkeCiphertext.fromBytes(epochKeyDTO.sealedEpochSecret)
+            const state: EpochKeyState = {
+                status: 'EPOCH_KEY_SEALED',
+                announced: epochKeyDTO.announced,
+                sealedEpochSecret,
+            }
+            return new EpochKey(streamId, epoch, state)
+        }
+        if (
+            epochKeyDTO.status === 'EPOCH_KEY_OPEN' &&
+            epochKeyDTO.openEpochSecret !== undefined &&
+            epochKeyDTO.secretKey !== undefined &&
+            epochKeyDTO.publicKey !== undefined
+        ) {
+            const openEpochSecret = MlsSecret.fromBytes(epochKeyDTO.openEpochSecret)
+            const secretKey = HpkeSecretKey.fromBytes(epochKeyDTO.secretKey)
+            const publicKey = HpkePublicKey.fromBytes(epochKeyDTO.publicKey)
+            const state: EpochKeyState = {
+                status: 'EPOCH_KEY_OPEN',
+                openEpochSecret,
+                secretKey,
+                publicKey,
+                announced: epochKeyDTO.announced,
+            }
+            if (epochKeyDTO.sealedEpochSecret !== undefined) {
+                state.sealedEpochSecret = HpkeCiphertext.fromBytes(epochKeyDTO.sealedEpochSecret)
+            }
+            return new EpochKey(streamId, epoch, state)
+        }
 
-    async savePublicKey(streamId: string, epoch: bigint, secretKey: Uint8Array): Promise<void> {
-        await this.publicKeys.put({ streamId, epoch: epoch.toString(), bytes: secretKey })
+        throw new Error(`Invalid epoch key state: ${streamId} ${epoch} ${epochKeyDTO.status}`)
     }
+    async setEpochKeyState(epochKey: EpochKey): Promise<void> {
+        const streamId = epochKey.streamId
+        const epoch = epochKey.epoch.toString()
+        let announced: boolean
+        let openEpochSecret: Uint8Array | undefined
+        let sealedEpochSecret: Uint8Array | undefined
+        let epochKeyDTO: EpochKeyDTO
+        let publicKey: Uint8Array | undefined
+        let secretKey: Uint8Array | undefined
 
-    async getPublicKey(streamId: string, epoch: bigint): Promise<Uint8Array | undefined> {
-        // this could either throw or return undefined
-        const record = await this.publicKeys.get([streamId, epoch.toString()])
-        return record?.bytes
-    }
+        switch (epochKey.state.status) {
+            case 'EPOCH_KEY_SEALED':
+                sealedEpochSecret = epochKey.state.sealedEpochSecret.toBytes()
+                announced = epochKey.state.announced
+                epochKeyDTO = {
+                    streamId,
+                    epoch,
+                    announced,
+                    status: 'EPOCH_KEY_SEALED',
+                    sealedEpochSecret,
+                }
 
-    async saveGroup(streamId: string, group: Uint8Array): Promise<void> {
-        await this.groups.put({ streamId, bytes: group })
-    }
-
-    async getGroup(streamId: string): Promise<Uint8Array | undefined> {
-        // this could either throw or return undefined
-        const record = await this.groups.get(streamId)
-        return record?.bytes
-    }
-
-    async saveEpochSecret(streamId: string, epoch: bigint, secret: Uint8Array): Promise<void> {
-        await this.epochSecrets.put({ streamId, epoch: epoch.toString(), bytes: secret })
-    }
-
-    async getEpochSecret(streamId: string, epoch: bigint): Promise<Uint8Array | undefined> {
-        // this could either throw or return undefined
-        const record = await this.epochSecrets.get([streamId, epoch.toString()])
-        return record?.bytes
+                return this.epochKeys.put(epochKeyDTO)
+            case 'EPOCH_KEY_OPEN':
+                openEpochSecret = epochKey.state.openEpochSecret.toBytes()
+                announced = epochKey.state.announced
+                publicKey = epochKey.state.publicKey.toBytes()
+                secretKey = epochKey.state.secretKey.toBytes()
+                if (epochKey.state.sealedEpochSecret !== undefined) {
+                    sealedEpochSecret = epochKey.state.sealedEpochSecret.toBytes()
+                }
+                epochKeyDTO = {
+                    streamId,
+                    epoch,
+                    announced,
+                    status: 'EPOCH_KEY_OPEN',
+                    openEpochSecret,
+                    sealedEpochSecret,
+                    secretKey,
+                    publicKey,
+                }
+                return this.epochKeys.put(epochKeyDTO)
+        }
     }
 }
