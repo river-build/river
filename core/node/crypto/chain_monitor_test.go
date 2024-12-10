river-build/river

commit 262b07db4d294d9b59ef5c305643340003fd4c8b

Fix double start of chain monitor in tests (#1759)
Was started from both test harness and node start functions

core/xchain/server/server.go
@@ -120,7 +120,6 @@ func New(
 		if err != nil {
 			return nil, err
 		}
-		riverChain.StartChainMonitor(ctx)
 	}
 
 	registryContract, err := registries.NewRiverRegistryContract(
@@ -187,7 +186,6 @@ func New(
 	}
 
 	log.Info("Start processing entitlement check requests", "startBlock", baseChain.InitialBlockNum)
-	baseChain.StartChainMonitor(ctx)
 
 	decoder, err := crypto.NewEVMErrorDecoder(
 		base.IEntitlementCheckerMetaData,

core/node/rpc/server.go
@@ -184,8 +184,6 @@ func (s *Service) start(opts *ServerStartOpts) error {
 		return AsRiverError(err).Message("Failed to init cache and sync").LogError(s.defaultLogger)
 	}
 
-	s.riverChain.StartChainMonitor(s.serverCtx)
-
 	s.initHandlers()
 
 	s.SetStatus("OK")

core/cmd/on_chain_config_cmd.go
@@ -246,7 +246,6 @@ func submitConfig(ctx context.Context, cfg *config.Config, args []setArgs) error
 	if err != nil {
 		return err
 	}
-	blockchain.StartChainMonitor(ctx)
 
 	caller, err := river.NewRiverConfigV1Transactor(cfg.RegistryContract.Address, blockchain.Client)
 	if err != nil {

core/cmd/register_xchain_node_cmd.go
@@ -84,7 +84,6 @@ func registerImpl(operatorKeyfile string, userConfirmationMessage string, regist
 	if err != nil {
 		return fmt.Errorf("unable to instantiate base chain client: %s", err)
 	}
-	baseChain.StartChainMonitor(ctx)
 
 	checker, err := base.NewIEntitlementChecker(cmdConfig.GetEntitlementContractAddress(), baseChain.Client)
 	if err != nil {

core/node/crypto/blockchain.go
@@ -144,6 +144,14 @@ func NewBlockchainWithClient(
 		}
 	}
 
+	monitor.Start(
+		ctx,
+		client,
+		initialBlockNum,
+		time.Duration(cfg.BlockTimeMs)*time.Millisecond,
+		metrics,
+	)
+
 	return bc, nil
 }
 
@@ -159,15 +167,4 @@ func (b *Blockchain) GetBlockNumber(ctx context.Context) (BlockNumber, error) {
 		return 0, AsRiverError(err, Err_CANNOT_CONNECT).Message("Cannot retrieve block number").Func("GetBlockNumber")
 	}
 	return BlockNumber(n), nil
-}
-
-// TODO: refactor to start chain monitor in NewBlockchainWithClient
-func (b *Blockchain) StartChainMonitor(ctx context.Context) {
-	go b.ChainMonitor.RunWithBlockPeriod(
-		ctx,
-		b.Client,
-		b.InitialBlockNum,
-		time.Duration(b.Config.BlockTimeMs)*time.Millisecond,
-		b.Metrics,
-	)
 }

core/node/crypto/chain_monitor.go
@@ -17,9 +17,8 @@ import (
 type (
 	// ChainMonitor monitors the EVM chain for new blocks and/or events.
 	ChainMonitor interface {
-		// RunWithBlockPeriod the monitor until the given ctx expires using the client to interact
-		// with the chain.
-		RunWithBlockPeriod(
+		// Start starts the chain monitor in background. Goroutine is going to be running until the given ctx is cancelled.
+		Start(
 			ctx context.Context,
 			client BlockchainClient,
 			initialBlock BlockNumber,
@@ -75,6 +74,7 @@ type (
 		mu        sync.Mutex
 		builder   chainMonitorBuilder
 		fromBlock *big.Int
+		started   bool
 	}
 
 	// ChainMonitorPollInterval determines the next poll interval for the chain monitor
@@ -215,6 +215,23 @@ func (cm *chainMonitor) OnStopped(cb OnChainMonitorStoppedCallback) {
 	cm.builder.OnChainMonitorStopped(cb)
 }
 
+func (cm *chainMonitor) Start(
+	ctx context.Context,
+	client BlockchainClient,
+	initialBlock BlockNumber,
+	blockPeriod time.Duration,
+	metrics infra.MetricsFactory,
+) {
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+	if cm.started {
+		dlog.FromCtx(ctx).Error("chain monitor already started")
+		return
+	}
+	cm.started = true
+	go cm.runWithBlockPeriod(ctx, client, initialBlock, blockPeriod, metrics)
+}
+
 // RunWithBlockPeriod monitors the chain the given client is connected to and calls the
 // associated callback for each event that matches its filter.
 //
@@ -225,7 +242,7 @@ func (cm *chainMonitor) OnStopped(cb OnChainMonitorStoppedCallback) {
 // Callbacks are called in the order they were added and
 // aren't called concurrently to ensure that events are processed in the order
 // they were received.
-func (cm *chainMonitor) RunWithBlockPeriod(
+func (cm *chainMonitor) runWithBlockPeriod(
 	ctx context.Context,
 	client BlockchainClient,
 	initialBlock BlockNumber,

core/node/crypto/chain_txpool_test.go
@@ -11,10 +11,11 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stretchr/testify/require"
+
 	"github.com/river-build/river/core/node/base/test"
 	"github.com/river-build/river/core/node/crypto"
 	"github.com/river-build/river/core/node/infra"
-	"github.com/stretchr/testify/require"
 )
 
 func TestNewTransactionPoolWithReplaceTx(t *testing.T) {
@@ -193,7 +194,7 @@ func TestReplacementTxOnBoot(t *testing.T) {
 	monitor := crypto.NewChainMonitor()
 	blockNum, err := bc.Client.BlockNumber(ctx)
 	require.NoError(err, "unable to get block number")
-	go monitor.RunWithBlockPeriod(
+	monitor.Start(
 		ctx, bc.Client, crypto.BlockNumber(blockNum), 100*time.Millisecond,
 		infra.NewMetricsFactory(nil, "", ""))
 

core/node/crypto/testutil.go
@@ -480,8 +480,6 @@ func makeTestBlockchain(
 		panic(err)
 	}
 
-	bc.StartChainMonitor(ctx)
-
 	return bc
 }
 
@@ -654,7 +652,7 @@ type NoopChainMonitor struct{}
 
 var _ ChainMonitor = NoopChainMonitor{}
 
-func (NoopChainMonitor) RunWithBlockPeriod(
+func (NoopChainMonitor) Start(
 	context.Context,
 	BlockchainClient,
 	BlockNumber,

commit 43409cbad6e7742e79758776058f91a6f07284fa

Fix reading history on start in xchain  (#1770)
I broke it in #1759

core/xchain/server/server.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log/slog"
 	"math/big"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 
@@ -33,15 +34,16 @@ import (
 type (
 	// xchain reads entitlement requests from base chain and writes the result after processing back to base.
 	xchain struct {
-		workerID        int
-		checker         *base.IEntitlementChecker
-		checkerABI      *abi.ABI
-		checkerContract *bind.BoundContract
-		baseChain       *crypto.Blockchain
-		evmErrDecoder   *crypto.EvmErrorDecoder
-		config          *config.Config
-		cancel          context.CancelFunc
-		evaluator       *entitlement.Evaluator
+		workerID            int
+		checker             *base.IEntitlementChecker
+		checkerABI          *abi.ABI
+		checkerContract     *bind.BoundContract
+		baseChain           *crypto.Blockchain
+		baseChainStartBlock crypto.BlockNumber
+		evmErrDecoder       *crypto.EvmErrorDecoder
+		config              *config.Config
+		cancel              context.CancelFunc
+		evaluator           *entitlement.Evaluator
 
 		riverChain       *crypto.Blockchain
 		registryContract *registries.RiverRegistryContract
@@ -79,6 +81,9 @@ type XChain interface {
 	Run(ctx context.Context)
 	Stop()
 }
+
+// MaxHistoricalBlockOffset is the maximum number of blocks to go back when searching for a start block.
+const MaxHistoricalBlockOffset crypto.BlockNumber = 100
 
 // New creates a new xchain instance that reads entitlement requests from Base,
 // processes the requests and writes the results back to Base.
@@ -176,16 +181,22 @@ func New(
 			return nil, err
 		}
 	}
-	// determine from which block to start processing entitlement check requests
-	startBlock, err := util.StartBlockNumberWithHistory(ctx, baseChain.Client, cfg.History)
-	if err != nil {
-		return nil, err
-	}
-	if startBlock < baseChain.InitialBlockNum.AsUint64() {
-		baseChain.InitialBlockNum = crypto.BlockNumber(startBlock)
+
+	baseChainStartBlock := baseChain.InitialBlockNum
+
+	if cfg.History > 0 {
+		history := min(cfg.History, time.Minute)
+		blockTime := time.Duration(baseChain.Config.BlockTimeMs) * time.Millisecond
+		numBlocksToSubtract := crypto.BlockNumber(history/blockTime + 1)
+		numBlocksToSubtract = min(numBlocksToSubtract, MaxHistoricalBlockOffset)
+		if baseChainStartBlock > numBlocksToSubtract {
+			baseChainStartBlock = baseChainStartBlock - numBlocksToSubtract
+		} else {
+			baseChainStartBlock = 1
+		}
 	}
 
-	log.Info("Start processing entitlement check requests", "startBlock", baseChain.InitialBlockNum)
+	log.Info("Start processing entitlement check requests", "startBlock", baseChainStartBlock)
 
 	decoder, err := crypto.NewEVMErrorDecoder(
 		base.IEntitlementCheckerMetaData,
@@ -204,14 +215,15 @@ func New(
 		"name",
 	)
 	x := &xchain{
-		workerID:        workerID,
-		checker:         checker,
-		checkerABI:      checkerABI,
-		checkerContract: checkerContract,
-		baseChain:       baseChain,
-		evmErrDecoder:   decoder,
-		config:          cfg,
-		evaluator:       evaluator,
+		workerID:            workerID,
+		checker:             checker,
+		checkerABI:          checkerABI,
+		checkerContract:     checkerContract,
+		baseChain:           baseChain,
+		baseChainStartBlock: baseChainStartBlock,
+		evmErrDecoder:       decoder,
+		config:              cfg,
+		evaluator:           evaluator,
 
 		riverChain:       riverChain,
 		registryContract: registryContract,
@@ -313,7 +325,7 @@ func (x *xchain) Run(ctx context.Context) {
 
 	// register callback for Base EntitlementCheckRequested events
 	x.baseChain.ChainMonitor.OnContractWithTopicsEvent(
-		x.baseChain.InitialBlockNum,
+		x.baseChainStartBlock,
 		entitlementAddress,
 		[][]common.Hash{{x.checkerABI.Events["EntitlementCheckRequested"].ID}},
 		onEntitlementCheckRequestedCallback)

core/xchain/util/chain.go
@@ -1,73 +1,0 @@
-package util
-
-import (
-	"context"
-	"math/big"
-	"time"
-
-	"github.com/river-build/river/core/node/base"
-	"github.com/river-build/river/core/node/protocol"
-
-	"github.com/river-build/river/core/node/crypto"
-)
-
-const (
-	// MaxHistoricalBlockOffset is the maximum number of blocks to go back when searching for a start block.
-	MaxHistoricalBlockOffset uint64 = 100
-)
-
-func StartBlockNumber(
-	ctx context.Context,
-	client crypto.BlockchainClient,
-	deadline time.Time,
-) (uint64, error) {
-	head, err := client.HeaderByNumber(ctx, nil)
-	if err != nil {
-		return 0, err
-	}
-
-	// determine the first block with block.Time >= deadline, start at the maximum offset of 100 blocks
-	// and do a binary search to find the first block that satisfies the criteria.
-	var (
-		last  = head.Number.Uint64()
-		start = uint64(0)
-		step  = MaxHistoricalBlockOffset
-	)
-
-	if step < last { // consider only last 100 blocks if chain > 100 blocks long
-		start = last - step
-	}
-
-	for start <= last {
-		middle := (start + last) / 2
-		middleHead, err := client.HeaderByNumber(ctx, new(big.Int).SetUint64(middle))
-		if err != nil {
-			return 0, base.AsRiverError(err, protocol.Err_UNAVAILABLE).Message("Unable to determine start block")
-		}
-		t := time.Unix(int64(middleHead.Time), 0)
-		if t.After(deadline) {
-			last = middle - 1
-		} else if t.Before(deadline) {
-			start = middle + 1
-		} else {
-			return middle, nil
-		}
-	}
-
-	return start, nil
-}
-
-// StartBlockNumberWithHistory returns the first block with a timestamp that is equal or larger than the given history
-// to look back. It will only go back MaxHistoricalBlockOffset blocks.
-func StartBlockNumberWithHistory(
-	ctx context.Context,
-	client crypto.BlockchainClient,
-	history time.Duration,
-) (uint64, error) {
-	if history < 0 || history > time.Minute {
-		return 0, base.AsRiverError(nil, protocol.Err_INVALID_ARGUMENT).
-			Message("History must be in range [0s, 1m]")
-	}
-
-	return StartBlockNumber(ctx, client, time.Now().Add(-history))
-}

core/node/crypto/chain_monitor_test.go
@@ -19,6 +19,7 @@ import (
 	"github.com/river-build/river/core/contracts/river"
 	"github.com/river-build/river/core/node/base/test"
 	"github.com/river-build/river/core/node/crypto"
+	"github.com/river-build/river/core/node/infra"
 )
 
 func TestChainMonitorBlocks(t *testing.T) {
@@ -504,6 +505,122 @@ func TestContractAllEventsFromPast(t *testing.T) {
 	require.EqualValues(events, historicalContractEvents, "unexpected logs")
 	require.Equal(nodeCount, len(historicalAllEvents), "unexpected NodeAdded logs count")
 	require.EqualValues(events, historicalContractEvents, "unexpected logs")
+}
+
+func TestContracEventsWithTopicsBeforeStart(t *testing.T) {
+	require := require.New(t)
+	ctx, cancel := test.NewTestContext()
+	defer cancel()
+
+	tc, err := crypto.NewBlockchainTestContext(ctx, crypto.TestParams{})
+	require.NoError(err)
+	defer tc.Close()
+
+	var (
+		owner     = tc.DeployerBlockchain
+		nodeCount = 5
+
+		nodeRegistryABI, _ = abi.JSON(strings.NewReader(river.NodeRegistryV1MetaData.ABI))
+	)
+
+	// register several nodes
+	var (
+		pendingTx     crypto.TransactionPoolPendingTransaction
+		nodeAddresses = make([]common.Address, nodeCount)
+	)
+	for i := range nodeCount {
+		wallet, err := crypto.NewWallet(ctx)
+		require.NoError(err, "new wallet")
+		nodeAddresses[i] = wallet.Address
+		pendingTx, err = owner.TxPool.Submit(
+			ctx,
+			"RegisterNode",
+			func(opts *bind.TransactOpts) (*types.Transaction, error) {
+				return tc.NodeRegistry.RegisterNode(
+					opts,
+					wallet.Address,
+					fmt.Sprintf("https://node%d.river.test", i),
+					river.NodeStatus_NotInitialized,
+				)
+			},
+		)
+		require.NoError(err, "register node")
+	}
+
+	require.NoError(err)
+
+	// generate some blocks
+	N := 5
+	for i := 0; i < N; i++ {
+		tc.Commit(ctx)
+	}
+
+	receipt, err := pendingTx.Wait(ctx)
+	require.NoError(err)
+	require.Equal(crypto.TransactionResultSuccess, receipt.Status)
+
+	events1C := make(chan types.Log, nodeCount)
+	tc.DeployerBlockchain.ChainMonitor.OnContractWithTopicsEvent(
+		0,
+		tc.RiverRegistryAddress,
+		[][]common.Hash{{nodeRegistryABI.Events["NodeAdded"].ID}},
+		func(ctx context.Context, event types.Log) {
+			events1C <- event
+		},
+	)
+
+	events1 := []types.Log{}
+	require.Eventually(func() bool {
+		for {
+			select {
+			case event := <-events1C:
+				events1 = append(events1, event)
+			default:
+				return len(events1) == nodeCount
+			case <-ctx.Done():
+				panic("context cancelled")
+			}
+		}
+	}, 10*time.Second, 10*time.Millisecond, "unexpected NodeAdded logs count: first monitor reading historical events")
+
+	for i := 0; i < N; i++ {
+		tc.Commit(ctx)
+	}
+
+	// Create a new chain monitor and start it.
+	chainMonitor := crypto.NewChainMonitor()
+	chainMonitor.Start(ctx, tc.Client(), tc.BlockNum(ctx), 100*time.Millisecond, infra.NewMetricsFactory(nil, "", ""))
+
+	events2C := make(chan types.Log, nodeCount)
+
+	// register a callback from block 0, which is before starting block for the new monitor
+	chainMonitor.OnContractWithTopicsEvent(
+		crypto.BlockNumber(0),
+		tc.RiverRegistryAddress,
+		[][]common.Hash{{nodeRegistryABI.Events["NodeAdded"].ID}},
+		func(ctx context.Context, event types.Log) {
+			events2C <- event
+		},
+	)
+
+	events2 := []types.Log{}
+	require.Eventually(func() bool {
+		for {
+			select {
+			case event := <-events2C:
+				events2 = append(events2, event)
+			default:
+				return len(events2) == nodeCount
+			case <-ctx.Done():
+				panic("context cancelled")
+			}
+		}
+	}, 10*time.Second, 10*time.Millisecond, "unexpected NodeAdded logs count: second monitor reading pre-start events")
+
+	require.EqualValues(events1, events2, "historical events mismatch pre-start events")
+
+	require.Empty(events1C, "unexpected events")
+	require.Empty(events2C, "unexpected events")
 }
 
 type onBlockCollector struct {

core/xchain/util/chain_test.go
@@ -1,121 +1,0 @@
-package util_test
-
-import (
-	"testing"
-	"time"
-
-	"github.com/river-build/river/core/xchain/util"
-
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/stretchr/testify/require"
-
-	"github.com/river-build/river/core/node/base/test"
-	"github.com/river-build/river/core/node/crypto"
-	"github.com/river-build/river/core/node/testutils/testfmt"
-)
-
-// TestStartBlockNumberRange ensures that utils.StartBlockNumber ensures that the duration to go back falls within an
-// acceptable range.
-func TestStartBlockNumberRange(t *testing.T) {
-	t.Parallel()
-
-	var (
-		require     = require.New(t)
-		ctx, cancel = test.NewTestContext()
-		tests       = []struct {
-			history time.Duration
-		}{
-			{history: -1},
-			{history: time.Minute + 1},
-		}
-	)
-	defer cancel()
-
-	for _, tt := range tests {
-		_, err := util.StartBlockNumberWithHistory(ctx, nil, tt.history)
-		require.Error(err, "history expected out of range")
-	}
-}
-
-// TestStartBlockNumber tests that utils.StartBlockNumber returns the correct block number given a deadline.
-func TestStartBlockNumber(t *testing.T) {
-	t.Parallel()
-
-	require := require.New(t)
-	ctx, cancel := test.NewTestContext()
-	defer cancel()
-
-	btc, err := crypto.NewBlockchainTestContext(ctx, crypto.TestParams{})
-	require.NoError(err, "instantiate blockchain test context")
-	defer btc.Close()
-
-	// create several blocks so we can go back in history
-	var (
-		blocks []*types.Header
-		client = btc.Client()
-		tests  []struct {
-			deadline time.Time
-			exp      uint64
-		}
-	)
-
-	for i := 0; i < 4; i++ {
-		<-time.After(time.Duration(1+i) * time.Second)
-		btc.Commit(ctx)
-
-		header, err := client.HeaderByNumber(ctx, nil)
-		require.NoError(err, "get header by number")
-		blocks = append(blocks, header)
-		testfmt.Logf(t, "block %d: %d", header.Number.Uint64(), header.Time)
-	}
-
-	for _, b := range blocks {
-		tests = append(tests, struct {
-			deadline time.Time
-			exp      uint64
-		}{
-			deadline: time.Unix(int64(b.Time), 0),
-			exp:      b.Number.Uint64(),
-		})
-	}
-
-	for _, tt := range tests {
-		start, err := util.StartBlockNumber(ctx, client, tt.deadline)
-		require.NoError(err, "start block number")
-		require.Equalf(tt.exp, start, "unexpected start block number - deadline: %d", tt.deadline.Unix())
-	}
-}
-
-// TestStartBlockNumberLongChain tests that utils.StartBlockNumber returns the correct block number given a deadline
-// on a chain that is longer than MaxHistoricalBlockOffset.
-func TestStartBlockNumberLongChain(t *testing.T) {
-	t.Parallel()
-
-	require := require.New(t)
-	ctx, cancel := test.NewTestContext()
-	defer cancel()
-
-	btc, err := crypto.NewBlockchainTestContext(ctx, crypto.TestParams{})
-	require.NoError(err, "instantiate blockchain test context")
-	defer btc.Close()
-
-	// create a chain that has more blocks that MaxHistoricalBlockOffset
-	var (
-		blocks []*types.Header
-		client = btc.Client()
-	)
-
-	for range int(util.MaxHistoricalBlockOffset) + 25 {
-		btc.Commit(ctx)
-		header, err := client.HeaderByNumber(ctx, nil)
-		require.NoError(err, "get header by number")
-		blocks = append(blocks, header)
-	}
-
-	// go no more than util.MaxHistoricalBlockOffset blocks back
-	exp := blocks[len(blocks)-1-int(util.MaxHistoricalBlockOffset)].Number.Uint64()
-
-	start, err := util.StartBlockNumberWithHistory(ctx, client, time.Minute)
-	require.NoError(err, "start block number")
-	require.Equal(exp, start, "unexpected start block number")
-}