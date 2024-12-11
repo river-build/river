e389e520eb89cac4161ca527cee7cd5bbc9f5dda
Crystal Lemire <crystal@hntlabs.com>
2024-12-10 17:41:22 -0800

    Refactor complex entitlements so that long running tests get their own file to bring all CI times for entitlement test files down to about 2 minutes or less.

--- packages/sdk/src/tests/multi/entitlements/channelsWithComplexEntitlements.test.ts
+++ packages/sdk/src/tests/multi/entitlements/channelsWithComplexEntitlements.test.ts
@@ -27,7 +27,6 @@ import {
     CheckOperationType,
     treeToRuleData,
     encodeThresholdParams,
-    createExternalNFTStruct,
 } from '@river-build/web3'
 
 const log = dlog('csb:test:channelsWithComplexEntitlements')
@@ -114,77 +113,6 @@ describe('channelsWithComplexEntitlements', () => {
 
         // Join alice to the channel
         await expectUserCanJoinChannel(alice, aliceSpaceDapp, spaceId, channelId!)
-    })
-
-    test('user with only one entitlement from 3-nested NFT rule data can join channel', async () => {
-        const testNft1 = 'TestNft1'
-        const testNft2 = 'TestNft2'
-        const testNft3 = 'TestNft3'
-        const testNftAddress = await TestERC721.getContractAddress(testNft1)
-        const testNftAddress2 = await TestERC721.getContractAddress(testNft2)
-        const testNftAddress3 = await TestERC721.getContractAddress(testNft3)
-
-        const ruleData = createExternalNFTStruct([testNftAddress, testNftAddress2, testNftAddress3])
-        const {
-            alice,
-            alicesWallet,
-            aliceSpaceDapp,
-            bob,
-            carol,
-            carolsWallet,
-            carolSpaceDapp,
-            spaceId,
-            defaultChannelId,
-            channelId,
-        } = await setupChannelWithCustomRole([], ruleData)
-
-        // Set up additional users
-        const {
-            alice: dave,
-            alicesWallet: davesWallet,
-            aliceSpaceDapp: daveSpaceDapp,
-            aliceProvider: daveProvider,
-        } = await setupWalletsAndContexts()
-        // Add Dave to the space
-        await expectUserCanJoin(
-            spaceId,
-            defaultChannelId,
-            'dave',
-            dave,
-            daveSpaceDapp,
-            davesWallet.address,
-            daveProvider.wallet,
-        )
-
-        // Alice initially cannot join because she has no nft
-        await expectUserCannotJoinChannel(alice, aliceSpaceDapp, spaceId, channelId!)
-
-        // Alice, Carol and Dave will each have one of the three NFTs, all should be able to join.
-        // Mint an nft for alice - she should be able to join now
-        await TestERC721.publicMint(testNft1, alicesWallet.address as Address)
-
-        // Wait 2 seconds for the negative auth cache on the client to expire
-        await new Promise((f) => setTimeout(f, 2000))
-
-        // Validate alice can join the channel
-        await expectUserCanJoinChannel(alice, aliceSpaceDapp, spaceId, channelId!)
-
-        // Mint an nft for carol - she should be able to join now
-        await TestERC721.publicMint(testNft2, carolsWallet.address as Address)
-
-        // Validate carol can join the channel
-        await expectUserCanJoinChannel(carol, carolSpaceDapp, spaceId, channelId!)
-
-        // Mint an nft for dave - he should be able to join now
-        await TestERC721.publicMint(testNft3, davesWallet.address as Address)
-
-        // Validate dave can join the channel
-        await expectUserCanJoinChannel(dave, daveSpaceDapp, spaceId, channelId!)
-
-        await bob.stopSync()
-        await alice.stopSync()
-        await carol.stopSync()
-        await dave.stopSync()
     })
 
     test('twoNftGateJoinPass', async () => {
--- packages/sdk/src/tests/multi/entitlements/spaceWithComplexEntitlements.test.ts
+++ packages/sdk/src/tests/multi/entitlements/spaceWithComplexEntitlements.test.ts
@@ -9,7 +9,6 @@ import {
     expectUserCannotJoinSpace,
     expectUserCanJoin,
     linkWallets,
-    setupWalletsAndContexts,
     twoNftRuleData,
 } from '../../testUtils'
 import { dlog } from '@river-build/dlog'
@@ -22,7 +21,6 @@ import {
     TestERC721,
     treeToRuleData,
     encodeThresholdParams,
-    createExternalNFTStruct,
 } from '@river-build/web3'
 
 const log = dlog('csb:test:spaceWithComplexEntitlements')
@@ -248,99 +246,6 @@ describe('spaceWithComplexEntitlements', () => {
         const doneStart = Date.now()
         await bob.stopSync()
         await alice.stopSync()
-        log('Done', Date.now() - doneStart)
-    })
-
-    test('user with only one entitlement from 3-nested NFT rule data can join space', async () => {
-        const testNft1 = 'TestNft1'
-        const testNft2 = 'TestNft2'
-        const testNft3 = 'TestNft3'
-        const testNftAddress = await TestERC721.getContractAddress(testNft1)
-        const testNftAddress2 = await TestERC721.getContractAddress(testNft2)
-        const testNftAddress3 = await TestERC721.getContractAddress(testNft3)
-
-        const ruleData = createExternalNFTStruct([testNftAddress, testNftAddress2, testNftAddress3])
-
-        const {
-            alice,
-            bob,
-            carol,
-            aliceSpaceDapp,
-            aliceProvider,
-            carolProvider,
-            carolSpaceDapp,
-            alicesWallet,
-            carolsWallet,
-            spaceId,
-            channelId,
-        } = await createTownWithRequirements({
-            everyone: false,
-            users: [],
-            ruleData,
-        })
-        // Set up additional users to test single ownership of all three nfts.
-        const {
-            alice: dave,
-            alicesWallet: davesWallet,
-            aliceSpaceDapp: daveSpaceDapp,
-            aliceProvider: daveProvider,
-            carol: emily,
-            carolProvider: emilyProvider,
-            carolSpaceDapp: emilySpaceDapp,
-            carolsWallet: emilyWallet,
-        } = await setupWalletsAndContexts()
-
-        // Have alice create her own space so she can initialize her user stream.
-        // Then she will attempt to join the space from herclient, which should fail.
-        await createUserStreamAndSyncClient(
-            alice,
-            aliceSpaceDapp,
-            'alice',
-            await everyoneMembershipStruct(aliceSpaceDapp, alice),
-            aliceProvider.wallet,
-        )
-        await expectUserCannotJoinSpace(spaceId, alice, aliceSpaceDapp, alicesWallet.address)
-
-        await TestERC721.publicMint(testNft1, carolsWallet.address as Address)
-        await expectUserCanJoin(
-            spaceId,
-            channelId,
-            'carol',
-            carol,
-            carolSpaceDapp,
-            carolsWallet.address,
-            carolProvider.wallet,
-        )
-
-        await TestERC721.publicMint(testNft2, davesWallet.address as Address)
-        await expectUserCanJoin(
-            spaceId,
-            channelId,
-            'dave',
-            dave,
-            daveSpaceDapp,
-            davesWallet.address,
-            daveProvider.wallet,
-        )
-
-        await TestERC721.publicMint(testNft3, emilyWallet.address as Address)
-        await expectUserCanJoin(
-            spaceId,
-            channelId,
-            'emily',
-            emily,
-            emilySpaceDapp,
-            emilyWallet.address,
-            emilyProvider.wallet,
-        )
-
-        // kill the clients
-        const doneStart = Date.now()
-        await bob.stopSync()
-        await alice.stopSync()
-        await carol.stopSync()
-        await dave.stopSync()
-        await emily.stopSync()
         log('Done', Date.now() - doneStart)
     })
 })
