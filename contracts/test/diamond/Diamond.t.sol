fb4146dc1cb39d9d6f8a2926ff165b365797b47f
John Terzis <john@hntlabs.com>
2024-12-12 15:00:27 -0700

    NET-158 interact river sparse (#1744)
    - Refactor Diamond, Facet helper abc's with interface to improve code
    quality in interaction* scripts for facet upgrades
    - Introduce InteractRiverAlphaSparse script along similar lines to
    InteractAlphaSparse
    
    Merging this and updating ci will complete RiverDiffing sparsely.

--- contracts/test/diamond/Diamond.t.sol
+++ contracts/test/diamond/Diamond.t.sol
@@ -8,7 +8,14 @@ pragma solidity ^0.8.0;
 //contracts
 import {IDiamond, Diamond} from "contracts/src/diamond/Diamond.sol";
 
-abstract contract DiamondHelper is IDiamond {
+interface IDiamondInitHelper is IDiamond {
+  function diamondInitHelper(
+    address deployer,
+    string[] memory facetNames
+  ) external returns (FacetCut[] memory);
+}
+
+abstract contract DiamondHelper is IDiamondInitHelper {
   string public name = "DiamondHelper";
 
   uint256 private _index = 0;
@@ -45,6 +52,13 @@ abstract contract DiamondHelper is IDiamond {
 
   function baseFacets() internal view returns (FacetCut[] memory) {
     return _cuts;
+  }
+
+  function diamondInitHelper(
+    address, // deployer
+    string[] memory // facetNames
+  ) external virtual returns (FacetCut[] memory) {
+    return new FacetCut[](0);
   }
 
   function _resetIndex() internal {
