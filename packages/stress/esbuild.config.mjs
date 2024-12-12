265c23caa136ce17b86b424992de99128bf85129
Jakub Zalewski <jakub@hntlabs.com>
2024-12-12 16:09:35 +0100

    npx prettier --write esbuild.config.mjs

--- packages/stress/esbuild.config.mjs
+++ packages/stress/esbuild.config.mjs
@@ -3,10 +3,10 @@ import esbuildPluginPino from "esbuild-plugin-pino";
 
 build({
   entryPoints: {
-      'start': './src/start.ts',
-      'demo': './src/demo.ts',
-      // NOTE: For some reason esbuild is not picking it up
-      'mls_rs_wasm_bg': '@river-build/mls-rs-wasm-node/mls_rs_wasm_bg.wasm'
+    start: "./src/start.ts",
+    demo: "./src/demo.ts",
+    // NOTE: For some reason esbuild is not picking it up
+    mls_rs_wasm_bg: "@river-build/mls-rs-wasm-node/mls_rs_wasm_bg.wasm",
   },
   bundle: true,
   sourcemap: "inline",
@@ -17,7 +17,7 @@ build({
   outExtension: { ".js": ".cjs" },
   plugins: [esbuildPluginPino({ transports: ["pino-pretty"] })],
   alias: {
-      '@river-build/mls-rs-wasm': '@river-build/mls-rs-wasm-node'
+    "@river-build/mls-rs-wasm": "@river-build/mls-rs-wasm-node",
   },
   ignoreAnnotations: true,
   assetNames: "[name]",
