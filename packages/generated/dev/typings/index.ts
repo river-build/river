1720d4526aa5ba41445f1ecdd062c25eb99dec7a
Evan Salzbrenner <15717901+salzbrenner@users.noreply.github.com>
2024-12-12 15:02:50 -0700

    Tipping: build contract types and hook up to SpaceDapp

--- packages/generated/dev/typings/index.ts
+++ packages/generated/dev/typings/index.ts
@@ -32,6 +32,7 @@ export type { IRoles } from "./IRoles";
 export type { ISpaceDelegation } from "./ISpaceDelegation";
 export type { ISpaceOwner } from "./ISpaceOwner";
 export type { IStreamRegistry } from "./IStreamRegistry";
+export type { ITipping } from "./ITipping";
 export type { IWalletLink } from "./IWalletLink";
 export type { Member } from "./Member";
 export type { MembershipFacet } from "./MembershipFacet";
@@ -76,6 +77,7 @@ export { IRuleEntitlementV2__factory } from "./factories/IRuleEntitlement.sol/IR
 export { ISpaceDelegation__factory } from "./factories/ISpaceDelegation__factory";
 export { ISpaceOwner__factory } from "./factories/ISpaceOwner__factory";
 export { IStreamRegistry__factory } from "./factories/IStreamRegistry__factory";
+export { ITipping__factory } from "./factories/ITipping__factory";
 export { IWalletLink__factory } from "./factories/IWalletLink__factory";
 export { Member__factory } from "./factories/Member__factory";
 export { MembershipFacet__factory } from "./factories/MembershipFacet__factory";
---
+++ packages/web3/src/v3/ITipping.ts
@@ -0,0 +1,15 @@
+import {
+    ITipping as LocalhostContract,
+    ITippingInterface as LocalhostInterface,
+} from '@river-build/generated/dev/typings/ITipping'
+
+import { ethers } from 'ethers'
+import { BaseContractShim } from './BaseContractShim'
+
+import DevAbi from '@river-build/generated/dev/abis/ITipping.abi.json' assert { type: 'json' }
+
+export class ITipping extends BaseContractShim<LocalhostContract, LocalhostInterface> {
+    constructor(address: string, provider: ethers.providers.Provider | undefined) {
+        super(address, provider, DevAbi)
+    }
+}
