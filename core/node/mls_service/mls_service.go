e7a278b6e7bd73219f1f899e92ca46e66af7cbf4
Erik Olsson <valross2@gmail.com>
2024-12-19 14:56:44 +0100

    add simple info request test

---
+++ core/node/mls_service/mls_service.go
@@ -0,0 +1,26 @@
+package mls_service
+
+import (
+	"context"
+	"log"
+
+	"github.com/river-build/river/core/node/mls_service/mls_tools"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+func InfoRequest() (*mls_tools.InfoResponse, error) {
+	client, err := grpc.NewClient("unix:/tmp/mls_service",
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("NewClient:", err)
+	}
+	defer client.Close()
+
+	mlsClient := mls_tools.NewMslClient(client)
+	info, err := mlsClient.Info(context.Background(), &mls_tools.InfoRequest{})
+
+	if err != nil {
+		return nil, err
+	}
+	return info, nil
+}
