7e168be7f1a672e4ac814ab34667324037f5dd13
Erik Olsson <valross2@gmail.com>
2024-12-27 19:18:57 +0100

    log pwd

--- scripts/build-mls.sh
+++ scripts/build-mls.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+pwd
 cargo build --release --manifest-path ./mls/mls-tools/crates/mlslib/Cargo.toml
 cp ./mls/mls-tools/target/release/libmls* ./core/node/mls_service/
 cp ./mls/mls-tools/target/release/libmls* ./core/bin/
