fbd3a9e5610ccc3fc67948e5c0fe78cd802003be
Erik Olsson <valross2@gmail.com>
2024-12-27 19:55:47 +0100

    final test for today

--- scripts/build-mls.sh
+++ scripts/build-mls.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+mkdir -p ./bin
 cargo build --release --manifest-path ./mls/mls-tools/crates/mlslib/Cargo.toml
 cp ./mls/mls-tools/target/release/libmls* ./core/node/mls_service/
 cp ./mls/mls-tools/target/release/libmls* ./core
+cp ./mls/mls-tools/target/release/libmls* ./bin
