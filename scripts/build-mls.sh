c4e78ca4646af8d5fa405a0545087b5a0bc1ddcf
Erik Olsson <valross2@gmail.com>
2024-12-27 19:33:01 +0100

    Update build-mls.sh

--- scripts/build-mls.sh
+++ scripts/build-mls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir -p ./core/bin
 cargo build --release --manifest-path ./mls/mls-tools/crates/mlslib/Cargo.toml
 cp ./mls/mls-tools/target/release/libmls* ./core/node/mls_service/
-cp ./mls/mls-tools/target/release/libmls* ./core/bin/
+cp ./mls/mls-tools/target/release/libmls* ./lib/
+ldconfig
