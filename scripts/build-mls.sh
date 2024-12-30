e0e1246bb68bd2d28b709b0108a27698c2fb11ae
Erik Olsson <valross2@gmail.com>
2024-12-30 21:51:29 +0100

    cleanup

--- scripts/build-mls.sh
+++ scripts/build-mls.sh
@@ -4,8 +4,4 @@ set -euo pipefail
 mkdir -p ./bin
 cargo build --release --manifest-path ./mls/mls-tools/crates/mlslib/Cargo.toml
 cp ./mls/mls-tools/target/release/libmls* ./core/node/mls_service/
-cp ./mls/mls-tools/target/release/libmls* ./core
-cp ./mls/mls-tools/target/release/libmls* ./bin
 cp ./mls/mls-tools/target/release/libmls* ./
-
-export CGO_LDFLAGS="./libmls_lib.a -ldl"
