0120a9c170dcd20c10c01039d88dc87dcde24096
Erik Olsson <valross2@gmail.com>
2024-12-31 09:44:05 +0100

    tweaks

--- scripts/build-mls.sh
+++ scripts/build-mls.sh
@@ -3,5 +3,4 @@ set -euo pipefail
 
 mkdir -p ./bin
 cargo build --release --manifest-path ./core/mls/mls-tools/crates/mlslib/Cargo.toml
-cp ./core/mls/mls-tools/target/release/libmls* ./core/node/mls_service/
 cp ./core/mls/mls-tools/target/release/libmls* ./core
