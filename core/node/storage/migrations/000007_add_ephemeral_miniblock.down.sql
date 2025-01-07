db468045b149f090093786a5557f4cdccc135819
rbhnt <roman@hntlabs.com>
2025-01-07 17:19:44 +0000

    Updated migration

--- core/node/storage/migrations/000007_add_ephemeral_miniblock.down.sql
+++ core/node/storage/migrations/000007_add_ephemeral_miniblock.down.sql
@@ -1,2 +1,23 @@
 -- Remove ephemeral column from miniblocks table
-ALTER TABLE miniblocks DROP COLUMN ephemeral;
+DO $$
+    DECLARE
+
+    suffix CHAR(2);
+    i INT;
+
+    numPartitions INT;
+
+    BEGIN
+
+        SELECT num_partitions from settings where single_row_key=true into numPartitions;
+
+        FOR i IN 0.. numPartitions LOOP
+            suffix = LPAD(TO_HEX(i), 2, '0');
+
+            EXECUTE 'ALTER TABLE miniblocks_m' || suffix || ' DROP COLUMN ephemeral;';
+            EXECUTE 'ALTER TABLE miniblocks_r' || suffix || ' DROP COLUMN ephemeral;';
+        END LOOP;
+
+    END;
+$$;
+
--- core/node/storage/migrations/000007_add_ephemeral_miniblock.up.sql
+++ core/node/storage/migrations/000007_add_ephemeral_miniblock.up.sql
@@ -1,2 +1,22 @@
 -- Add ephemeral miniblock state as a new column to the miniblocks table
-ALTER TABLE miniblocks ADD ephemeral BOOLEAN DEFAULT NULL;
+DO $$
+    DECLARE
+
+    suffix CHAR(2);
+    i INT;
+
+    numPartitions INT;
+
+    BEGIN
+
+        SELECT num_partitions from settings where single_row_key=true into numPartitions;
+
+        FOR i IN 0.. numPartitions LOOP
+                suffix = LPAD(TO_HEX(i), 2, '0');
+
+                EXECUTE 'ALTER TABLE miniblocks_m' || suffix || ' ADD ephemeral BOOLEAN DEFAULT NULL;';
+                EXECUTE 'ALTER TABLE miniblocks_r' || suffix || ' ADD ephemeral BOOLEAN DEFAULT NULL;';
+        END LOOP;
+
+    END;
+$$;
