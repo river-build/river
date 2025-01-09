5b3123b011cdf51bd83fc4bbaf520babb39cac5a
Jakub Zalewski <jakub@hntlabs.com>
2025-01-09 23:08:15 +0100

    Add logging to ExternalGroupService

--- packages/sdk/src/mls/externalGroup/externalGroupService.ts
+++ packages/sdk/src/mls/externalGroup/externalGroupService.ts
@@ -5,20 +5,34 @@ import {
     MemberPayload_Mls_InitializeGroup,
 } from '@river-build/proto'
 import { ExternalCrypto } from './externalCrypto'
+import { dlog, DLogger } from '@river-build/dlog'
 
 type InitializeGroupMessage = PlainMessage<MemberPayload_Mls_InitializeGroup>
 type ExternalJoinMessage = PlainMessage<MemberPayload_Mls_ExternalJoin>
 
+const defaultLogger = dlog('csb:mls:externalGroupService')
+
 export class ExternalGroupService {
     private externalGroupCache: Map<string, ExternalGroup> = new Map()
+    private log: {
+        debug: DLogger
+        error: DLogger
+    }
 
     private crypto: ExternalCrypto
 
-    constructor(crypto: ExternalCrypto) {
+    constructor(crypto: ExternalCrypto, opts?: { log: DLogger }) {
         this.crypto = crypto
+        const logger = opts?.log ?? defaultLogger
+        this.log = {
+            debug: logger.extend('debug'),
+            error: logger.extend('error'),
+        }
     }
 
     public getExternalGroup(streamId: string): ExternalGroup | undefined {
+        this.log.debug('getExternalGroup', { streamId })
+
         return this.externalGroupCache.get(streamId)
     }
 
@@ -29,8 +43,11 @@ export class ExternalGroupService {
     }
 
     public async handleInitializeGroup(streamId: string, message: InitializeGroupMessage) {
+        this.log.debug('handleInitializeGroup', { streamId })
+
         if (this.externalGroupCache.has(streamId)) {
             const message = `group already present: ${streamId}`
+            this.log.error(`handleInitializeGroup: ${message}`)
             throw new Error(message)
         }
 
@@ -43,9 +60,12 @@ export class ExternalGroupService {
     }
 
     public async handleExternalJoin(streamId: string, message: ExternalJoinMessage) {
+        this.log.debug('handleExternalJoin', { streamId })
+
         const group = this.externalGroupCache.get(streamId)
         if (!group) {
             const message = `group not found: ${streamId}`
+            this.log.error(`handleExternalJoin: ${message}`)
             throw new Error(message)
         }
 
