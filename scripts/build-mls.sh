3e8aed7d9499b59ab2cc34cd566d2e2e9d0f4f96
Serge Khorun <serge@hntlabs.com>
2025-01-10 18:34:25 -0800

    Remove build-mls.sh script and its related build process.

--- scripts/build-mls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-mkdir -p ./bin
-cargo build --release --manifest-path ./core/mls/mls-tools/crates/mlslib/Cargo.toml
-cp ./core/mls/mls-tools/target/release/libmls* ./core
