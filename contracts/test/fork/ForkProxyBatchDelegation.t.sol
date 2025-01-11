c716c6f80afff54bb1fd70ee0b40e813f7c12285
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-01-10 19:03:37 -0500

    chore(contract): deploy mainnet delegation contracts and fork test (#2010)
    - Updated the deployment addresses for mainnetDelegationFacet in both
    omega and gamma networks. This ensures the configurations point to the
    correct, intended contracts. No other changes were made to the
    deployment files.
    - Add etherscan API key to base-sepolia deployment commands
    - Remove unused test for ForkMainnetDelegation

--- contracts/test/fork/ForkProxyBatchDelegation.t.sol
+++ contracts/test/fork/ForkProxyBatchDelegation.t.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.19;
 
 // utils
 import {Vm} from "forge-std/Vm.sol";
-import {console} from "forge-std/console.sol";
 import {TestUtils} from "contracts/test/utils/TestUtils.sol";
 
 //interfaces
+import {IERC173} from "@river-build/diamond/src/facets/ownable/IERC173.sol";
 import {IMainnetDelegation} from "contracts/src/tokens/river/base/delegation/IMainnetDelegation.sol";
 import {IRewardsDistribution} from "contracts/src/base/registry/facets/distribution/v2/IRewardsDistribution.sol";
 
@@ -33,7 +33,7 @@ contract ForkProxyBatchDelegationTest is TestUtils {
   ProxyBatchDelegation internal proxyBatchDelegation;
 
   function setUp() external onlyForked {
-    vm.createSelectFork("mainnet", 21438000);
+    vm.createSelectFork("mainnet", 21596238);
 
     proxyBatchDelegation = new ProxyBatchDelegation(
       RIVER,
@@ -43,45 +43,51 @@ contract ForkProxyBatchDelegationTest is TestUtils {
     );
   }
 
-  //  function test_sendDelegators() external onlyForked {
-  //    uint32 minGasLimit = 15_000_000;
-  //    vm.recordLogs();
-  //    vm.prank(_randomAddress());
-  //    proxyBatchDelegation.sendDelegators(minGasLimit, true);
-  //
-  //    Vm.Log[] memory logs = vm.getRecordedLogs();
-  //    bytes memory message;
-  //    for (uint256 i; i < logs.length; ++i) {
-  //      if (
-  //        logs[i].topics.length > 0 && logs[i].topics[0] == SENT_MESSAGE_TOPIC
-  //      ) {
-  //        (, message, , ) = abi.decode(
-  //          logs[i].data,
-  //          (address, bytes, uint256, uint256)
-  //        );
-  //        break;
-  //      }
-  //    }
-  //    assertGt(message.length, 0, "message not found");
-  //
-  //    // switch to the base fork
-  //    vm.createSelectFork("base", 23925711);
-  //
-  //    address getMessenger = IMainnetDelegation(BASE_REGISTRY).getMessenger();
-  //    vm.etch(getMessenger, type(MockMessenger).runtimeCode);
-  //    MockMessenger(getMessenger).setXDomainMessageSender(
-  //      IMainnetDelegation(BASE_REGISTRY).getProxyDelegation()
-  //    );
-  //
-  //    vm.prank(address(getMessenger));
-  //    (bool success, ) = BASE_REGISTRY.call{gas: minGasLimit}(message);
-  //    assertTrue(success, "sendDelegators failed");
-  //    assertGt(
-  //      IRewardsDistribution(BASE_REGISTRY)
-  //        .getDepositsByDepositor(BASE_REGISTRY)
-  //        .length,
-  //      0,
-  //      "mainnet delegation failed"
-  //    );
-  //  }
+  function test_relayDelegationDigest() external onlyForked {
+    uint32 minGasLimit = 50_000;
+    vm.recordLogs();
+    vm.prank(_randomAddress());
+    proxyBatchDelegation.relayDelegationDigest(minGasLimit);
+
+    bytes memory encodedMsgs = proxyBatchDelegation.getEncodedMsgs();
+
+    Vm.Log[] memory logs = vm.getRecordedLogs();
+    bytes memory message;
+    for (uint256 i; i < logs.length; ++i) {
+      if (
+        logs[i].topics.length > 0 && logs[i].topics[0] == SENT_MESSAGE_TOPIC
+      ) {
+        (, message, , ) = abi.decode(
+          logs[i].data,
+          (address, bytes, uint256, uint256)
+        );
+        break;
+      }
+    }
+    assertGt(message.length, 0, "message not found");
+
+    // switch to the base fork
+    vm.createSelectFork("base", 24877033);
+
+    address getMessenger = IMainnetDelegation(BASE_REGISTRY).getMessenger();
+    vm.etch(getMessenger, type(MockMessenger).runtimeCode);
+    MockMessenger(getMessenger).setXDomainMessageSender(
+      IMainnetDelegation(BASE_REGISTRY).getProxyDelegation()
+    );
+
+    vm.prank(address(getMessenger));
+    (bool success, ) = BASE_REGISTRY.call{gas: minGasLimit}(message);
+    assertTrue(success, "setDelegationDigest failed");
+
+    vm.prank(IERC173(BASE_REGISTRY).owner());
+    IMainnetDelegation(BASE_REGISTRY).relayDelegations(encodedMsgs);
+
+    assertGt(
+      IRewardsDistribution(BASE_REGISTRY)
+        .getDepositsByDepositor(BASE_REGISTRY)
+        .length,
+      0,
+      "mainnet delegation failed"
+    );
+  }
 }
