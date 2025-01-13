53e137adf1eb6cf19ee654b44fa080db7e6147ab
Erik Olsson <valross2@gmail.com>
2025-01-13 11:15:49 +0100

    add execute connect handler

--- core/node/rpc/get_miniblock_header.go
+++ core/node/rpc/get_miniblock_header.go
@@ -11,6 +11,13 @@ func (s *Service) GetMiniblockHeader(
 	ctx context.Context,
 	req *connect.Request[GetMiniblockHeaderRequest],
 ) (*connect.Response[GetMiniblockHeaderResponse], error) {
+	return executeConnectHandler(ctx, req, s, s.getMiniblockHeaderImpl, "GetMiniblockHeader")
+}
+
+func (s *Service) getMiniblockHeaderImpl(
+	ctx context.Context,
+	req *connect.Request[GetMiniblockHeaderRequest],
+) (*connect.Response[GetMiniblockHeaderResponse], error) {
 	miniblocksRequest := &GetMiniblocksRequest{
 		StreamId: req.Msg.StreamId,
 		FromInclusive: req.Msg.MiniblockNum,
