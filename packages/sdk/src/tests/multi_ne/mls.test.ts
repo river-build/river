19718efc5a5370fb5ac36100be40318658b3985b
Erik Olsson <valross2@gmail.com>
2025-01-14 08:43:31 +0100

    keep track of welcome messages inside snapshot (#2005)
    This is a simpler approach than #2004 
    
    What we need is a foolproof way to keep track of where welcome messages
    are stored, since clients need them to join the group. We also want to
    avoid blind scrollbacks since that will be too inefficient and is
    non-deterministic (what if the welcome pack isn't in there?)
    
    This PR:
    
    When new users join an existing stream, they will upload their
    `KeyPackages`.
    Existing users will process the `KeyPackages`, run
    `Group.addMembers(keyPackages)` and publish an event containing:
    ```
    {
      commit: []byte // commit mutating the group state
      group_info_message: []byte // new updated group info msg for future members
      signature_public_keys: [][]byte // which sigs are included by this op
      welcome_messages: [][]byte // welcome msgs that can be used by the new users to bootstrap their group
    }
    ```
    
    this PR makes sure that returning clients:
    - know that they've been sent a welcome message
    - know where to find it (miniblock num)

--- packages/sdk/src/tests/multi_ne/mls.test.ts
+++ packages/sdk/src/tests/multi_ne/mls.test.ts
@@ -5,7 +5,7 @@
 import { makeTestClient, waitFor } from '../testUtils'
 import { Client } from '../../client'
 import { PlainMessage } from '@bufbuild/protobuf'
-import { MemberPayload_Mls } from '@river-build/proto'
+import { MemberPayload_Mls, MemberPayload_Mls_WelcomeMessage } from '@river-build/proto'
 import {
     ExternalClient,
     Group as MlsGroup,
@@ -20,6 +20,8 @@ import { bin_equal, check } from '@river-build/dlog'
 import { addressFromUserId } from '../../id'
 import { bytesToHex } from 'ethereum-cryptography/utils'
 import { isDefined } from '../../check'
+import { ParsedMiniblock } from '../../types'
+import { fail } from 'assert'
 
 describe('mlsTests', () => {
     let clients: Client[] = []
@@ -43,6 +45,7 @@ describe('mlsTests', () => {
     let latestGroupInfoMessage: Uint8Array
     let latestExternalGroupSnapshot: Uint8Array
     let latestAliceMlsKeyPackage: Uint8Array
+    let welcomeMessageCommit: Uint8Array
     const commits: Uint8Array[] = []
 
     beforeAll(async () => {
@@ -581,6 +584,7 @@ describe('mlsTests', () => {
         ).resolves.not.toThrow()
         latestGroupInfoMessage = groupInfoMessage!.toBytes()
         commits.push(commit)
+        welcomeMessageCommit = commit
     })
 
     test('key packages are cleared after being applied', async () => {
@@ -658,5 +662,39 @@ describe('mlsTests', () => {
         const streamAfterSnapshot = await bobClient.getStream(streamId)
         const mls = streamAfterSnapshot.membershipContent.mls
         expect(mls.members[aliceClient.userId].signaturePublicKeys.length).toBe(3)
+    })
+
+    test('the snapshot contains a pointer to the miniblock containing the welcome message', async () => {
+        function getWelcomeMessage(miniblock: ParsedMiniblock): MemberPayload_Mls_WelcomeMessage {
+            for (const payload of miniblock.events.map((e) => e.event.payload)) {
+                if (payload.value?.content.case !== 'mls') {
+                    continue
+                }
+                if (payload.value.content.value.content.case !== 'welcomeMessage') {
+                    continue
+                }
+                return payload.value.content.value.content.value
+            }
+            fail('no welcome message found')
+        }
+
+        const streamAfterSnapshot = await aliceClient.getStream(streamId)
+        const mls = streamAfterSnapshot.membershipContent.mls
+        const signature = aliceMlsClient2.signaturePublicKey()
+        const miniblockNum = mls.welcomeMessagesMiniblockNum[bytesToHex(signature)]
+        expect(miniblockNum).toBeGreaterThan(0n)
+
+        const { miniblocks } = await aliceClient.getMiniblocks(
+            streamId,
+            miniblockNum,
+            miniblockNum + 1n,
+        )
+
+        expect(miniblocks.length).toBe(1)
+        const welcomeMessage = getWelcomeMessage(miniblocks[0])
+        expect(bin_equal(welcomeMessage.commit, welcomeMessageCommit)).toBe(true)
+        expect(
+            welcomeMessage.signaturePublicKeys.find((val) => bin_equal(val, signature)),
+        ).toBeDefined()
     })
 })
