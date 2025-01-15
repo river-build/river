03eefc104e67ebd0f0421d7dfd304b745aa7dfc7
Jakub Zalewski <jakub@hntlabs.com>
2025-01-15 14:22:57 +0100

    Fix esbuild for stream-metadata

--- packages/stream-metadata/esbuild.config.mjs
+++ packages/stream-metadata/esbuild.config.mjs
@@ -3,7 +3,11 @@ import esbuildPluginPino from "esbuild-plugin-pino";
 
 build({
 	bundle: true,
-	entryPoints: { node_esbuild: "./src/node.ts" }, // Rename the entry point to control the output file name
+	entryPoints: {
+		node_esbuild: "./src/node.ts",
+    // NOTE: For some reason esbuild is not picking it up
+    mls_rs_wasm_bg: "@river-build/mls-rs-wasm-node/mls_rs_wasm_bg.wasm",
+	}, // Rename the entry point to control the output file name
 	format: "cjs",
 	logLevel: "info",
 	loader: {
@@ -26,6 +30,14 @@ build({
 	outExtension: { ".js": ".cjs" }, // Ensure the output file has .cjs extension
 	platform: "node",
 	plugins: [esbuildPluginPino({ transports: ["pino-pretty"] })],
+  alias: {
+    "@river-build/mls-rs-wasm": "@river-build/mls-rs-wasm-node",
+  },
+  assetNames: "[name]",
+  loader: {
+    ".ts": "ts",
+    ".wasm": "file",
+  },
 	sourcemap: true,
 	target: "es2022",
 	minify: false, // No minification for easier debugging. Add minification in production later
