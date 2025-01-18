c2e94ae418281f4504ef500ce476650cdbe34bcd
Crystal Lemire <crystal@hntlabs.com>
2025-01-17 16:44:35 -0800

    Omit sensitive notifications config and increase test coverage over the top-level config.

--- core/config/config_test.go
+++ core/config/config_test.go
@@ -62,3 +62,70 @@ func TestTlsConfig_KeyDoesNotLog(t *testing.T) {
 	// Assert output is as expected: TLS Key is not logged, other fields included
 	require.Equal(t, expected, logOutput)
 }
+
+func TestNotificationsConfig_SensitiveKeysDontLog(t *testing.T) {
+	cfg := config.NotificationsConfig{
+		APN: config.APNPushNotificationsConfig{
+			AuthKey: "APN_AUTH_KEY",
+		},
+		Web: config.WebPushNotificationConfig{
+			Vapid: config.WebPushVapidNotificationConfig{
+				PrivateKey: "WEB_VAPID_PRIVATE_KEY",
+			},
+		},
+		Authentication: config.AuthenticationConfig{
+			SessionToken: config.SessionTokenConfig{
+				Key: config.SessionKeyConfig{
+					Algorithm: "SESSION_KEY_ALGORITHM",
+					Key:       "SESSION_KEY",
+				},
+			},
+		},
+	}
+
+	// Log cfg to buffer
+	logger, buffer := testutils.ZapJsonLogger()
+	logger.Infow("test message", "notificationsConfig", cfg)
+
+	logOutput := buffer.String()
+	require := require.New(t)
+
+	require.NotContains(logOutput, "APN_AUTH_KEY", "Expected APN_AUTH_KEY to be omitted from logOutput `%v`", logOutput)
+	require.NotContains(
+		logOutput,
+		"WEB_VAPID_PRIVATE_KEY",
+		"Expected WEB_VAPID_PRIVATE_KEY to be omitted from logOutput `%v`",
+		logOutput,
+	)
+	require.NotContains(
+		logOutput,
+		"SESSION_KEY_ALGORITHM",
+		"Expected SESSION_KEY_ALGORITHM to be omitted from logOutput `%v`",
+		logOutput,
+	)
+	require.NotContains(logOutput, "SESSION_KEY", "Expected SESSION_KEY to be omitted from logOutput `%v`", logOutput)
+}
+
+func TestConfig_ChainProvidersDoNotLog(t *testing.T) {
+	cfg := config.Config{
+		// In practice both of these fields would contain private information in key:value pairs separated
+		// by commas, but we really just don't want to see whatever the value is in the log output.
+		Chains:       "CHAINS",
+		ChainsString: "CHAINS_STRING",
+	}
+
+	// Log cfg to buffer
+	logger, buffer := testutils.ZapJsonLogger()
+	logger.Infow("test message", "config", cfg)
+
+	logOutput := buffer.String()
+	require := require.New(t)
+
+	require.NotContains(
+		logOutput,
+		"CHAINS_STRING",
+		"Expected CHAINS_STRING to be omitted from logOutput `%v`",
+		logOutput,
+	)
+	require.NotContains(logOutput, "CHAINS", "Expected CHAINS to be omitted from logOutput `%v`", logOutput)
+}
