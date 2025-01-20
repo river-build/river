8c89dc5a02e43d7bf9aa0ed0300f5adfeea4582d
Crystal Lemire <crystal@hntlabs.com>
2025-01-20 11:05:43 -0800

    Reduce logging frequency for archiver a bit more.

--- core/node/logging/sampling.go
+++ core/node/logging/sampling.go
@@ -10,13 +10,13 @@ import (
 // SampledLogger returns a modified version of the input log that samples the logs
 // using zap's sampling algorithm, which drops logs with identical messages within
 // a time period after a threshold of identical logs is met. The sampling here is
-// configured to drop 95% of logs after 10 of the same log message is seen in 10
-// seconds.
+// configured to drop 95% of logs after 5 of the same log message is seen in 1
+// minute.
 func SampledLogger(log *zap.Logger) *zap.Logger {
 	return log.WithOptions(
 		zap.WrapCore(
 			func(core zapcore.Core) zapcore.Core {
-				return zapcore.NewSamplerWithOptions(core, 10*time.Second, 10, 20)
+				return zapcore.NewSamplerWithOptions(core, 1*time.Minute, 5, 20)
 			},
 		),
 	)
