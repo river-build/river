f0dc10ef7c77c50ec45466e0433c6646fd145cb5
Kerem Kazan <kerem.kazan@gmail.com>
2025-01-21 14:26:43 -0500

    more logs

--- core/river_node/main.go
+++ core/river_node/main.go
@@ -3,9 +3,22 @@ package main
 // Execute cobra root command from cmd
 
 import (
+	"fmt"
+	"runtime/debug"
+
 	"github.com/river-build/river/core/cmd"
 )
 
 func main() {
+	debug.SetTraceback("all")
+	// Defer to recover from panics and log debug information
+	defer func() {
+		fmt.Println("Defer function called for panic recovery: in main")
+		if r := recover(); r != nil {
+			fmt.Printf("Panic occurred in main: %v\n", r)
+			debug.PrintStack() // Print the stack trace
+			panic(r)
+		}
+	}()
 	cmd.Execute()
 }
