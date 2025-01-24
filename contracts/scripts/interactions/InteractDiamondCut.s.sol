1b9cb57a6fdaf230183734f26f229bb00ad35560
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-01-24 12:37:52 -0500

    feat(contract): add reinitialization logic for delegation proxies (#2137)
    Introduce a `reinitialize` function in `DelegationProxy` to reset the
    stake token and preserve delegation. Update `RewardsDistribution` to
    invoke reinitialization for existing proxies during initialization. This
    ensures seamless upgrades and delegation consistency.

--- contracts/scripts/interactions/InteractDiamondCut.s.sol
+++ contracts/scripts/interactions/InteractDiamondCut.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.23;
 //interfaces
 import {IDiamondCut} from "@river-build/diamond/src/facets/cut/IDiamondCut.sol";
 import {IDiamond} from "@river-build/diamond/src/Diamond.sol";
+
 //libraries
 
 //contracts
@@ -11,24 +12,29 @@ import {Interaction} from "../common/Interaction.s.sol";
 import {DiamondHelper} from "contracts/test/diamond/Diamond.t.sol";
 
 // facet
-import {DeployStreamRegistry} from "contracts/scripts/deployments/facets/DeployStreamRegistry.s.sol";
+import {DeployRewardsDistributionV2} from "contracts/scripts/deployments/facets/DeployRewardsDistributionV2.s.sol";
 
 contract InteractDiamondCut is Interaction, DiamondHelper {
-  DeployStreamRegistry streamRegistryHelper = new DeployStreamRegistry();
+  DeployRewardsDistributionV2 helper = new DeployRewardsDistributionV2();
 
   function __interact(address deployer) internal override {
     address diamond = getDeployment("riverRegistry");
 
-    address streamRegistry = streamRegistryHelper.deploy(deployer);
+    address rewardsDistribution = helper.deploy(deployer);
 
     addCut(
-      streamRegistryHelper.makeCut(
-        streamRegistry,
-        IDiamond.FacetCutAction.Replace
-      )
+      helper.makeCut(rewardsDistribution, IDiamond.FacetCutAction.Replace)
     );
 
     vm.broadcast(deployer);
-    IDiamondCut(diamond).diamondCut(_cuts, address(0), "");
+    IDiamondCut(diamond).diamondCut(
+      _cuts,
+      rewardsDistribution,
+      helper.makeInitData(
+        0xd47972d8A64Fc4ea4435E31D5c8C3E65BD51e293,
+        0xd47972d8A64Fc4ea4435E31D5c8C3E65BD51e293,
+        14 days
+      )
+    );
   }
 }
