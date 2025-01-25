cd2c190687172b9252e2f0625c3064932218f6ae
Jakub Zalewski <jakub@hntlabs.com>
2025-01-25 13:09:39 +0100

    (playground) Add a basic playground widget to enable MLS

---
+++ packages/playground/src/components/blocks/encryption.tsx
@@ -0,0 +1,20 @@
+import { useEncryptionAlgorithm } from '@/hooks/encryption-algorithm.tsx'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+
+type EncryptionProps = {
+    streamId: string
+}
+const MLS_ALGORITHM = 'mls_0.0.1'
+
+export const Encryption = ({ streamId }: EncryptionProps) => {
+    const { encryption, setEncryptionAlgorithm } = useEncryptionAlgorithm(streamId)
+    return (
+        <div>
+            <Label>
+                {encryption === MLS_ALGORITHM ? 'MLS ENABLED' : 'MLS DISABLED'}
+            </Label>
+            <Button onClick={() => setEncryptionAlgorithm(MLS_ALGORITHM)}>ENABLE MLS</Button>
+        </div>
+    )
+}
