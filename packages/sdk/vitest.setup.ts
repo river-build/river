c2f6eaaafaffa35f3191f0003c2fcdf573c13753
Jakub Zalewski <jakub@hntlabs.com>
2025-01-28 19:14:26 +0100

    Fix prettier

--- packages/sdk/vitest.setup.ts
+++ packages/sdk/vitest.setup.ts
@@ -1,17 +1,17 @@
-import 'fake-indexeddb/auto';
-import createFetchMock from 'vitest-fetch-mock';
-import { vi } from 'vitest';
+import 'fake-indexeddb/auto'
+import createFetchMock from 'vitest-fetch-mock'
+import { vi } from 'vitest'
 
-const fetchMocker = createFetchMock(vi);
-import { readFile } from 'fs/promises';
-import { resolve } from 'path';
+const fetchMocker = createFetchMock(vi)
+import { readFile } from 'fs/promises'
+import { resolve } from 'path'
 
-const olmWasmPath = resolve(__dirname, '../../node_modules/@matrix-org/olm/olm.wasm');
+const olmWasmPath = resolve(__dirname, '../../node_modules/@matrix-org/olm/olm.wasm')
 
 // Return olm.wasm from olm package
 fetchMocker.mockIf(/olm\.wasm$/, async (_req: Request) => {
     try {
-        const wasmContent = await readFile(olmWasmPath); // Read binary data
+        const wasmContent = await readFile(olmWasmPath) // Read binary data
 
         // Construct and return a Response object
         return new Response(wasmContent, {
@@ -21,9 +21,9 @@ fetchMocker.mockIf(/olm\.wasm$/, async (_req: Request) => {
                 'Cache-Control': 'public, max-age=31536000, immutable', // Optional caching
                 'Access-Control-Allow-Origin': '*', // Optional CORS
             },
-        });
+        })
     } catch (error) {
-        console.error('Error serving .wasm file:', error);
+        console.error('Error serving .wasm file:', error)
 
         // Return a 500 Response object on error
         return new Response('Internal Server Error', {
@@ -31,8 +31,8 @@ fetchMocker.mockIf(/olm\.wasm$/, async (_req: Request) => {
             headers: {
                 'Content-Type': 'text/plain',
             },
-        });
+        })
     }
-});
+})
 
-fetchMocker.enableMocks();
+fetchMocker.enableMocks()
