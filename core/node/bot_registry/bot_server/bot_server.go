f522fcf1458d965e5ac35702ddee3f714becba34
Crystal Lemire <crystal@hntlabs.com>
2025-01-31 14:39:24 -0800

    Add some comments and TODOs.

--- core/node/bot_registry/bot_server/bot_server.go
+++ core/node/bot_registry/bot_server/bot_server.go
@@ -1,4 +1,14 @@
 package bot_server
 
-func ValidateWebhook() {
+// InitializeWebhook calls "initialize" on a bot service specified by the webhook url
+// and returns the device id and fallback key returned by the service. This
+// (device_id, fallback_key) should match what we observe in the bot's user stream.
+// TODO - implement
+func InitializeWebhook() {
+}
+
+// GetWebhookStatus sends an "info" message to the bot service and expects a 200 with
+// version info returned.
+// TODO - implement.
+func GetWebhookStatus() {
 }
