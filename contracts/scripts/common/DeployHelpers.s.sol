97e36c36bf8497436ff1c7c636512e622fa9c300
g <5714678+giuseppecrj@users.noreply.github.com>
2025-02-07 14:24:39 -0500

    deploy xchain omega

--- contracts/scripts/common/DeployHelpers.s.sol
+++ contracts/scripts/common/DeployHelpers.s.sol
@@ -111,7 +111,7 @@ abstract contract DeployHelpers is CommonBase {
   // =============================================================
   //                     FILE SYSTEM HELPERS
   // =============================================================
-  function exists(string memory path) internal view returns (bool) {
+  function exists(string memory path) internal returns (bool) {
     return vm.exists(path);
   }
 
